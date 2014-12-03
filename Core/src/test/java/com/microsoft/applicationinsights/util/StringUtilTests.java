9c9fe46db4b69ee59637d67225778a8bc28e6dfb
Sergey Kanzhelev <Sergey.Kanzhelev@microsoft.com>
2014-12-02 17:29:04 -0800

    test for tryParseByte as an example

---
+++ Core/src/test/java/com/microsoft/applicationinsights/util/StringUtilTests.java
@@ -0,0 +1,18 @@
+package com.microsoft.applicationinsights.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class StringUtilTests {
+
+    @Test
+    public void testTestTryParseByteParses123() throws Exception {
+        Assert.assertTrue(StringUtil.tryParseByte("123"));
+    }
+
+    @Test
+    public void testTestTryParseByteDoNotParsesNonByte() throws Exception {
+        Assert.assertFalse(StringUtil.tryParseByte("xyz"));
+    }
+}
