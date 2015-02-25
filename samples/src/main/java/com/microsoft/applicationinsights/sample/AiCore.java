f998ccf420f5091a47a977eb21dffeeff8e76268
Amnon Shochot <amnonsh@microsoft.com>
2015-02-25 23:09:18 +0200

    Reorganizing SDK Sample Files

--- samples/src/main/java/com/microsoft/applicationinsights/sample/AiCore.java
+++ samples/src/main/java/com/microsoft/applicationinsights/sample/AiCore.java
@@ -27,97 +27,68 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.TelemetryConfiguration;
 import com.microsoft.applicationinsights.telemetry.HttpRequestTelemetry;
 import com.microsoft.applicationinsights.telemetry.MetricTelemetry;
 
 @SuppressWarnings("ALL")
-public class Program {
+public class AiCore {
     public static void main(String[] args) throws IOException {
-        validateCore();
+        System.out.println("This program sends various application insights telemetry events.");
 
-        traceLog4J12();
-        infoLog4J12();
-
-        traceLog4J2();
-        infoLog4J2();
-
-        traceLogback();
-        infoLogback();
-
-        System.out.println("Press Enter to terminate...");
-        System.in.read();
-    }
-
-    // region Logging
-
-    private static void traceLog4J12() {
-        org.apache.log4j.Logger logger = org.apache.log4j.LogManager.getRootLogger();
-        logger.trace("New Log4j 1.2 event!");
-    }
-
-    private static void infoLog4J12() {
-        org.apache.log4j.Logger logger = org.apache.log4j.LogManager.getRootLogger();
-        logger.info("info: New Log4j 1.2 event!");
-    }
-
-    private static void traceLog4J2() {
-        org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getRootLogger();
-        logger.trace("New Log4j 2 event!");
-    }
-
-    private static void infoLog4J2() {
-        org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getRootLogger();
-        logger.info("info: New Log4j 2 event!");
-    }
-
-    private static void traceLogback() {
-        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger("root");
-        logger.trace("New Logback event!");
-    }
-
-    private static void infoLogback() {
-        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger("root");
-        logger.info("info: New Logback event!");
-    }
-
-    // endregion Logging
-
-    // region Core
-
-    private static void validateCore() throws IOException {
         TelemetryClient appInsights = new TelemetryClient();
+        if (args.length > 0) {
+            appInsights.getContext().setInstrumentationKey(args[0]);
+        }
+        String iKey = appInsights.getContext().getInstrumentationKey();
+        if (iKey == null)
+        {
+            System.out.println("Error: no iKey set in ApplicationInsights.xml or as a parameter for this program.");
+            return;
+        }
+        System.out.println("Application iKey set to " + appInsights.getContext().getInstrumentationKey());
+        TelemetryConfiguration.getActive().getChannel().setDeveloperMode(true);
+
+        System.out.println();
         appInsights.getContext().getProperties().put("programmatic", "works");
+        System.out.println("Set context property      -- programmatic=works");
 
         appInsights.trackPageView("default page");
+        System.out.println("[1] PageView              -- page=\"default page\"");
 
+        // Tracking metrics event
         Map<String, Double> metrics = new HashMap<String, Double>();
         metrics.put("Answers", (double) 15);
-
         appInsights.trackEvent("A test event", null, metrics);
+        System.out.println("[2] Custom Event (metric) -- name=\"A test event\", metric:Answers=15");
 
+        // Trace telemetry
         appInsights.trackTrace("Things seem to be going well");
+        System.out.println("[3] Trace                 -- text=\"Things seem to be going well\"");
 
+        // Metric Telemetry
         MetricTelemetry mt = new MetricTelemetry("Test time", 17.0);
         mt.setMax(20.0);
         mt.setMin(10.0);
         mt.setCount(100);
         mt.setStandardDeviation(2.43);
         appInsights.trackMetric(mt);
+        System.out.println("[4] Metric                -- metric:\"Test time\", value=17.0, Max=20.0, Min=10.0, Count=100 and STDV=2.43");
 
+        // Http Request Telemetry
         HttpRequestTelemetry rt = new HttpRequestTelemetry("ping", new Date(), 4711, "200", true);
         rt.setHttpMethod("GET");
         rt.setUrl("http://tempuri.org/ping");
         appInsights.track(rt);
+        System.out.println("[5] HttpRequest           -- url=\"http://tempuri.org/ping\", HttpMethod=\"GET\", request=\"ping\", duration=4711, response=200 and success=true");
 
+        // Tracking Exception
         try {
-            throwException("This is only a test!");
+            throw new Exception("This is only a test!");
         } catch (Exception exc) {
             appInsights.trackException(exc);
+            System.out.println("[6] Exception             -- message=\"This is only a test!\"");
         }
-    }
-
-    private static void throwException(String msg) throws Exception {
-        throw new Exception(msg);
     }
 
     // endregion Core
