b922a0d94e2edd68c89218a7f18c2c97571099ea
Guy Peleg <gupele@microsoft.com>
2015-03-24 15:12:50 +0200

    Remove static that caused race conditions under load

--- core/src/main/java/com/microsoft/applicationinsights/internal/util/LocalStringsUtils.java
+++ core/src/main/java/com/microsoft/applicationinsights/internal/util/LocalStringsUtils.java
@@ -25,44 +25,25 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class LocalStringsUtils
-{
+public class LocalStringsUtils {
     /**
      * Determine whether a string is null or empty.
      * @param value The string value
      * @return True if the string is either null or empty.
      */
-    public static boolean isNullOrEmpty(String value)
-    {
+    public static boolean isNullOrEmpty(String value) {
         return value == null || value.isEmpty();
     }
 
-    public static String populateRequiredStringWithNullValue(String value, String parameterName, String telemetryType)
-    {
-        if (isNullOrEmpty(value))
-        {
+    public static String populateRequiredStringWithNullValue(String value, String parameterName, String telemetryType) {
+        if (isNullOrEmpty(value)) {
             return parameterName + " is a required field for " + telemetryType;
         }
 
         return value;
     }
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public static boolean tryParseByte(String str)
-    {
-        try
-        {
-            Byte.parseByte(str);
-            return true;
-        }
-        catch (NumberFormatException nfe)
-        {
-            return false;
-        }
-    }
-
-    public static String generateRandomId(boolean removeDashes)
-    {
+    public static String generateRandomId(boolean removeDashes) {
         String uuid = UUID.randomUUID().toString();
 
         if (removeDashes) {
@@ -72,23 +53,14 @@ public class LocalStringsUtils
         return uuid;
     }
 
-    public static String generateRandomIntegerId()
-    {
+    public static String generateRandomIntegerId() {
         Random random = new Random();
         long abs = Math.abs(random.nextLong());
 
         return String.valueOf(abs);
     }
 
-    public static DateFormat getDateFormatter()
-    {
-        if (s_dateFormatter == null)
-            s_dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
-        return s_dateFormatter;
+    public static DateFormat getDateFormatter() {
+        return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
     }
-
-
-    private static Calendar s_calendar;
-    private static DateFormat s_dateFormatter;
-    private static DateFormat s_durationFormatter;
 }
