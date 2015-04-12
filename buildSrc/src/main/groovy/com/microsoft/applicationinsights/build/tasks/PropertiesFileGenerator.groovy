2dac8da80eca7c57d29edb96a0ff7b2e8dd1b326
Amnon Shochot <amnonsh@microsoft.com>
2015-04-13 01:36:17 +0300

    Build/Versioning - added up-to-date logic for PropsFileGen task

--- buildSrc/src/main/groovy/com/microsoft/applicationinsights/build/tasks/PropertiesFileGenerator.groovy
+++ buildSrc/src/main/groovy/com/microsoft/applicationinsights/build/tasks/PropertiesFileGenerator.groovy
@@ -1,21 +1,51 @@
 package com.microsoft.applicationinsights.build.tasks
 
+import org.gradle.api.Task;
 import org.gradle.api.DefaultTask
+import org.gradle.api.specs.Spec
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 class PropsFileGen extends DefaultTask {
-    File targetFile
+    @Input
     String comment = "--- No Comment ---"
-    private Properties props = new Properties()
 
-    def property(String key, String value) { props.setProperty(key, value) }
+    @OutputFile
+    File targetFile
+
+    private final Properties props = new Properties()
+
+    PropsFileGen() {
+        getOutputs().upToDateWhen(new Spec<Task>() {
+            public boolean isSatisfiedBy(Task element) {
+                return propsAreEqual();
+            }
+        });
+    }
+
+    def property(String key, String value) {
+        props.setProperty(key, value)
+    }
 
     @TaskAction
-    def generate() {
+    private def generate() {
         def targetDir = targetFile.getParentFile()
         targetDir.mkdirs()
         FileOutputStream out = new FileOutputStream(targetFile.getAbsolutePath())
         props.store(out, comment)
         out.close()
+    }
+
+    private boolean propsAreEqual() {
+        if (!targetFile.exists()) {
+            return false;
+        }
+
+        Properties oldProps = new Properties();
+        FileInputStream inFileStream = new FileInputStream(targetFile);
+        oldProps.load(inFileStream);
+
+        return oldProps.equals(props);
     }
 }
