a053cd939e38671a50d4b1643d719034d27c25dd
Amnon Shochot <amnonsh@microsoft.com>
2015-06-01 18:23:20 +0300

    Integration Tests - improved excetion message in case of failure because of timeout

--- web/src/integTest/java/com/microsoft/applicationinsights/web/spring/TestEnvironment.java
+++ web/src/integTest/java/com/microsoft/applicationinsights/web/spring/TestEnvironment.java
@@ -13,11 +13,11 @@ public class TestEnvironment {
 
     String configurationEnvironmentVariable = "AI_JAVASDK_TEST_ENVIRONMENT_SETTINGS_PATH";
 
-    private final String KEY_APPLICATION_SERVER                    = "env.applicationServer";
-    private final String KEY_APPLICATION_SERVER_PORT               = "env.applicationServer.port";
-    private final String KEY_APPLICATION_NAME                      = "env.application.name";
-    private final String KEY_APPLICATION_STORAGE_CONNECTION_STRING = "env.application.storage.connectionString";
-    private final String KEY_APPLICATION_STORAGE_EXPORT_QUEUE_NAME = "env.application.storage.exportQueueName";
+    public static final String KEY_APPLICATION_SERVER                    = "env.applicationServer";
+    public static final String KEY_APPLICATION_SERVER_PORT               = "env.applicationServer.port";
+    public static final String KEY_APPLICATION_NAME                      = "env.application.name";
+    public static final String KEY_APPLICATION_STORAGE_CONNECTION_STRING = "env.application.storage.connectionString";
+    public static final String KEY_APPLICATION_STORAGE_EXPORT_QUEUE_NAME = "env.application.storage.exportQueueName";
 
     public TestEnvironment() throws IOException {
         String envPropsFilePath = System.getenv(configurationEnvironmentVariable);
