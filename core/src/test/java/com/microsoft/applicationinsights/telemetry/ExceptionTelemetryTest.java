6f77d11a26188e750284e4bc994ee1bc02ed6d84
Guy Peleg <gupele@microsoft.com>
2015-06-28 09:39:27 +0300

    Core - Exception Telemetry will create an Exception if passed null

--- core/src/test/java/com/microsoft/applicationinsights/telemetry/ExceptionTelemetryTest.java
+++ core/src/test/java/com/microsoft/applicationinsights/telemetry/ExceptionTelemetryTest.java
@@ -29,8 +29,17 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
 public final class ExceptionTelemetryTest {
+
+    @Test
+    public void testNullException() {
+        ExceptionTelemetry telemetry = new ExceptionTelemetry(null);
+        assertNotNull(telemetry.getException());
+        assertNotNull(telemetry.getThrowable());
+        assertSame(telemetry.getException(), telemetry.getThrowable());
+    }
 
     @Test
     public void testSetException() {
