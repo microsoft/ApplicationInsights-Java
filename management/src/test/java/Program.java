8158b5f2c5252cde2aeddf59db4b3dc38b65426f
Guy Peleg <gupele@microsoft.com>
2015-08-09 12:21:52 +0300

    Management - Added new method for location by subcription id

--- management/src/test/java/Program.java
+++ management/src/test/java/Program.java
@@ -1,3 +1,24 @@
+/*
+ * ApplicationInsights-Java
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ *
+ * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the ""Software""), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 import com.microsoft.applicationinsights.management.authentication.Authenticator;
 import com.microsoft.applicationinsights.management.rest.ApplicationInsightsManagementClient;
 import com.microsoft.applicationinsights.management.rest.model.Resource;
@@ -62,7 +83,9 @@ public class Program {
         String requiredSubscriptionID = subscriptions.get(0).getId();
         getResources(requiredSubscriptionID);
         getAvailableGeoLocations();
-        getAvailableGeoLocations(requiredSubscriptionID);
+        for (Subscription subscription : subscriptions) {
+            getAvailableGeoLocations(subscription.getId());
+        }
         getResourceGroups(requiredSubscriptionID);
 
 //        createResourceGroup(requiredSubscriptionID, resourceGroup, location);
