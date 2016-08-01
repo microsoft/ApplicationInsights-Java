297be09eadd0027028ebe743eed2acb8dc814490
Guy Peleg <gupele@microsoft.com>
2016-08-01 11:50:57 +0300

    Changed sample filter name and added doc

--- samples/src/main/java/com/microsoft/applicationinsights/sample/SampleFilter.java
+++ samples/src/main/java/com/microsoft/applicationinsights/sample/SampleFilter.java
@@ -26,21 +26,42 @@ import com.microsoft.applicationinsights.telemetry.Telemetry;
 
 /**
  * Sample filter
+ * This class is an example for having a custom filter that can filter out unneded telemetries.
+ * To have one, you should implement {@link TelemetryProcessor} and implement its 'process' method.
+ *
+ * The method gets a {@link Telemetry} instance that is ready to be sent. This is your chance to approve
+ * or deny it. Returning 'false' means that the Telemetry will not be sent while 'true' means you approve it.
+ *
+ * The Telemetry might go through other filters though, that might deny its sending.
+ *
+ * To enable this processor you need to add it in the ApplicationInsights.xml like this:
+ *
+ *
+ <TelemetryProcessors>
+
+     <CustomProcessors>
+        <Processor type="com.microsoft.applicationinsights.sample.SampleFilter">
+            <Add name="Pass" value="false"/>
+        </Processor>
+    </CustomProcessors>
+
+ </TelemetryProcessors>
+ *
+ * Note that the class defines a property named 'pass' which is configured too.
+ * Every property that you wish to configure needs to have a 'setX' public method like 'setPass' in this example
+ * <b>Exceptions thrown from the 'setX' methods will be caught by the framework that will ignore the filter</b>
  *
  * Created by gupele on 7/26/2016.
  */
-public class DebugProcessor implements TelemetryProcessor {
-    private boolean bad = false;
+public class SampleFilter implements TelemetryProcessor {
+    private boolean pass = false;
 
-    public void setBad(String bad) {
-        try {
-            this.bad = Boolean.valueOf(bad);
-        } catch (Throwable t) {
-        }
+    public void setPass(String bad) {
+        this.pass = Boolean.valueOf(bad);
     }
 
     @Override
     public boolean process(Telemetry telemetry) {
-        return bad;
+        return pass;
     }
 }
