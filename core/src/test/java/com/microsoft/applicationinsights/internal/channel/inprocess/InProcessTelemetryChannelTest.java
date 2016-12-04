8e025eb9f4bffabbab9ce0192a19309c24228afa
Guy Peleg <gupele@microsoft.com>
2016-12-04 09:29:47 +0200

    Added a way to set developer mode from system

--- core/src/test/java/com/microsoft/applicationinsights/internal/channel/inprocess/InProcessTelemetryChannelTest.java
+++ core/src/test/java/com/microsoft/applicationinsights/internal/channel/inprocess/InProcessTelemetryChannelTest.java
@@ -31,11 +31,6 @@ public class InProcessTelemetryChannelTest {
     private final static String NON_VALID_URL = "http:sd{@~fsd.s.d.f;fffff";
 
     @Test(expected = IllegalArgumentException.class)
-    public void testNotValidEndpointAddress() {
-        new InProcessTelemetryChannel(NON_VALID_URL, false);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
     public void testNotValidEndpointAddressAsMapValue() {
         HashMap<String, String> map = new HashMap<String, String>();
         map.put("EndpointAddress", NON_VALID_URL);
