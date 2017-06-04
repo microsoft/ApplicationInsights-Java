bf2cd49d932d4ae182cf4f4df57bd78699be76d2
James Davis <james.l.davis@outlook.com>
2017-06-04 19:34:39 -0400

    Fixing Ping Sender connection pool leak as well

--- core/src/main/java/com/microsoft/applicationinsights/internal/quickpulse/DefaultQuickPulsePingSender.java
+++ core/src/main/java/com/microsoft/applicationinsights/internal/quickpulse/DefaultQuickPulsePingSender.java
@@ -36,82 +36,89 @@ import com.microsoft.applicationinsights.internal.channel.common.ApacheSender;
  * Created by gupele on 12/12/2016.
  */
 final class DefaultQuickPulsePingSender implements QuickPulsePingSender {
-    private final static String QP_BASE_URI = "https://rt.services.visualstudio.com/QuickPulseService.svc/";
+	private final static String QP_BASE_URI = "https://rt.services.visualstudio.com/QuickPulseService.svc/";
 
-    private final String quickPulsePingUri;
-    private final ApacheSender apacheSender;
-    private final QuickPulseNetworkHelper networkHelper = new QuickPulseNetworkHelper();
-    private String pingPrefix;
-    private long lastValidTransmission = 0;
+	private final String quickPulsePingUri;
+	private final ApacheSender apacheSender;
+	private final QuickPulseNetworkHelper networkHelper = new QuickPulseNetworkHelper();
+	private String pingPrefix;
+	private long lastValidTransmission = 0;
 
-    public DefaultQuickPulsePingSender(final ApacheSender apacheSender, final String instanceName, final String quickPulseId) {
-        this.apacheSender = apacheSender;
+	public DefaultQuickPulsePingSender(final ApacheSender apacheSender, final String instanceName,
+			final String quickPulseId) {
+		this.apacheSender = apacheSender;
 
-        final String ikey = TelemetryConfiguration.getActive().getInstrumentationKey();
-        quickPulsePingUri = QP_BASE_URI + "ping?ikey=" + ikey;
+		final String ikey = TelemetryConfiguration.getActive().getInstrumentationKey();
+		quickPulsePingUri = QP_BASE_URI + "ping?ikey=" + ikey;
 
-        final StrBuilder sb = new StrBuilder();
-        sb.append("{");
-        sb.append("\"Documents\": null,");
-        sb.append("\"Instance\":\"" + instanceName + "\",");
-        sb.append("\"InstrumentationKey\": null,");
-        sb.append("\"InvariantVersion\": 2,");
-        sb.append("\"MachineName\":\"" + instanceName + "\",");
-        sb.append("\"Metrics\": null,");
-        sb.append("\"StreamId\": \"" + quickPulseId + "\",");
-        sb.append("\"Timestamp\": \"\\/Date(");
+		final StrBuilder sb = new StrBuilder();
+		sb.append("{");
+		sb.append("\"Documents\": null,");
+		sb.append("\"Instance\":\"" + instanceName + "\",");
+		sb.append("\"InstrumentationKey\": null,");
+		sb.append("\"InvariantVersion\": 2,");
+		sb.append("\"MachineName\":\"" + instanceName + "\",");
+		sb.append("\"Metrics\": null,");
+		sb.append("\"StreamId\": \"" + quickPulseId + "\",");
+		sb.append("\"Timestamp\": \"\\/Date(");
 
-        pingPrefix = sb.toString();
-    }
+		pingPrefix = sb.toString();
+	}
 
-    @Override
-    public QuickPulseStatus ping() {
-        final Date currentDate = new Date();
-        final HttpPost request = networkHelper.buildRequest(currentDate, quickPulsePingUri);
+	@Override
+	public QuickPulseStatus ping() {
+		final Date currentDate = new Date();
+		final HttpPost request = networkHelper.buildRequest(currentDate, quickPulsePingUri);
 
-        final ByteArrayEntity pingEntity = buildPingEntity(currentDate.getTime());
-        request.setEntity(pingEntity);
+		final ByteArrayEntity pingEntity = buildPingEntity(currentDate.getTime());
+		request.setEntity(pingEntity);
 
-        final long sendTime = System.nanoTime();
-        try {
-            HttpResponse response = apacheSender.sendPostRequest(request);
-            if (networkHelper.isSuccess(response)) {
-                final QuickPulseStatus quickPulseResultStatus = networkHelper.getQuickPulseStatus(response);
-                switch (quickPulseResultStatus) {
-                    case QP_IS_OFF:
-                    case QP_IS_ON:
-                        lastValidTransmission = sendTime;
-                        return quickPulseResultStatus;
+		final long sendTime = System.nanoTime();
+		HttpResponse response = null;
+		try {
+			response = apacheSender.sendPostRequest(request);
+			if (networkHelper.isSuccess(response)) {
+				final QuickPulseStatus quickPulseResultStatus = networkHelper.getQuickPulseStatus(response);
+				switch (quickPulseResultStatus) {
+				case QP_IS_OFF:
+				case QP_IS_ON:
+					lastValidTransmission = sendTime;
+					return quickPulseResultStatus;
 
-                    case ERROR:
-                        break;
+				case ERROR:
+					break;
 
-                    default:
-                        break;
-                }
-            }
-        } catch (IOException e) {
-        }
-        return onPingError(sendTime);
-    }
+				default:
+					break;
+				}
+			}
+		} catch (IOException e) {
 
-    private ByteArrayEntity buildPingEntity(long timeInMillis) {
+		} finally {
+			if (response != null) {
+				apacheSender.dispose(response);
+			}
+		}
+		return onPingError(sendTime);
+	}
 
-        StrBuilder sb = new StrBuilder(pingPrefix);
-        sb.append(timeInMillis);
-        sb.append(")\\/\",");
-        sb.append("\"Version\":\"2.2.0-738\"");
-        sb.append("}");
-        ByteArrayEntity bae = new ByteArrayEntity(sb.toString().getBytes());
-        return bae;
-    }
+	private ByteArrayEntity buildPingEntity(long timeInMillis) {
 
-    private QuickPulseStatus onPingError(long sendTime) {
-        final double timeFromLastValidTransmission = (sendTime - lastValidTransmission) / 1000000000.0;
-        if (timeFromLastValidTransmission >= 60.0) {
-            return QuickPulseStatus.ERROR;
-        }
+		StrBuilder sb = new StrBuilder(pingPrefix);
+		sb.append(timeInMillis);
+		sb.append(")\\/\",");
+		sb.append("\"Version\":\"2.2.0-738\"");
+		sb.append("}");
+		ByteArrayEntity bae = new ByteArrayEntity(sb.toString().getBytes());
+		return bae;
+	}
 
-        return QuickPulseStatus.QP_IS_OFF;
-    }
+	private QuickPulseStatus onPingError(long sendTime) {
+		final double timeFromLastValidTransmission = (sendTime - lastValidTransmission) / 1000000000.0;
+		if (timeFromLastValidTransmission >= 60.0) {
+			return QuickPulseStatus.ERROR;
+		}
+
+		return QuickPulseStatus.QP_IS_OFF;
+	}
 }
