fc5ec6e6226e05103602a144e33daff27c71b030
littleaj <1690572+littleaj@users.noreply.github.com>
2017-11-21 15:35:45 -0800

    reworked how logs are redirected to stdout

--- test/smoke/utils/src/main/java/com/microsoft/applicationinsights/smoketest/docker/AiDockerClient.java
+++ test/smoke/utils/src/main/java/com/microsoft/applicationinsights/smoketest/docker/AiDockerClient.java
@@ -53,38 +53,50 @@ public class AiDockerClient {
 		return new AiDockerClient(DEFAULT_WINDOWS_USER, DEFAULT_WINDOWS_SHELL);
 	}
 
+	private ProcessBuilder buildProcess(String... cmdLine) {
+		return new ProcessBuilder(cmdLine).redirectErrorStream(true);
+	}
+
+	private ProcessBuilder buildProcess(List<String> cmdLine) {
+		return new ProcessBuilder(cmdLine).redirectErrorStream(true);
+	}
+
 	public String startContainer(String image, String portMapping) throws IOException, InterruptedException {
 		Preconditions.checkNotNull(image, "image");
 		Preconditions.checkNotNull(portMapping, "portMapping");
 
 		String localIp = InetAddress.getLocalHost().getHostAddress();
 
-		Process p = new ProcessBuilder("docker", "run", "-d", "-p", portMapping, "--add-host=fakeingestion:"+localIp, image).start();
+		Process p = buildProcess("docker", "run", "-d", "-p", portMapping, "--add-host=fakeingestion:"+localIp, image).start();
 		if (!p.waitFor(3, TimeUnit.SECONDS)) {
 			p.destroyForcibly();
-			flushStandardStreams(p);
+			flushStdout(p);
 			throw new RuntimeException("Could not start container: timeout reached");
 		}
 		final int exitCode = p.exitValue();
 		if (exitCode != 0) {
-			flushStandardStreams(p);
+			flushStdout(p);
 			throw new RuntimeException("Starting container exited with code "+exitCode);
 		}
 		List<String> lines = CharStreams.readLines(new InputStreamReader(p.getInputStream()));
 		return lines.get(0);
 	}
 
-	private static void flushStandardStreams(Process p) throws IOException {
+	private static void flushStdout(Process p) throws IOException {
 		Preconditions.checkNotNull(p);
-		CharStreams.copy(new InputStreamReader(p.getInputStream()), System.out);
-		CharStreams.copy(new InputStreamReader(p.getErrorStream()), System.err);
+		
+		try (Scanner r = new Scanner(p.getInputStream())) {	
+			while (r.hasNext()) {
+				System.out.println(r.nextLine());
+			}
+		}
 	}
 
 	public void copyAndDeployToContainer(String id, File appArchive) throws IOException, InterruptedException {
 		Preconditions.checkNotNull(id, "id");
 		Preconditions.checkNotNull(appArchive, "appArchive");
 		
-		Process p = new ProcessBuilder("docker", "cp", appArchive.getAbsolutePath(), String.format("%s:%s", id, "/root/docker-stage")).start();
+		Process p = buildProcess("docker", "cp", appArchive.getAbsolutePath(), String.format("%s:%s", id, "/root/docker-stage")).start();
 		waitAndCheckCodeForProcess(p, 10, TimeUnit.SECONDS, String.format("copy %s to container %s", appArchive.getPath(), id));
 		
 		execOnContainer(id, getShellExecutor(), "./deploy.sh", appArchive.getName());
@@ -99,28 +111,27 @@ public class AiDockerClient {
 		if (args.length > 0) {
 			cmdList.addAll(Arrays.asList(args));
 		}
-		Process p = new ProcessBuilder(cmdList).start();
+		Process p = buildProcess(cmdList).start();
 		try {
 			waitAndCheckCodeForProcess(p, 10, TimeUnit.SECONDS, String.format("executing command on container: '%s'", id, Joiner.on(' ').join(cmdList)));
-			flushStandardStreams(p);
+			flushStdout(p);
 		}
 		catch (Exception e) {
 			throw e;
 		}
-		
 	}
 
 	private static void waitAndCheckCodeForProcess(Process p, long timeout, TimeUnit unit, String actionName) throws IOException, InterruptedException {
 		if (!p.waitFor(timeout, unit)) {
 			p.destroyForcibly();
-			flushStandardStreams(p);
+			flushStdout(p);
 			if (!Strings.isNullOrEmpty(actionName)) {
 				throw new RuntimeException("Hit timeout trying to "+actionName);
 			}
 			throw new RuntimeException("Process timed out");
 		}
 		if (p.exitValue() != 0) {
-			flushStandardStreams(p);
+			flushStdout(p);
 			if (!Strings.isNullOrEmpty(actionName)) {
 				throw new RuntimeException(String.format("Nonzero exit code (%d) trying to %s", p.exitValue(), actionName));
 			}
@@ -131,16 +142,12 @@ public class AiDockerClient {
 	public void printContainerLogs(String containerId) throws IOException {
 		Preconditions.checkNotNull(containerId, "containerId");
 
-		Process p = new ProcessBuilder("docker", "container", "logs", containerId).redirectErrorStream(true).start();
-		try (Scanner r = new Scanner(p.getInputStream())) {	
-			while (r.hasNext()) {
-				System.out.println(r.nextLine());
-			}
-		}
+		Process p = buildProcess("docker", "container", "logs", containerId).start();
+		flushStdout(p);
 	}
 
 	public void stopContainer(String id) throws IOException, InterruptedException {
-		Process p = new ProcessBuilder("docker", "container", "stop", id).start();
+		Process p = buildProcess("docker", "container", "stop", id).start();
 		waitAndCheckCodeForProcess(p, 30, TimeUnit.SECONDS, String.format("stopping container %s", id));
 	}
 
