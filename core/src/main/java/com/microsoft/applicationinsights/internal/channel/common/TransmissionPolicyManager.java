cd32481e9c588c1b22ebe2763c7d25b041229fa3
James Davis <debugthings@users.noreply.github.com>
2018-02-11 17:27:44 -0500

    Missed one

--- core/src/main/java/com/microsoft/applicationinsights/internal/channel/common/TransmissionPolicyManager.java
+++ core/src/main/java/com/microsoft/applicationinsights/internal/channel/common/TransmissionPolicyManager.java
@@ -24,6 +24,7 @@ package com.microsoft.applicationinsights.internal.channel.common;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
