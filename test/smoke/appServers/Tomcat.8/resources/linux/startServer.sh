bea5cc83907c4e2bb6e8cc549022f4a9c770ca04
littleaj <1690572+littleaj@users.noreply.github.com>
2018-04-27 12:48:08 -0700

    updated tomcat scripts to use agent when active

--- test/smoke/appServers/Tomcat.8/resources/linux/startServer.sh
+++ test/smoke/appServers/Tomcat.8/resources/linux/startServer.sh
@@ -5,4 +5,9 @@ if [ -z "$CATALINA_HOME" ]; then
 	exit 1
 fi
 
+if [ ! -z "$AI_AGENT_MODE" ]; then
+    cp -f ./setenv.sh $CATALINA_HOME/bin/setenv.sh
+    cp -f ./${AI_AGENT_MODE}_AI-Agent.xml ./aiagent/AI-Agent.xml
+fi
+
 $CATALINA_HOME/bin/catalina.sh run
