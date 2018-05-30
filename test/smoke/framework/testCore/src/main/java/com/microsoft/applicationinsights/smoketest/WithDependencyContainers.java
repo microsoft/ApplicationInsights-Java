c5b0c662ced40403c8e88f25a28d7db8fd44a2b2
Arthur Little <1690572+littleaj@users.noreply.github.com>
2018-05-30 13:34:06 -0700

    Smoketest support for dependency containers (#666)
    * added annotations to define dependency containersbegan implementing a test for testing the dependnecy container feature
    
    * finished dependency container orchestration.
    needed portMapping for dependency containers.
    added orchestration for docker network.
    
    * changed name of testapp container
    
    * reverted something that snuck in
    
    * added unique names to dependency containersadded more logs for network creation/deletionusing Deque instead of older Stack
    
    * changed name of testapp container; needs to be unique for the entire test run
    
    * added unique container id incremented after each container starts.
    This should have no conflicts.
    
    * using utility method and fixed log message that would've printed garbage.
    
    * instead of trying to make a unique name, let docker handling container naming.
    
    * added instructions for using dependency containers

---
+++ test/smoke/framework/testCore/src/main/java/com/microsoft/applicationinsights/smoketest/WithDependencyContainers.java
@@ -0,0 +1,12 @@
+package com.microsoft.applicationinsights.smoketest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface WithDependencyContainers {
+    DependencyContainer[] value();
+}
