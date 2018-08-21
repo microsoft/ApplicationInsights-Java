4c63e07b7ce433053eedec4136f009f33d1b1705
Arthur Little <1690572+littleaj@users.noreply.github.com>
2018-08-20 18:26:47 -0700

    Added javadoc and rewrote assert in smoketests (#713)
    * adjusted assert to make tests more specific and less likely to fail.
    
    * added comments about agent and dependency related annotations

--- test/smoke/framework/testCore/src/main/java/com/microsoft/applicationinsights/smoketest/WithDependencyContainers.java
+++ test/smoke/framework/testCore/src/main/java/com/microsoft/applicationinsights/smoketest/WithDependencyContainers.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicated that the annotated class should also start the listed containers on the same Docker network to be used
+ * as test app dependencies. Dependency contianers are started before the test application in the same order listed
+ * in this annotation.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface WithDependencyContainers {
