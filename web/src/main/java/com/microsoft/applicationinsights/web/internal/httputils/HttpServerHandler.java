af88cdd615de96ba787ddfb2637feb4f3efcf44e
dhaval24 <dhavaldoshi24@gmail.com>
2019-03-03 16:14:34 -0800

    Refactor handleEnd() and tests

--- web/src/main/java/com/microsoft/applicationinsights/web/internal/httputils/HttpServerHandler.java
+++ web/src/main/java/com/microsoft/applicationinsights/web/internal/httputils/HttpServerHandler.java
@@ -3,19 +3,20 @@ package com.microsoft.applicationinsights.web.internal.httputils;
 import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.common.CommonUtils;
 import com.microsoft.applicationinsights.internal.logger.InternalLogger;
+import com.microsoft.applicationinsights.internal.util.ThreadLocalCleaner;
 import com.microsoft.applicationinsights.telemetry.Duration;
 import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
 import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
 import com.microsoft.applicationinsights.web.internal.ThreadContext;
 import com.microsoft.applicationinsights.web.internal.WebModulesContainer;
+import java.net.MalformedURLException;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.Validate;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.http.annotation.Experimental;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.net.MalformedURLException;
-import java.util.Date;
 
 /**
  * This Helper Handler class provides the required methods to instrument requests.
@@ -42,6 +43,11 @@ public final class HttpServerHandler<P /* >>> extends @NonNull Object */, Q> {
     private final TelemetryClient telemetryClient;
 
     /**
+     * ThreadLocal Cleaners for Agent connector
+     */
+    private final List<ThreadLocalCleaner> cleaners;
+
+    /**
      * Creates a new instance of {@link HttpServerHandler}
      *
      * @param extractor The {@code HttpExtractor} used to extract information from request and repsonse
@@ -51,11 +57,14 @@ public final class HttpServerHandler<P /* >>> extends @NonNull Object */, Q> {
      */
     public HttpServerHandler(HttpExtractor<P, Q> extractor,
         WebModulesContainer<P, Q> webModulesContainer,
+        List<ThreadLocalCleaner> cleaners,
         /* Nullable */ TelemetryClient telemetryClient) {
         Validate.notNull(extractor, "extractor");
         Validate.notNull(webModulesContainer, "WebModuleContainer");
+        Validate.notNull(cleaners, "ThreadLocalCleaners");
         this.extractor = extractor;
         this.webModulesContainer = webModulesContainer;
+        this.cleaners = cleaners;
         this.telemetryClient = telemetryClient;
     }
 
@@ -69,6 +78,8 @@ public final class HttpServerHandler<P /* >>> extends @NonNull Object */, Q> {
      */
     public RequestTelemetryContext handleStart(P request, Q response) throws MalformedURLException {
         RequestTelemetryContext context = new RequestTelemetryContext(new Date().getTime(),null);
+        // TODO: uncomment this line in final integration
+        //webModulesContainer.setRequestTelemetryContext(context);
         RequestTelemetry requestTelemetry = context.getHttpRequestTelemetry();
         ThreadContext.setRequestTelemetryContext(context);
         String method = extractor.getMethod(request);
@@ -94,9 +105,10 @@ public final class HttpServerHandler<P /* >>> extends @NonNull Object */, Q> {
      * This method is used to indicate request end instrumentation, complete correlation and record timing, response
      * @param request HttpRequest object
      * @param response HttpResponse object
+     * @param context RequestTelemetryContext object
      */
-    public void handleEnd(P request, Q response) {
-        RequestTelemetryContext context = ThreadContext.getRequestTelemetryContext();
+    public void handleEnd(P request, Q response,
+        RequestTelemetryContext context) {
         RequestTelemetry requestTelemetry = context.getHttpRequestTelemetry();
         long endTime = new Date().getTime();
         requestTelemetry.setDuration(new Duration(endTime - context.getRequestStartTimeTicks()));
@@ -104,6 +116,7 @@ public final class HttpServerHandler<P /* >>> extends @NonNull Object */, Q> {
         requestTelemetry.setSuccess(resultCode < 400);
         requestTelemetry.setResponseCode(Integer.toString(resultCode));
         webModulesContainer.invokeOnEndRequest(request, response);
+        cleanup();
     }
 
     /**
@@ -119,6 +132,21 @@ public final class HttpServerHandler<P /* >>> extends @NonNull Object */, Q> {
             }
         } catch (Exception ex) {
             // swallow AI Exception
+        }
+    }
+
+    /**
+     * Remove data from Threadlocal and ThreadLocalCleaners
+     */
+    private void cleanup() {
+        try {
+            ThreadContext.remove();
+            for (ThreadLocalCleaner cleaner : cleaners) {
+                cleaner.clean();
+            }
+        } catch (Exception t) {
+            InternalLogger.INSTANCE.warn(String.format("unable to perform TLS Cleaning: %s",
+                ExceptionUtils.getStackTrace(t)));
         }
     }
 }
