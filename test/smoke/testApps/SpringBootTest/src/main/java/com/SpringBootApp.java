ca1271b86c8b114a7a483593489fae32d4921ad8
dhaval24 <dhavaldoshi24@gmail.com>
2019-03-06 23:29:58 -0800

    disable springboot error handling

--- test/smoke/testApps/SpringBootTest/src/main/java/com/SpringBootApp.java
+++ test/smoke/testApps/SpringBootTest/src/main/java/com/SpringBootApp.java
@@ -15,6 +15,14 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class SpringBootApp extends SpringBootServletInitializer {
 
+	public SpringBootApp() {
+		super();
+
+		// This lets tomcat handle error and hence filter catches exception.
+		// Disables Springboot error handling which prevents response from propagating up.
+		// See: https://github.com/spring-projects/spring-boot/commit/6381a07c71310c56dc29cf99709adf5fe6e6406a
+		setRegisterErrorPageFilter(false);
+	}
 	@Override
 	protected  SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
 		return applicationBuilder.sources(SpringBootApp.class);
