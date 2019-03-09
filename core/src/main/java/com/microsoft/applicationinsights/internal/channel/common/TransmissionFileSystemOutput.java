e9afa9ba9d11641cb5f775a39e52e0f75bf8579a
littleaj <1690572+littleaj@users.noreply.github.com>
2019-03-08 18:20:20 -0800

    convert to try with resources block. removed unneeded stream variables. parent decorators close children

--- core/src/main/java/com/microsoft/applicationinsights/internal/channel/common/TransmissionFileSystemOutput.java
+++ core/src/main/java/com/microsoft/applicationinsights/internal/channel/common/TransmissionFileSystemOutput.java
@@ -245,16 +245,10 @@ public final class TransmissionFileSystemOutput implements TransmissionOutput {
     private Optional<Transmission> loadTransmission(File file) {
         Transmission transmission = null;
 
-        InputStream fileInput = null;
-        ObjectInput input = null;
-        try {
-            if (file == null) {
-                return Optional.absent();
-            }
-
-            fileInput = new FileInputStream(file);
-            InputStream buffer = new BufferedInputStream(fileInput);
-            input = new ObjectInputStream (buffer);
+        if (file == null) {
+            return Optional.absent();
+        }
+        try (ObjectInput input = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
             transmission = (Transmission)input.readObject();
         } catch (FileNotFoundException e) {
             InternalLogger.INSTANCE.error("Failed to load transmission, file not found, exception: %s", e.toString());
@@ -262,13 +256,6 @@ public final class TransmissionFileSystemOutput implements TransmissionOutput {
             InternalLogger.INSTANCE.error("Failed to load transmission, non transmission, exception: %s", e.toString());
         } catch (IOException e) {
             InternalLogger.INSTANCE.error("Failed to load transmission, io exception: %s", e.toString());
-        } finally{
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                }
-            }
         }
 
         return Optional.fromNullable(transmission);
