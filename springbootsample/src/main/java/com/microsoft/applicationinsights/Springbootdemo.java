1361d8bb731c3b622fcfc27a99c168aca2370a4b
dhaval24 <dhavaldoshi24@gmail.com>
2019-03-25 16:07:36 -0700

    Add Springboot sample app

---
+++ springbootsample/src/main/java/com/microsoft/applicationinsights/Springbootdemo.java
@@ -0,0 +1,11 @@
+package com.microsoft.applicationinsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemo {
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemo.class, args);
+    }
+}
