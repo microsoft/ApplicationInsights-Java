a8ca79da0ee5c9ee0ab6947908272026e4d8e76c
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-05-06 18:51:50 -0700

    Support for debugging smoke tests on tomcat 8.5

--- test/smoke/appServers/Tomcat.8.5/resources/linux/startServer.sh
+++ test/smoke/appServers/Tomcat.8.5/resources/linux/startServer.sh
@@ -6,9 +6,13 @@ if [ -z "$CATALINA_HOME" ]; then
 fi
 
 if [ ! -z "$AI_AGENT_MODE" ]; then
+
     echo "AI_AGENT_MODE=$AI_AGENT_MODE"
-    cp -f ./setenv.sh $CATALINA_HOME/bin/setenv.sh
     cp -f ./${AI_AGENT_MODE}_AI-Agent.xml ./aiagent/AI-Agent.xml
+
+    export CATALINA_OPTS="-javaagent:/root/docker-stage/aiagent/$AGENT_JAR_NAME $CATALINA_OPTS"
 fi
+
+export CATALINA_OPTS="-Xdebug -agentlib:jdwp=transport=dt_socket,address=5005,server=y,suspend=n $CATALINA_OPTS"
 
 $CATALINA_HOME/bin/catalina.sh run
