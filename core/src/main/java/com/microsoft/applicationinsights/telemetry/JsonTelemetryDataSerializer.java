e9bd42096914525ef829012a1f6186f257126d2d
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-05-11 17:00:22 -0700

    Normalize exception measurement json data
    So that serialized exception data is the same whether or not
    getMeasurements() has been called (to help make data reported during
    tests more consistent).

--- core/src/main/java/com/microsoft/applicationinsights/telemetry/JsonTelemetryDataSerializer.java
+++ core/src/main/java/com/microsoft/applicationinsights/telemetry/JsonTelemetryDataSerializer.java
@@ -243,26 +243,22 @@ public final class JsonTelemetryDataSerializer {
 
     public <T> void write(String name, Map<String, T> map) throws IOException {
 
-        if (map == null) {
+        if (map == null || map.isEmpty()) {
             return;
         }
 
         writeName(name);
         try {
-            if (map.size() < 1) {
-                out.write("null");
-            } else {
-                out.write(JSON_START_OBJECT);
+            out.write(JSON_START_OBJECT);
 
-                separator = "";
-                for (Map.Entry<String, T> entry : map.entrySet()) {
-                    writeName(sanitizeKey(entry.getKey()));
-                    write(entry.getValue());
-                    separator = JSON_SEPARATOR;
-                }
-
-                out.write(JSON_CLOSE_OBJECT);
+            separator = "";
+            for (Map.Entry<String, T> entry : map.entrySet()) {
+                writeName(sanitizeKey(entry.getKey()));
+                write(entry.getValue());
+                separator = JSON_SEPARATOR;
             }
+
+            out.write(JSON_CLOSE_OBJECT);
         } finally {
             separator = JSON_SEPARATOR;
         }
