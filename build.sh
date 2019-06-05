acfd5366ce487e61f1b65d4ffe01fe78d360bc40
Max Golovanov <maxgolov@microsoft.com>
2019-06-05 02:01:13 -0700

    Build options

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+export DOCKER_EXE=`which docker`
 export PATH=.:$PATH
-gradlew --info build jar --stacktrace
+#gradlew --info clean build jar --stacktrace
+#gradlew --info build jar --stacktrace -DDEBUG=true run
+gradlew -DDEBUG=true smokeTest
