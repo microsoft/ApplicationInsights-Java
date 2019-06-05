eb160b84ee844a51e1fea86b2a7866642e93c2a4
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-06-05 12:33:57 -0700

    Fix warning messages from Spring

--- test/smoke/testApps/SpringBootTest/src/main/java/com/springbootstartertest/controller/TestBean.java
+++ test/smoke/testApps/SpringBootTest/src/main/java/com/springbootstartertest/controller/TestBean.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.Future;
 
 import com.google.common.io.ByteStreams;
 import org.apache.commons.httpclient.Cookie;
@@ -25,7 +26,7 @@ public class TestBean {
     private CloseableHttpClient httpClient = HttpClientBuilder.create().disableAutomaticRetries().build();
 
     @Async
-    public AsyncResult<Integer> asyncDependencyCallWithApacheHttpClient4() throws IOException {
+    public Future<Integer> asyncDependencyCallWithApacheHttpClient4() throws IOException {
         String url = "https://www.bing.com";
         HttpGet get = new HttpGet(url);
         try (CloseableHttpResponse response = httpClient.execute(get)) {
@@ -34,7 +35,7 @@ public class TestBean {
     }
 
     @Async
-    public AsyncResult<Integer> asyncDependencyCallWithApacheHttpClient3() throws IOException {
+    public Future<Integer> asyncDependencyCallWithApacheHttpClient3() throws IOException {
         HttpClient httpClient3 = new org.apache.commons.httpclient.HttpClient();
         CookiePolicy.registerCookieSpec("PermitAllCookiesSpec", PermitAllCookiesSpec.class);
         httpClient3.getParams().setCookiePolicy("PermitAllCookiesSpec");
@@ -46,7 +47,7 @@ public class TestBean {
     }
 
     @Async
-    public AsyncResult<Integer> asyncDependencyCallWithOkHttp3() throws IOException {
+    public Future<Integer> asyncDependencyCallWithOkHttp3() throws IOException {
         okhttp3.OkHttpClient client = new okhttp3.OkHttpClient();
         okhttp3.Request request = new okhttp3.Request.Builder()
                 .url("https://www.bing.com")
@@ -57,7 +58,7 @@ public class TestBean {
     }
 
     @Async
-    public AsyncResult<Integer> asyncDependencyCallWithOkHttp2() throws IOException {
+    public Future<Integer> asyncDependencyCallWithOkHttp2() throws IOException {
         com.squareup.okhttp.OkHttpClient client = new com.squareup.okhttp.OkHttpClient();
         com.squareup.okhttp.Request request = new com.squareup.okhttp.Request.Builder()
                 .url("https://www.bing.com")
@@ -68,7 +69,7 @@ public class TestBean {
     }
 
     @Async
-    public AsyncResult<Integer> asyncDependencyCallWithHttpURLConnection() throws IOException {
+    public Future<Integer> asyncDependencyCallWithHttpURLConnection() throws IOException {
         URL obj = new URL("https://www.bing.com");
         HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
         if (connection.getResponseCode() != 200) {
