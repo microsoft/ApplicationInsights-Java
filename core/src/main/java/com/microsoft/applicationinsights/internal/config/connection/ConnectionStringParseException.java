f5634df0fea150fab578e9cb460024c74773ce68
littleaj <1690572+littleaj@users.noreply.github.com>
2019-08-02 14:38:10 -0700

    removed unused constructors

--- core/src/main/java/com/microsoft/applicationinsights/internal/config/connection/ConnectionStringParseException.java
+++ core/src/main/java/com/microsoft/applicationinsights/internal/config/connection/ConnectionStringParseException.java
@@ -1,22 +1,13 @@
 package com.microsoft.applicationinsights.internal.config.connection;
 
 public abstract class ConnectionStringParseException extends Exception {
-    public ConnectionStringParseException() {
-    }
 
-    public ConnectionStringParseException(String message) {
+    ConnectionStringParseException(String message) {
         super(message);
     }
 
-    public ConnectionStringParseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ConnectionStringParseException(Throwable cause) {
+    ConnectionStringParseException(Throwable cause) {
         super(cause);
     }
 
-    public ConnectionStringParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
--- core/src/main/java/com/microsoft/applicationinsights/internal/config/connection/InvalidConnectionStringException.java
+++ core/src/main/java/com/microsoft/applicationinsights/internal/config/connection/InvalidConnectionStringException.java
@@ -1,22 +1,13 @@
 package com.microsoft.applicationinsights.internal.config.connection;
 
 public class InvalidConnectionStringException extends ConnectionStringParseException {
-    public InvalidConnectionStringException() {
-    }
 
-    public InvalidConnectionStringException(String message) {
+    InvalidConnectionStringException(String message) {
         super(message);
     }
 
-    public InvalidConnectionStringException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidConnectionStringException(Throwable cause) {
+    InvalidConnectionStringException(Throwable cause) {
         super(cause);
     }
 
-    public InvalidConnectionStringException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
