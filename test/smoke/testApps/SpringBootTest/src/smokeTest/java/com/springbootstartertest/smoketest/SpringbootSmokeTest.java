9c8d788e1bded586d66d6b3a813311c9e661a812
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-11-01 21:12:11 -0700

    Fix target field

--- test/smoke/testApps/SpringBootTest/src/smokeTest/java/com/springbootstartertest/smoketest/SpringbootSmokeTest.java
+++ test/smoke/testApps/SpringBootTest/src/smokeTest/java/com/springbootstartertest/smoketest/SpringbootSmokeTest.java
@@ -113,7 +113,7 @@ public class SpringbootSmokeTest extends AiSmokeTest {
         RemoteDependencyData rdd = (RemoteDependencyData) ((Data) rddEnvelope.getData()).getBaseData();
 
         assertEquals("GET /", rdd.getName());
-        assertEquals("www.bing.com:-1 | www.bing.com", rdd.getTarget());
+        assertEquals("www.bing.com", rdd.getTarget());
 
         assertTrue(rdd.getId().contains(d.getId()));
         assertSameOperationId(rdEnvelope, rddEnvelope);
