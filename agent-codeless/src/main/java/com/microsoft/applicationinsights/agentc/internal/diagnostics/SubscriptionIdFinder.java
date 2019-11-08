237131a7eb48a702a8202c1fc91a0ea629493f80
littleaj <1690572+littleaj@users.noreply.github.com>
2019-11-08 12:11:14 -0800

    add test for subid finder

--- agent-codeless/src/main/java/com/microsoft/applicationinsights/agentc/internal/diagnostics/SubscriptionIdFinder.java
+++ agent-codeless/src/main/java/com/microsoft/applicationinsights/agentc/internal/diagnostics/SubscriptionIdFinder.java
@@ -3,11 +3,13 @@ package com.microsoft.applicationinsights.agentc.internal.diagnostics;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
 
 public class SubscriptionIdFinder extends CachedDiagnosticsValueFinder {
 
-    private static final String WEBSITE_OWNER_NAME_ENV_VAR = "WEBSITE_OWNER_NAME";
+    @VisibleForTesting
+    static final String WEBSITE_OWNER_NAME_ENV_VAR = "WEBSITE_OWNER_NAME";
 
     @Nullable
     @Override
