b4af752fb5a4fdf04ebcdac1e3521c1edb90c8aa
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-04-09 21:36:51 -0700

    add devtest artifact for use with etw-testapp. updated test controller to use loggerProxy. added controller for long tests.

--- etw/etw-testapp/src/main/java/com/microsoft/applicationinsights/etw_testapp/EtwTestApp.java
+++ etw/etw-testapp/src/main/java/com/microsoft/applicationinsights/etw_testapp/EtwTestApp.java
@@ -4,10 +4,16 @@
 package com.microsoft.applicationinsights.etw_testapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.task.TaskSchedulerBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class EtwTestApp extends SpringBootServletInitializer {
 
     public EtwTestApp() {
@@ -17,5 +23,10 @@ public class EtwTestApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EtwTestApp.class, args);
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new TaskSchedulerBuilder(1, "ewt-testapp-worker", null).build();
     }
 }
--- etw/etw-testapp/src/main/java/com/microsoft/applicationinsights/etw_testapp/EtwTestController.java
+++ etw/etw-testapp/src/main/java/com/microsoft/applicationinsights/etw_testapp/EtwTestController.java
@@ -1,29 +1,57 @@
 package com.microsoft.applicationinsights.etw_testapp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.microsoft.applicationinsights.agentc.internal.diagnostics.etw.DiagnosticsLoggerProxy;
 
 @RestController
 public class EtwTestController {
+    private static final DiagnosticsLoggerProxy DIAGNOSTICS_LOGGER = new DiagnosticsLoggerProxy();
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EtwTestController.class);
-    private static final Logger DIAGNOSTICS_LOGGER = LoggerFactory.getLogger("applicationinsights.extension.diagnostics");
+    private final AtomicInteger errorCount = new AtomicInteger();
+    private final AtomicInteger warnCount = new AtomicInteger();
+    private final AtomicInteger infoCount = new AtomicInteger();
 
-    public EtwTestController() {
-        info("Loading EtwTestController");
+    @GetMapping("/{level}")
+    public ResponseEntity<String> logPage(
+            @PathVariable String level,
+            @RequestParam(name = "e", required = false, defaultValue = "false") boolean hasException)
+    {
+        String msg = "Hit /" + level + " ";
+        int n = -1;
+        Throwable t = null;
+        switch (level.toLowerCase()) {
+            case "info":
+                n = infoCount.incrementAndGet();
+                DIAGNOSTICS_LOGGER.info(msg + n);
+                break;
+            case "error":
+                n = errorCount.incrementAndGet();
+                if (hasException) {
+                    t = new Exception("the error " + n);
+                    DIAGNOSTICS_LOGGER.error(msg + n, t);
+                } else {
+                    DIAGNOSTICS_LOGGER.error(msg + n);
+                }
+                break;
+            case "warn":
+                n = warnCount.incrementAndGet();
+                if (hasException) {
+                    t = new Exception("the warn " + n);
+                    DIAGNOSTICS_LOGGER.warn(msg + n, t);
+                } else {
+                    DIAGNOSTICS_LOGGER.warn(msg + n);
+                }
+                break;
+            default:
+                return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(level.toUpperCase() + " " + n + (t == null ? "" : "<br/>\n"+t.toString()));
     }
-
-    @GetMapping("/")
-    public String rootPage() {
-        info("Hit EtwTestController.rootPage");
-        return "ROOT";
-    }
-
-    private static void info(String message) {
-        LOGGER.info(message);
-        DIAGNOSTICS_LOGGER.info(message);
-    }
-
 }
