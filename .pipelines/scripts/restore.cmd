99ab66b0faf9306c458385e173c19039807792bb
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-06-08 16:03:07 -0700

    added missing version/environment sections fixed exit command option added tag for buddy builds

--- .pipelines/scripts/restore.cmd
+++ .pipelines/scripts/restore.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo "Installing Java..."
-powershell -file "%~dp0install-java.ps1" || exit \b 1
+powershell -file "%~dp0install-java.ps1" || exit /b %ERRORLEVEL%
 
 echo "C:\Program Files\Java\zulu-11-azure-jdk_11.39.15-11.0.7-win_x64"
 dir "C:\Program Files\Java\zulu-11-azure-jdk_11.39.15-11.0.7-win_x64"
