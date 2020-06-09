32e9b051be28d00c2b8bc089c3ac5cf5d557d2ff
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-06-08 21:38:09 -0700

    update directory in scripts

--- .scripts/test.cmd
+++ .scripts/test.cmd
@@ -1,2 +1,2 @@
-@cd "%~dp0"
-call ..\gradlew.bat :core:check --info
+@cd "%~dp0.."
+call gradlew.bat :core:check --info
--- .scripts/version.cmd
+++ .scripts/version.cmd
@@ -1,5 +1,6 @@
 
-echo Generating version properties...
+@echo Generating version properties...
+@cd "%~dp0.."
 call gradlew.bat :core:generateVersionProperties || exit /B 1
 
 powershell.exe -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0GenerateVersion.ps1"
