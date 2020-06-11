02e7be5f0a8097772153e50131143c1240d2e772
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-06-10 17:31:50 -0700

    skipWinNative for for now. add installJava7 script

--- .scripts/installjava.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo "Installing Java..."
-powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-java.ps1" || exit /B 1
-
-echo "C:\Program Files\Java\zulu-11-azure-jdk_11.39.15-11.0.7-win_x64"
-dir "C:\Program Files\Java\zulu-11-azure-jdk_11.39.15-11.0.7-win_x64"
-
-echo "Installed Java Successfully."
