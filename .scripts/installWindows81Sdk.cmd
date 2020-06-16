f762ddac2da4c34d599ad487539d0ef43f87c3d5
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-06-16 14:37:30 -0700

    fix options for win sdk installer set env var with win sdk installation directory

--- .scripts/installWindows81Sdk.cmd
+++ .scripts/installWindows81Sdk.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 echo "Installing Windows 8.1 SDK..."
-
-powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0Install-Something.ps1" -Install -Url "http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe" -InstallerArgs "/q","/norestart","/features","+" -CleanOnFinish || exit /B 1
+::todo /log to set logs directory
+powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0Install-Something.ps1" -Install -Url "http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe" -InstallerArgs "/features OptionId.WindowsDesktopSoftwareDevelopmentKit OptionId.WindowsPerformanceToolkit OptionId.NetFxSoftwareDevelopmentKit /norestart /q" -InstallationDirectory "C:\Program Files (x86)\Windows Kits\8.1" -HomeVar "APPINSIGHTS_WIN_SDK_PATH" -CleanOnFinish || exit /B 1
 
 echo "C:\Program Files (x86)\Windows Kits\8.1"
 dir "C:\Program Files (x86)\Windows Kits\8.1"
