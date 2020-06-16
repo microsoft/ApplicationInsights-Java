bdb814047e426bf9c61af3becde4222930d5b6a2
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-06-16 14:56:17 -0700

    set logs dir for installer script

--- .scripts/installWindows81Sdk.cmd
+++ .scripts/installWindows81Sdk.cmd
@@ -1,10 +1,13 @@
 @echo off
 
 echo "Installing Windows 8.1 SDK..."
-::todo /log to set logs directory
-powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0Install-Something.ps1" -Install -Url "http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe" -InstallerArgs "/features OptionId.WindowsDesktopSoftwareDevelopmentKit OptionId.WindowsPerformanceToolkit OptionId.NetFxSoftwareDevelopmentKit /norestart /q" -InstallationDirectory "C:\Program Files (x86)\Windows Kits\8.1" -HomeVar "APPINSIGHTS_WIN_SDK_PATH" -CleanOnFinish || exit /B 1
+
+powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0Install-Something.ps1" -Install -Url "http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe" -InstallerArgs "/features OptionId.WindowsDesktopSoftwareDevelopmentKit OptionId.WindowsPerformanceToolkit OptionId.NetFxSoftwareDevelopmentKit /log %CDP_USER_SOURCE_FOLDER_CONTAINER_ACCESS_PATH%\AIJ-BuildLogs\Restore\WinSdk\winsdkinstall.log /norestart /q" -InstallationDirectory "C:\Program Files (x86)\Windows Kits\8.1" -HomeVar "APPINSIGHTS_WIN_SDK_PATH" -CleanOnFinish || exit /B 1
 
 echo "C:\Program Files (x86)\Windows Kits\8.1"
 dir "C:\Program Files (x86)\Windows Kits\8.1"
+
+echo "%CDP_USER_SOURCE_FOLDER_CONTAINER_ACCESS_PATH%\AIJ-BuildLogs\WinSdk"
+dir "%CDP_USER_SOURCE_FOLDER_CONTAINER_ACCESS_PATH%\AIJ-BuildLogs\WinSdk"
 
 echo "Windows 8.1 SDK Installed Successfully"
