5b2f0f7f8a98ced4e2a2c7ab6681a862aaa18c75
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-07-15 20:19:20 -0700

    include --info in default gradle options

--- .scripts/gradle.cmd
+++ .scripts/gradle.cmd
@@ -8,8 +8,8 @@ popd
 pushd %~dp0..
 set PROJECT_ROOT=%CD%
 
-set DEFAULT_OPTIONS=--stacktrace -DisBuildServer=true
-set GRADLE_CMD=gradlew.bat %* %DEFAULT_OPTIONS%
+set DEFAULT_OPTIONS=--info --stacktrace -DisBuildServer=true
+set GRADLE_CMD=gradlew.bat %DEFAULT_OPTIONS% %*
 echo Running '%GRADLE_CMD%' in '%PROJECT_ROOT%'
 call %GRADLE_CMD%
 if errorlevel 1 (
