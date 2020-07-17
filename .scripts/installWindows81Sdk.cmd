6089f621ac1893fadd56270f6bf2d17d9c7056fa
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-07-16 18:23:03 -0700

    change root path for winsdk install logs

--- .scripts/installWindows81Sdk.cmd
+++ .scripts/installWindows81Sdk.cmd
@@ -15,12 +15,12 @@ echo "Installing Windows 8.1 SDK..."
 ::    OptionId.MSIInstallTools
 ::  * OptionId.Netfx
 
-powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0Install-Something.ps1" -Install -Url "http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe" -InstallerArgs "/features OptionId.WindowsDesktopSoftwareDevelopmentKit OptionId.WindowsPerformanceToolkit OptionId.NetFxSoftwareDevelopmentKit /log %CDP_USER_SOURCE_FOLDER_CONTAINER_ACCESS_PATH%\AIJ-BuildLogs\Restore\WinSdk\winsdkinstall.log /norestart /q" -InstallationDirectory "C:\Program Files (x86)\Windows Kits\8.1" -HomeVar "APPINSIGHTS_WIN_SDK_PATH" -CleanOnFinish || exit /B 1
+powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0Install-Something.ps1" -Install -Url "http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe" -InstallerArgs "/features OptionId.WindowsDesktopSoftwareDevelopmentKit OptionId.WindowsPerformanceToolkit OptionId.NetFxSoftwareDevelopmentKit /log %CDP_USER_SOURCE_FOLDER_CONTAINER_PATH%\WinSdkInstall\winsdkinstall.log /norestart /q" -InstallationDirectory "C:\Program Files (x86)\Windows Kits\8.1" -HomeVar "APPINSIGHTS_WIN_SDK_PATH" -CleanOnFinish || exit /B 1
 
 echo "C:\Program Files (x86)\Windows Kits\8.1"
 dir "C:\Program Files (x86)\Windows Kits\8.1"
 
-echo "%CDP_USER_SOURCE_FOLDER_CONTAINER_ACCESS_PATH%\AIJ-BuildLogs\WinSdk"
-dir "%CDP_USER_SOURCE_FOLDER_CONTAINER_ACCESS_PATH%\AIJ-BuildLogs\WinSdk"
+echo "%CDP_USER_SOURCE_FOLDER_CONTAINER_PATH%\WinSdkInstall"
+dir "%CDP_USER_SOURCE_FOLDER_CONTAINER_PATH%\WinSdkInstall"
 
 echo "Windows 8.1 SDK Installed Successfully"
