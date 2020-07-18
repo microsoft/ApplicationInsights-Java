bfbf7aaa7b2f63f8e19f35438d14c11b8b0c658b
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-07-18 13:18:38 -0700

    Remove unused InternalLogger

--- core/src/test/java/com/microsoft/applicationinsights/internal/logger/LoggerTestHelper.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.applicationinsights.internal.logger;
-
-public class LoggerTestHelper {
-    public static void resetInternalLogger() {
-        InternalLogger.INSTANCE.reset();
-    }
-}
