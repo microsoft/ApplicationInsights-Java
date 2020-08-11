3e949ccbfac9ce2262825af1f3acf5114e8fb647
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-08-11 09:56:02 -0700

    rework collectd script; simplifying and fixing directory issues

--- .scripts/collectd-init.sh
+++ .scripts/collectd-init.sh
@@ -2,32 +2,45 @@
 
 set -e
 
+
+
 sudo apt-get update -y
 
-sudo apt-get install -y gnupg
-sudo apt-get install -y software-properties-common
+sudo apt-get install -y gnupg software-properties-common
+
+sudo apt-add-repository 'deb http://archive.ubuntu.com/ubuntu/ eoan main restricted universe'
+sudo apt-add-repository 'deb http://security.ubuntu.com/ubuntu/ eoan-security main restricted universe'
+echo "Package: *
+Pin: release n=bionic
+Pin-Priority: -10
+
+Package: automake
+Pin: release n=eoan
+Pin-Priority: 500" > /etc/apt/preferences.d/myprefs.pref
+
+echo "--------------------"
+echo "Updated preferences:"
+cat /etc/apt/preferences.d/myprefs.pref
+echo "--------------------"
+
+sudo apt-get install -y automake autoconf gcc make wget pkg-config 
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
 sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
-
-sudo apt-get install -y autoconf
-sudo apt-get install -y automake
 sudo apt-get install -y zulu-8
-sudo apt-get install -y gcc
-sudo apt-get install -y make
-
 
 export JAVA_HOME=/usr/lib/jvm/zulu-8-amd64
 
 ls -FAl $JAVA_HOME
 
-ROOT_DIR="$(pwd)"
-export COLLECTD_HOME=$ROOT_DIR/collectd
+mkdir ./collectd-stage
+pushd collectd-stage
+export COLLECTD_HOME="$(pwd)"
 
 wget https://github.com/collectd/collectd/releases/download/collectd-5.11.0/collectd-5.11.0.tar.bz2
-tar -xvf collectd-5.11.0.tar.bz2 --directory $COLLECTD_HOME
+tar -xvf collectd-5.11.0.tar.bz2
 
-pushd $COLLECTD_HOME/collectd-5.11.0
+pushd collectd-5.11.0
 
 autoconf -v
 ./configure --with-java="$JAVA_HOME" --disable-dependency-tracking
@@ -42,4 +55,5 @@ cp ./collectd-api.jar $COLLECTD_LIB
 ls -FAl $COLLECTD_HOME
 ls -FAl $COLLECTD_LIB
 
+popd
 popd
