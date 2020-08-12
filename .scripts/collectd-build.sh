2079edc75a054f464f157daacb3461ec5fdbcf3e
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-08-12 11:49:37 -0700

    use specific paths for gradlew

--- .scripts/collectd-build.sh
+++ .scripts/collectd-build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
-pwd
-ls -FAl
-./gradlew --info --stacktrace -DisBuildServer=true --warning-mode=all :collectd:build
+
+if [ ! -f "$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH" ]; then
+    echo "CDP_USER_SOURCE_FOLDER_CONTAINER_PATH ('$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH') does not exist" > &2
+    exit 1
+fi
+
+GRALDEW_PATH="$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH/gradlew"
+if [ ! -f "$GRADLEW_PATH" ]; then
+    echo "GRADLEW_PATH ('') does not exist" > &2
+    exit 1
+fi
+
+$GRADLEW_PATH -p "$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH" --info --stacktrace -DisBuildServer=true --warning-mode=all :collectd:build
+
