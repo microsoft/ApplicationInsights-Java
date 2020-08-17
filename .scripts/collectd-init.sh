d12d66cf371263ebcdc3e1cae917da03784738ad
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-08-17 01:16:53 -0700

    resolve collectd dependencies in restore phase

--- .scripts/collectd-init.sh
+++ .scripts/collectd-init.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-
-
 sudo apt-get update -y
 
 sudo apt-get install -y gnupg software-properties-common
@@ -57,3 +55,16 @@ ls -FAl $COLLECTD_LIB
 
 popd
 popd
+
+if [ ! -d "$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH" ]; then
+    >&2 echo "CDP_USER_SOURCE_FOLDER_CONTAINER_PATH ('$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH') does not exist"
+    exit 1
+fi
+
+GRADLEW_PATH="$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH/gradlew"
+if [ ! -f "$GRADLEW_PATH" ]; then
+    >&2 echo "GRADLEW_PATH ('$GRADLEW_PATH') does not exist"
+    exit 1
+fi
+
+$GRADLEW_PATH -p "$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH" --info --stacktrace -DisBuildServer=true --warning-mode=all :collectd:clean :collectd:resolveDependencies
