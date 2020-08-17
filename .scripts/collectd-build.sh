68b8f1fabccd6fedabdb26ef63e0156b13fd1214
Arthur Little <1690572+littleaj@users.noreply.github.com>
2020-08-17 01:36:46 -0700

    set env vars in build script

--- .scripts/collectd-build.sh
+++ .scripts/collectd-build.sh
@@ -11,5 +11,14 @@ if [ ! -f "$GRADLEW_PATH" ]; then
     exit 1
 fi
 
+export JAVA_JRE_7=/usr/lib/jvm/zulu-7-amd64/jre
+export COLLECTD_HOME=/collectd-stage
+
+if [ ! -d "$COLLECTD_HOME" ]; then
+    >&2 echo "COLLECTD_HOME ('$COLLECTD_HOME') does not exist"
+    ls -FAl /
+    exit 1
+fi
+
 $GRADLEW_PATH -p "$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH" --info --stacktrace -DisBuildServer=true --warning-mode=all :collectd:build
 
