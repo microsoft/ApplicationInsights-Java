6924647bb26d17c02395ac55993e1c3ad3cdd7b4
Helen Yang <heya@microsoft.com>
2020-08-25 13:25:55 -0700

    Reformat code without google formatter enabled

--- test/smoke/testApps/JMS/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/JmsTest.java
+++ test/smoke/testApps/JMS/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/JmsTest.java
@@ -15,40 +15,40 @@ import static org.junit.Assert.*;
 @UseAgent
 public class JmsTest extends AiSmokeTest {
 
-  @Test
-  @TargetUri("/sendMessage")
-  public void doMostBasicTest() throws Exception {
-    List<Envelope> rdList = mockedIngestion.waitForItems("RequestData", 2);
-    List<Envelope> rddList = mockedIngestion.waitForItemsInRequest("RemoteDependencyData", 2);
+    @Test
+    @TargetUri("/sendMessage")
+    public void doMostBasicTest() throws Exception {
+        List<Envelope> rdList = mockedIngestion.waitForItems("RequestData", 2);
+        List<Envelope> rddList = mockedIngestion.waitForItemsInRequest("RemoteDependencyData", 2);
 
-    Envelope rdEnvelope1 = rdList.get(0);
-    Envelope rdEnvelope2 = rdList.get(1);
-    Envelope rddEnvelope1 = rddList.get(0);
-    Envelope rddEnvelope2 = rddList.get(1);
+        Envelope rdEnvelope1 = rdList.get(0);
+        Envelope rdEnvelope2 = rdList.get(1);
+        Envelope rddEnvelope1 = rddList.get(0);
+        Envelope rddEnvelope2 = rddList.get(1);
 
-    RequestData rd1 = (RequestData) ((Data) rdEnvelope1.getData()).getBaseData();
-    RequestData rd2 = (RequestData) ((Data) rdEnvelope2.getData()).getBaseData();
-    RemoteDependencyData rdd1 =
-        (RemoteDependencyData) ((Data) rddEnvelope1.getData()).getBaseData();
-    RemoteDependencyData rdd2 =
-        (RemoteDependencyData) ((Data) rddEnvelope2.getData()).getBaseData();
+        RequestData rd1 = (RequestData) ((Data) rdEnvelope1.getData()).getBaseData();
+        RequestData rd2 = (RequestData) ((Data) rdEnvelope2.getData()).getBaseData();
+        RemoteDependencyData rdd1 =
+                (RemoteDependencyData) ((Data) rddEnvelope1.getData()).getBaseData();
+        RemoteDependencyData rdd2 =
+                (RemoteDependencyData) ((Data) rddEnvelope2.getData()).getBaseData();
 
-    assertEquals("GET /sendMessage", rd1.getName());
-    assertEquals("HelloController.sendMessage", rdd2.getName());
-    assertEquals("queue/message", rdd1.getName());
-    assertEquals("queue/message", rd2.getName());
+        assertEquals("GET /sendMessage", rd1.getName());
+        assertEquals("HelloController.sendMessage", rdd2.getName());
+        assertEquals("queue/message", rdd1.getName());
+        assertEquals("queue/message", rd2.getName());
 
-    assertParentChild(rdd2.getId(), rdEnvelope1, rddEnvelope1);
-    assertParentChild(rdd1.getId(), rddEnvelope2, rdEnvelope2);
-  }
+        assertParentChild(rdd2.getId(), rdEnvelope1, rddEnvelope1);
+        assertParentChild(rdd1.getId(), rddEnvelope2, rdEnvelope2);
+    }
 
-  private static void assertParentChild(
-      String parentId, Envelope parentEnvelope, Envelope childEnvelope) {
-    String operationId = parentEnvelope.getTags().get("ai.operation.id");
+    private static void assertParentChild(
+            String parentId, Envelope parentEnvelope, Envelope childEnvelope) {
+        String operationId = parentEnvelope.getTags().get("ai.operation.id");
 
-    assertNotNull(operationId);
+        assertNotNull(operationId);
 
-    assertEquals(operationId, childEnvelope.getTags().get("ai.operation.id"));
-    assertEquals(parentId, childEnvelope.getTags().get("ai.operation.parentId"));
-  }
+        assertEquals(operationId, childEnvelope.getTags().get("ai.operation.id"));
+        assertEquals(parentId, childEnvelope.getTags().get("ai.operation.parentId"));
+    }
 }
--- test/smoke/testApps/SpringBootTest/src/smokeTest/java/com/springbootstartertest/smoketest/SpringbootSmokeTest.java
+++ test/smoke/testApps/SpringBootTest/src/smokeTest/java/com/springbootstartertest/smoketest/SpringbootSmokeTest.java
@@ -24,127 +24,127 @@ import static org.junit.Assert.*;
 @UseAgent
 public class SpringbootSmokeTest extends AiSmokeTest {
 
-  @Test
-  @TargetUri("/basic/trackEvent")
-  public void trackEvent() throws Exception {
-    mockedIngestion.waitForItems("RequestData", 1);
-    mockedIngestion.waitForItemsInRequest("EventData", 2);
+    @Test
+    @TargetUri("/basic/trackEvent")
+    public void trackEvent() throws Exception {
+        mockedIngestion.waitForItems("RequestData", 1);
+        mockedIngestion.waitForItemsInRequest("EventData", 2);
 
-    // TODO get event data envelope and verify value
-    final List<EventData> data = mockedIngestion.getTelemetryDataByTypeInRequest("EventData");
-    assertThat(
-        data,
-        hasItem(
-            new TypeSafeMatcher<EventData>() {
-              final String name = "EventDataTest";
-              Matcher<String> nameMatcher = Matchers.equalTo(name);
+        // TODO get event data envelope and verify value
+        final List<EventData> data = mockedIngestion.getTelemetryDataByTypeInRequest("EventData");
+        assertThat(
+                data,
+                hasItem(
+                        new TypeSafeMatcher<EventData>() {
+                            final String name = "EventDataTest";
+                            Matcher<String> nameMatcher = Matchers.equalTo(name);
 
-              @Override
-              protected boolean matchesSafely(EventData item) {
-                return nameMatcher.matches(item.getName());
-              }
+                            @Override
+                            protected boolean matchesSafely(EventData item) {
+                                return nameMatcher.matches(item.getName());
+                            }
 
-              @Override
-              public void describeTo(Description description) {
-                description.appendDescriptionOf(nameMatcher);
-              }
-            }));
+                            @Override
+                            public void describeTo(Description description) {
+                                description.appendDescriptionOf(nameMatcher);
+                            }
+                        }));
 
-    assertThat(
-        data,
-        hasItem(
-            new TypeSafeMatcher<EventData>() {
-              final String expectedKey = "key";
-              final String expectedName = "EventDataPropertyTest";
-              final String expectedPropertyValue = "value";
-              final Double expectedMetricValue = 1d;
-              Matcher<Map<? extends String, ? extends Double>> metricMatcher =
-                  Matchers.hasEntry(expectedKey, expectedMetricValue);
-              Matcher<Map<? extends String, ? extends String>> propertyMatcher =
-                  Matchers.hasEntry(expectedKey, expectedPropertyValue);
-              Matcher<String> nameMatcher = Matchers.equalTo(expectedName);
+        assertThat(
+                data,
+                hasItem(
+                        new TypeSafeMatcher<EventData>() {
+                            final String expectedKey = "key";
+                            final String expectedName = "EventDataPropertyTest";
+                            final String expectedPropertyValue = "value";
+                            final Double expectedMetricValue = 1d;
+                            Matcher<Map<? extends String, ? extends Double>> metricMatcher =
+                                    Matchers.hasEntry(expectedKey, expectedMetricValue);
+                            Matcher<Map<? extends String, ? extends String>> propertyMatcher =
+                                    Matchers.hasEntry(expectedKey, expectedPropertyValue);
+                            Matcher<String> nameMatcher = Matchers.equalTo(expectedName);
 
-              @Override
-              public void describeTo(Description description) {
-                description.appendDescriptionOf(nameMatcher);
-                description.appendDescriptionOf(propertyMatcher);
-                description.appendDescriptionOf(metricMatcher);
-              }
+                            @Override
+                            public void describeTo(Description description) {
+                                description.appendDescriptionOf(nameMatcher);
+                                description.appendDescriptionOf(propertyMatcher);
+                                description.appendDescriptionOf(metricMatcher);
+                            }
 
-              @Override
-              protected boolean matchesSafely(EventData item) {
-                return nameMatcher.matches(item.getName())
-                    && propertyMatcher.matches(item.getProperties())
-                    && metricMatcher.matches(item.getMeasurements());
-              }
-            }));
-  }
-
-  @Test
-  @TargetUri("/throwsException")
-  public void testResultCodeWhenRestControllerThrows() throws Exception {
-    List<Envelope> rdList = mockedIngestion.waitForItems("RequestData", 1);
-    List<Envelope> rddList = mockedIngestion.waitForItemsInRequest("RemoteDependencyData", 1);
-    List<Envelope> edList = mockedIngestion.waitForItemsInRequest("ExceptionData", 2);
-
-    Envelope rdEnvelope = rdList.get(0);
-    Envelope rddEnvelope = rddList.get(0);
-    Envelope edEnvelope1 = edList.get(0);
-
-    RequestData rd = getTelemetryDataForType(0, "RequestData");
-    RemoteDependencyData rdd = (RemoteDependencyData) ((Data) rddEnvelope.getData()).getBaseData();
-    System.out.println("Response code after exception: " + rd.getResponseCode());
-    int code = -123;
-    try {
-      code = Integer.parseInt(rd.getResponseCode());
-    } catch (NumberFormatException e) {
-      fail("Response code is not a number");
+                            @Override
+                            protected boolean matchesSafely(EventData item) {
+                                return nameMatcher.matches(item.getName())
+                                        && propertyMatcher.matches(item.getProperties())
+                                        && metricMatcher.matches(item.getMeasurements());
+                            }
+                        }));
     }
-    assertThat(code, greaterThanOrEqualTo(500));
 
-    assertParentChild(rdd.getId(), rdEnvelope, edEnvelope1);
-  }
+    @Test
+    @TargetUri("/throwsException")
+    public void testResultCodeWhenRestControllerThrows() throws Exception {
+        List<Envelope> rdList = mockedIngestion.waitForItems("RequestData", 1);
+        List<Envelope> rddList = mockedIngestion.waitForItemsInRequest("RemoteDependencyData", 1);
+        List<Envelope> edList = mockedIngestion.waitForItemsInRequest("ExceptionData", 2);
 
-  @Test
-  @TargetUri("/asyncDependencyCall")
-  public void testAsyncDependencyCall() throws Exception {
-    commonValidation();
-  }
+        Envelope rdEnvelope = rdList.get(0);
+        Envelope rddEnvelope = rddList.get(0);
+        Envelope edEnvelope1 = edList.get(0);
 
-  private static void commonValidation() throws Exception {
-    List<Envelope> rdList = mockedIngestion.waitForItems("RequestData", 1);
-    List<Envelope> rddList = mockedIngestion.waitForItemsInRequest("RemoteDependencyData", 3);
+        RequestData rd = getTelemetryDataForType(0, "RequestData");
+        RemoteDependencyData rdd = (RemoteDependencyData) ((Data) rddEnvelope.getData()).getBaseData();
+        System.out.println("Response code after exception: " + rd.getResponseCode());
+        int code = -123;
+        try {
+            code = Integer.parseInt(rd.getResponseCode());
+        } catch (NumberFormatException e) {
+            fail("Response code is not a number");
+        }
+        assertThat(code, greaterThanOrEqualTo(500));
 
-    Envelope rdEnvelope = rdList.get(0);
-    Envelope rddEnvelope1 = rddList.get(0);
-    Envelope rddEnvelope2 = rddList.get(1);
-    Envelope rddEnvelope3 = rddList.get(2);
+        assertParentChild(rdd.getId(), rdEnvelope, edEnvelope1);
+    }
 
-    RequestData rd = (RequestData) ((Data) rdEnvelope.getData()).getBaseData();
-    RemoteDependencyData rdd1 =
-        (RemoteDependencyData) ((Data) rddEnvelope1.getData()).getBaseData();
-    RemoteDependencyData rdd2 =
-        (RemoteDependencyData) ((Data) rddEnvelope2.getData()).getBaseData();
-    RemoteDependencyData rdd3 =
-        (RemoteDependencyData) ((Data) rddEnvelope3.getData()).getBaseData();
+    @Test
+    @TargetUri("/asyncDependencyCall")
+    public void testAsyncDependencyCall() throws Exception {
+        commonValidation();
+    }
 
-    assertEquals("GET /asyncDependencyCall", rd.getName());
-    assertEquals("TestController.asyncDependencyCall", rdd1.getName());
-    assertEquals("GET /", rdd2.getName());
-    assertEquals("TestController.asyncDependencyCall", rdd3.getName());
-    assertEquals("www.bing.com", rdd2.getTarget());
+    private static void commonValidation() throws Exception {
+        List<Envelope> rdList = mockedIngestion.waitForItems("RequestData", 1);
+        List<Envelope> rddList = mockedIngestion.waitForItemsInRequest("RemoteDependencyData", 3);
 
-    assertParentChild(rdd1.getId(), rdEnvelope, rddEnvelope2);
-    assertParentChild(rdd1.getId(), rdEnvelope, rddEnvelope3);
-  }
+        Envelope rdEnvelope = rdList.get(0);
+        Envelope rddEnvelope1 = rddList.get(0);
+        Envelope rddEnvelope2 = rddList.get(1);
+        Envelope rddEnvelope3 = rddList.get(2);
 
-  private static void assertParentChild(
-      String parentId, Envelope parentEnvelope, Envelope childEnvelope) {
-    String operationId = parentEnvelope.getTags().get("ai.operation.id");
+        RequestData rd = (RequestData) ((Data) rdEnvelope.getData()).getBaseData();
+        RemoteDependencyData rdd1 =
+                (RemoteDependencyData) ((Data) rddEnvelope1.getData()).getBaseData();
+        RemoteDependencyData rdd2 =
+                (RemoteDependencyData) ((Data) rddEnvelope2.getData()).getBaseData();
+        RemoteDependencyData rdd3 =
+                (RemoteDependencyData) ((Data) rddEnvelope3.getData()).getBaseData();
 
-    assertNotNull(operationId);
+        assertEquals("GET /asyncDependencyCall", rd.getName());
+        assertEquals("TestController.asyncDependencyCall", rdd1.getName());
+        assertEquals("GET /", rdd2.getName());
+        assertEquals("TestController.asyncDependencyCall", rdd3.getName());
+        assertEquals("www.bing.com", rdd2.getTarget());
 
-    assertEquals(operationId, childEnvelope.getTags().get("ai.operation.id"));
-    assertEquals(parentId, childEnvelope.getTags().get("ai.operation.parentId"));
-  }
+        assertParentChild(rdd1.getId(), rdEnvelope, rddEnvelope2);
+        assertParentChild(rdd1.getId(), rdEnvelope, rddEnvelope3);
+    }
+
+    private static void assertParentChild(
+            String parentId, Envelope parentEnvelope, Envelope childEnvelope) {
+        String operationId = parentEnvelope.getTags().get("ai.operation.id");
+
+        assertNotNull(operationId);
+
+        assertEquals(operationId, childEnvelope.getTags().get("ai.operation.id"));
+        assertEquals(parentId, childEnvelope.getTags().get("ai.operation.parentId"));
+    }
 }
