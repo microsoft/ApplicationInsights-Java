89a752e5792bda87a255531fb4702ce01e1d1fd5
Helen Yang <heya@microsoft.com>
2020-10-15 13:56:29 -0700

    Fix broken dependencies for Azure Function consumption plan

--- agent/agent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/TracerInstaller.java
+++ agent/agent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/TracerInstaller.java
@@ -8,6 +8,7 @@ import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.context.propagation.DefaultContextPropagators;
 import io.opentelemetry.instrumentation.api.aiappid.AiHttpTraceContext;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.Samplers;
 import io.opentelemetry.sdk.trace.config.TraceConfig;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 
@@ -28,6 +29,14 @@ public class TracerInstaller {
             OpenTelemetrySdk.getTracerProvider().updateActiveTraceConfig(
                     TraceConfig.getDefault().toBuilder()
                             .setSampler(new FixedRateSampler(fixedRateSamplingPercentage))
+                            .build());
+        } else {
+            // TODO will revisit to delete DefaultSpan
+            // For the case of AzureFunctionInstrumentation, TraceParent comes in this format: 00-ea5a92ef3cf03649acf1b89c2b5d4211-b70350d129319a4b-00
+            // Sampling is always off.  We need to use AlwaysOnSampler.
+            OpenTelemetrySdk.getTracerProvider().updateActiveTraceConfig(
+                    TraceConfig.getDefault().toBuilder()
+                            .setSampler(Samplers.alwaysOn())
                             .build());
         }
         // if changing the span processor to something async, flush it in the shutdown hook before flushing TelemetryClient
