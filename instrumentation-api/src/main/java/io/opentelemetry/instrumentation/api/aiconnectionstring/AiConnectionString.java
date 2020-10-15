46a0d794d87ec9cbbefc03f2ee1263b1d312ba43
Helen Y <56097766+heyams@users.noreply.github.com>
2020-10-15 16:46:14 -0700

    Support concurrency
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/aiconnectionstring/AiConnectionString.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/aiconnectionstring/AiConnectionString.java
@@ -18,7 +18,7 @@ package io.opentelemetry.instrumentation.api.aiconnectionstring;
 
 public class AiConnectionString {
 
-  private static Accessor accessor;
+  private static volatile Accessor accessor;
 
   public static void setAccessor(Accessor accessor) {
     AiConnectionString.accessor = accessor;
