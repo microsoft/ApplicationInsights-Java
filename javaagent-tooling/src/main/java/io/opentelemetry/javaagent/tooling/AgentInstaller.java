6e8b0ae585ac054772268e2a0501932c05cb7a09
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-10-17 12:04:58 -0700

    Allow BeforeAgentInstaller to override Config

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/AgentInstaller.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/AgentInstaller.java
@@ -81,9 +81,6 @@ public class AgentInstaller {
 
     // WeakMap is used by other classes below, so we need to register the provider first.
     AgentTooling.registerWeakMapProvider();
-
-    // this needs to be done as early as possible - before the first Config.get() call
-    ConfigInitializer.initialize();
   }
 
   public static void installBytebuddyAgent(Instrumentation inst) throws Exception {
@@ -98,6 +95,9 @@ public class AgentInstaller {
       final Method method = clazz.getMethod("beforeInstallBytebuddyAgent", Instrumentation.class);
       method.invoke(null, inst);
     }
+
+    // this needs to be done as early as possible - before the first Config.get() call
+    ConfigInitializer.initialize();
 
     if (Config.get().getBooleanProperty(JAVAAGENT_ENABLED_CONFIG, true)) {
       installBytebuddyAgent(inst, false, new AgentBuilder.Listener[0]);
