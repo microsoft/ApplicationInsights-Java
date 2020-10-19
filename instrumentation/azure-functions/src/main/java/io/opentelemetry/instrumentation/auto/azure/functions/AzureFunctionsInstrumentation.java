e27e47bfbff46de73eb52155c953066ac43af056
Helen Y <56097766+heyams@users.noreply.github.com>
2020-10-19 10:45:17 -0700

    Lazily set connection string

--- instrumentation/azure-functions/src/main/java/io/opentelemetry/instrumentation/auto/azure/functions/AzureFunctionsInstrumentation.java
+++ instrumentation/azure-functions/src/main/java/io/opentelemetry/instrumentation/auto/azure/functions/AzureFunctionsInstrumentation.java
@@ -23,11 +23,13 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import com.google.auto.service.AutoService;
+import com.google.common.base.Strings;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.api.aiconnectionstring.AiConnectionString;
 import io.opentelemetry.javaagent.tooling.Instrumenter;
 import java.util.Map;
 import net.bytebuddy.asm.Advice;
@@ -67,6 +69,23 @@ public class AzureFunctionsInstrumentation extends Instrumenter.Default {
     @Advice.OnMethodEnter(suppress = Throwable.class)
     public static Scope methodEnter(@Advice.Argument(0) final Object request)
         throws ReflectiveOperationException {
+      // race condition (two initial requests happening at the same time) is not a worry here
+      // because at worst they both enter the condition below and update the connection string
+      if (!AiConnectionString.hasConnectionString()) {
+        String connectionString = System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING");
+        if (!Strings.isNullOrEmpty(connectionString)) {
+          AiConnectionString.setConnectionString(connectionString);
+        } else {
+          // if the instrumentation key is neither null nor empty , we will create a default
+          // connection string based on the instrumentation key.
+          // this is to support Azure Functions that were created prior to the introduction of
+          // connection strings
+          String instrumentationKey = System.getenv("APPINSIGHTS_INSTRUMENTATIONKEY");
+          if (!Strings.isNullOrEmpty(instrumentationKey)) {
+            AiConnectionString.setConnectionString("InstrumentationKey=" + instrumentationKey);
+          }
+        }
+      }
 
       final Object traceContext =
           InvocationRequestExtractAdapter.getTraceContextMethod.invoke(request);
