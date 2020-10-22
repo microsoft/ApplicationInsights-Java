3878da593fa4b7b51705d5bd811fad733fd0b0b5
Helen Yang <heya@microsoft.com>
2020-10-22 14:11:16 -0700

    Fix doMostBasicTest

--- test/smoke/testApps/SpringBootAuto/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/SpringBootAutoTest.java
+++ test/smoke/testApps/SpringBootAuto/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/SpringBootAutoTest.java
@@ -1,6 +1,14 @@
 package com.microsoft.applicationinsights.smoketest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.microsoft.applicationinsights.internal.schemav2.Data;
+import com.microsoft.applicationinsights.internal.schemav2.Envelope;
+import com.microsoft.applicationinsights.internal.schemav2.RequestData;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 @UseAgent
 public class SpringBootAutoTest extends AiSmokeTest {
@@ -8,6 +16,14 @@ public class SpringBootAutoTest extends AiSmokeTest {
     @Test
     @TargetUri("/test")
     public void doMostBasicTest() throws Exception {
-        mockedIngestion.waitForItems("RequestData", 1);
+        List<Envelope> rdList = mockedIngestion.getItemsEnvelopeDataType("RequestData");
+        List<Envelope> httpRequestList = new ArrayList<>();
+        for (Envelope envelope : rdList) {
+            RequestData rd = (RequestData) ((Data) envelope.getData()).getBaseData();
+            if (rd.getName().equals("GET /test")) {
+                httpRequestList.add(envelope);
+            }
+        }
+        assertEquals(1, httpRequestList.size());
     }
 }
