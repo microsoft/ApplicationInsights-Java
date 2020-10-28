facafc0868d6f8dc9331aa1256f55c499d9e0c96
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-10-27 20:21:00 -0700

    More jetty handler exclusions

--- instrumentation/jetty-8.0/src/main/java/io/opentelemetry/javaagent/instrumentation/jetty/JettyHandlerInstrumentation.java
+++ instrumentation/jetty-8.0/src/main/java/io/opentelemetry/javaagent/instrumentation/jetty/JettyHandlerInstrumentation.java
@@ -9,6 +9,7 @@ import static io.opentelemetry.javaagent.tooling.ClassLoaderMatcher.hasClassesNa
 import static io.opentelemetry.javaagent.tooling.bytebuddy.matcher.AgentElementMatchers.implementsInterface;
 import static java.util.Collections.singletonMap;
 import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.not;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
@@ -40,10 +41,14 @@ public final class JettyHandlerInstrumentation extends Instrumenter.Default {
     // normally, which the jetty instrumentation does not capture since jetty doesn't populate
     // contextPath and servletPath until right before calling the servlet
     // (another option is to instrument ServletHolder.handle() to capture those fields)
-    return not(named("org.eclipse.jetty.server.handler.HandlerWrapper"))
-        .and(not(named("org.eclipse.jetty.server.handler.ScopedHandler")))
-        .and(not(named("org.eclipse.jetty.server.handler.ContextHandler")))
-        .and(not(named("org.eclipse.jetty.servlet.ServletHandler")))
+    //
+    // using nameStartsWith() as there are many built-in handlers, e.g.
+    // org.eclipse.jetty.server.handler.HandlerWrapper
+    // org.eclipse.jetty.server.handler.ScopedHandler
+    // org.eclipse.jetty.server.handler.ContextHandler
+    // org.eclipse.jetty.security.SecurityHandler
+    // org.eclipse.jetty.servlet.ServletHandler
+    return not(nameStartsWith("org.eclipse.jetty."))
         .and(implementsInterface(named("org.eclipse.jetty.server.Handler")));
   }
 
