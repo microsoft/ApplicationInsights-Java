a0044c15bf5e4031deaefe1b6157f2a031876035
Helen Yang <heya@microsoft.com>
2020-11-10 08:45:20 -0800

    Use console output

--- instrumentation/azure-functions/src/main/java/io/opentelemetry/instrumentation/auto/azure/functions/AzureFunctionsInstrumentationHelper.java
+++ instrumentation/azure-functions/src/main/java/io/opentelemetry/instrumentation/auto/azure/functions/AzureFunctionsInstrumentationHelper.java
@@ -13,15 +13,15 @@ public class AzureFunctionsInstrumentationHelper {
     boolean lazySetOptIn = Boolean.parseBoolean(System.getProperty("LazySetOptIn"));
     String connectionString = System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING");
     String enableAgent = System.getenv("APPLICATIONINSIGHTS_ENABLE_AGENT");
-    logger.debug("################################### lazySetOptIn: {}", lazySetOptIn);
-    logger.debug("################################### enableAgent: {}", enableAgent);
-    logger.debug("################################### Boolean.parseBoolean(enableAgent): {}", Boolean.parseBoolean(enableAgent));
+    System.out.println("################################### lazySetOptIn: " + lazySetOptIn);
+    System.out.println("################################### enableAgent: " + enableAgent);
+    System.out.println("################################### Boolean.parseBoolean(enableAgent): " + Boolean.parseBoolean(enableAgent));
 
     // race condition (two initial requests happening at the same time) is not a worry here
     // because at worst they both enter the condition below and update the connection string
     if (!AiConnectionString.hasConnectionString()) {
       if (!Strings.isNullOrEmpty(connectionString)) {
-        logger.debug("################################### setting connection string: {}", connectionString);
+        System.out.println("################################### setting connection string: " + connectionString);
         setConnectionString(lazySetOptIn, enableAgent, connectionString);
       }
     } else {
@@ -31,29 +31,29 @@ public class AzureFunctionsInstrumentationHelper {
       // connection strings
       String instrumentationKey = System.getenv("APPINSIGHTS_INSTRUMENTATIONKEY");
       if (!Strings.isNullOrEmpty(instrumentationKey)) {
-        logger.debug("################################### setting instrumentation key: {}", instrumentationKey);
+        System.out.println("################################### setting instrumentation key: " + instrumentationKey);
         setConnectionString(lazySetOptIn, enableAgent, instrumentationKey);
       }
     }
   }
 
   private static void setConnectionString(boolean lazySetOptIn, String enableAgent, String value) {
-    logger.debug("################################### connection string or instrumentation key: {}", value);
+    System.out.println("################################### connection string or instrumentation key: " + value);
     if (lazySetOptIn) {
       // when LazySetOptIn is on, enable agent if APPLICATIONINSIGHTS_ENABLE_AGENT is null or true
       if (enableAgent == null || Boolean.parseBoolean(enableAgent)) {
-        logger.debug("################################### lazily set connection string when lazySetOptIn is true");
+        System.out.println("################################### lazily set connection string when lazySetOptIn is true");
         AiConnectionString.setConnectionString(value);
       } else {
-        logger.debug("################################### DO NOT lazily set connection string when lazySetOptIn is true");
+        System.out.println("################################### DO NOT lazily set connection string when lazySetOptIn is true");
       }
     } else {
       // when LazySetOptIn is off, enable agent only when APPLICATIONINSIGHTS_ENABLE_AGENT is true
       if (Boolean.parseBoolean(enableAgent)) {
-        logger.debug("################################### lazily set instrumentation key when lazySetOptIn is off");
+        System.out.println("################################### lazily set instrumentation key when lazySetOptIn is off");
         AiConnectionString.setConnectionString(value);
       } else {
-        logger.debug("################################### DO NOT lazily set instrumentation key when lazySetOptIn is off");
+        System.out.println("################################### DO NOT lazily set instrumentation key when lazySetOptIn is off");
       }
     }
   }
