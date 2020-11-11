689b27f94acd9ee9a6d3d8079c787c3d7483a0c3
Helen Yang <heya@microsoft.com>
2020-11-11 15:06:06 -0800

    Add unit tests

--- instrumentation/azure-functions/src/main/java/io/opentelemetry/instrumentation/auto/azure/functions/AzureFunctionsInstrumentationHelper.java
+++ instrumentation/azure-functions/src/main/java/io/opentelemetry/instrumentation/auto/azure/functions/AzureFunctionsInstrumentationHelper.java
@@ -7,44 +7,56 @@ import org.slf4j.LoggerFactory;
 
 public class AzureFunctionsInstrumentationHelper {
 
-  private static final Logger logger = LoggerFactory.getLogger(AzureFunctionsInstrumentationHelper.class);
+  private static final Logger logger = LoggerFactory
+      .getLogger(AzureFunctionsInstrumentationHelper.class);
 
   public static void lazilySetConnectionString() {
+    // race condition (two initial requests happening at the same time) is not a worry here
+    // because at worst they both enter the condition below and update the connection string
+    if (AiConnectionString.hasConnectionString()) {
+      return;
+    }
+
     boolean lazySetOptIn = Boolean.parseBoolean(System.getProperty("LazySetOptIn"));
     String connectionString = System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING");
+    String instrumentationKey = System.getenv("APPINSIGHTS_INSTRUMENTATIONKEY");
     String enableAgent = System.getenv("APPLICATIONINSIGHTS_ENABLE_AGENT");
     logger.info("lazySetOptIn: {}", lazySetOptIn);
     logger.info("APPLICATIONINSIGHTS_ENABLE_AGENT: {}", enableAgent);
 
-    // race condition (two initial requests happening at the same time) is not a worry here
-    // because at worst they both enter the condition below and update the connection string
-    if (!AiConnectionString.hasConnectionString()) {
-      if (!Strings.isNullOrEmpty(connectionString)) {
-        setConnectionString(lazySetOptIn, enableAgent, connectionString);
-      } else {
-        // if the instrumentation key is neither null nor empty , we will create a default
-        // connection string based on the instrumentation key.
-        // this is to support Azure Functions that were created prior to the introduction of
-        // connection strings
-        String instrumentationKey = System.getenv("APPINSIGHTS_INSTRUMENTATIONKEY");
-        if (!Strings.isNullOrEmpty(instrumentationKey)) {
-          setConnectionString(lazySetOptIn, enableAgent, "InstrumentationKey=" + instrumentationKey);
-        }
+    if (!shouldSetConnectionString(lazySetOptIn, enableAgent)) {
+      return;
+    }
+
+    setConnectionString(connectionString, instrumentationKey);
+  }
+
+  static void setConnectionString(String connectionString, String instrumentationKey) {
+    if (!Strings.isNullOrEmpty(connectionString)) {
+      AiConnectionString.setConnectionString(connectionString);
+    } else {
+      // if the instrumentation key is neither null nor empty , we will create a default
+      // connection string based on the instrumentation key.
+      // this is to support Azure Functions that were created prior to the introduction of
+      // connection strings
+      if (!Strings.isNullOrEmpty(instrumentationKey)) {
+        AiConnectionString.setConnectionString("InstrumentationKey=" + instrumentationKey);
       }
     }
   }
 
-  private static void setConnectionString(boolean lazySetOptIn, String enableAgent, String value) {
+  static boolean shouldSetConnectionString(boolean lazySetOptIn, String enableAgent) {
     if (lazySetOptIn) {
       // when LazySetOptIn is on, enable agent if APPLICATIONINSIGHTS_ENABLE_AGENT is null or true
       if (enableAgent == null || Boolean.parseBoolean(enableAgent)) {
-        AiConnectionString.setConnectionString(value);
+        return true;
       }
     } else {
       // when LazySetOptIn is off, enable agent if APPLICATIONINSIGHTS_ENABLE_AGENT is true
       if (Boolean.parseBoolean(enableAgent)) {
-        AiConnectionString.setConnectionString(value);
+        return true;
       }
     }
+    return false;
   }
 }
