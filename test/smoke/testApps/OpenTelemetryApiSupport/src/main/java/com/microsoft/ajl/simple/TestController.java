fdc4146d4ea484cebe225078b6ddf7b5ee44f23c
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-11-22 17:35:49 -0800

    Add OpenTelemetry API smoke test

---
+++ test/smoke/testApps/OpenTelemetryApiSupport/src/main/java/com/microsoft/ajl/simple/TestController.java
@@ -0,0 +1,23 @@
+package com.microsoft.ajl.simple;
+
+import io.opentelemetry.api.trace.Span;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String root() {
+        return "OK";
+    }
+
+
+    @GetMapping("/test")
+    public String test() {
+        Span.current().setAttribute("myattr1", "myvalue1");
+        Span.current().setAttribute("myattr2", "myvalue2");
+        Span.current().updateName("myspanname");
+        return "OK!";
+    }
+}
---
+++ test/smoke/testApps/OpenTelemetryApiSupport/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/OpenTelemetryApiSupportTest.java
@@ -0,0 +1,38 @@
+package com.microsoft.applicationinsights.smoketest;
+
+import java.util.List;
+
+import com.microsoft.applicationinsights.internal.schemav2.Data;
+import com.microsoft.applicationinsights.internal.schemav2.Envelope;
+import com.microsoft.applicationinsights.internal.schemav2.RemoteDependencyData;
+import com.microsoft.applicationinsights.internal.schemav2.RequestData;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@UseAgent("opentelemetryapisupport")
+public class OpenTelemetryApiSupportTest extends AiSmokeTest {
+
+    @Test
+    @TargetUri("/test")
+    public void doMostBasicTest() throws Exception {
+        List<Envelope> rdList = mockedIngestion.waitForItems("RequestData", 1);
+
+        Envelope rdEnvelope = rdList.get(0);
+        String operationId = rdEnvelope.getTags().get("ai.operation.id");
+        List<Envelope> rddList = mockedIngestion.waitForItemsInOperation("RemoteDependencyData", 1, operationId);
+
+        Envelope rddEnvelope = rddList.get(0);
+
+        RequestData rd = (RequestData) ((Data) rdEnvelope.getData()).getBaseData();
+        RemoteDependencyData rdd = (RemoteDependencyData) ((Data) rddEnvelope.getData()).getBaseData();
+
+        // ideally want these on rd, but can't get SERVER span yet
+        // see https://github.com/open-telemetry/opentelemetry-java-instrumentation/issues/1726#issuecomment-731890267
+        assertEquals("myvalue1", rdd.getProperties().get("myattr1"));
+        assertEquals("myvalue2", rdd.getProperties().get("myattr2"));
+        assertEquals("myspanname", rdd.getName());
+
+        assertTrue(rd.getSuccess());
+    }
+}
