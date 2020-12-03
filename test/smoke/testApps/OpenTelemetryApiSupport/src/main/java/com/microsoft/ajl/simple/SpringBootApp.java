ced183205fee2ee4c89c338dd8d74e5d8bebd590
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-12-02 17:31:39 -0800

    Add OpenTelemetry API smoke test (#1391)
    * Add OpenTelemetry API smoke test
    
    * Remove unnecessary smoke test dependencies
    
    * Update to 0.11.0

---
+++ test/smoke/testApps/OpenTelemetryApiSupport/src/main/java/com/microsoft/ajl/simple/SpringBootApp.java
@@ -0,0 +1,13 @@
+package com.microsoft.ajl.simple;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootApp extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(SpringBootApp.class);
+    }
+}
