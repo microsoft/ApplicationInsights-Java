6c4c900c377368afbda7fee85065f3a3730baf47
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-02-02 15:05:40 -0800

    Comment

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/sampling/AiBackCompatSampler.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/sampling/AiBackCompatSampler.java
@@ -14,11 +14,15 @@ import io.opentelemetry.sdk.trace.samplers.SamplingResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// this sampler does two things:
+// * implements same trace id hashing algorithm so that traces are sampled the same across multiple nodes
+//   when some of those nodes are being monitored by other Application Insights SDKs (and 2.x Java SDK)
+// * adds sampling percentage to span attribute (TODO this is not being applied to child spans)
 public final class AiBackCompatSampler implements Sampler {
 
     private static final Logger logger = LoggerFactory.getLogger(AiBackCompatSampler.class);
 
-    // all sampling percentage must be in a ratio of 100/N where N is a whole number (2, 3, 4, …)
+    // all sampling percentage must be in a ratio of 100/N where N is a whole number (2, 3, 4, ...)
     // e.g. 50 for 1/2 or 33.33 for 1/3
     //
     // failure to follow this pattern can result in unexpected / incorrect computation of values in the portal
