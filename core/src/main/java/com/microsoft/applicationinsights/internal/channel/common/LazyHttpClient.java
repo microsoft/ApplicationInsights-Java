fb2ecb3b90588880949b4a321ed78f8a10c40efa
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-03-02 18:38:30 -0800

    LazyHttpClient

--- core/src/main/java/com/microsoft/applicationinsights/internal/channel/common/LazyHttpClient.java
+++ core/src/main/java/com/microsoft/applicationinsights/internal/channel/common/LazyHttpClient.java
@@ -1,24 +1,3 @@
-/*
- * ApplicationInsights-Java
- * Copyright (c) Microsoft Corporation
- * All rights reserved.
- *
- * MIT License
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the ""Software""), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
- * persons to whom the Software is furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
 package com.microsoft.applicationinsights.internal.channel.common;
 
 import java.io.IOException;
@@ -27,17 +6,16 @@ import java.util.concurrent.TimeUnit;
 import javax.annotation.concurrent.GuardedBy;
 import javax.net.ssl.SSLHandshakeException;
 
-import com.microsoft.applicationinsights.customExceptions.FriendlyException;
 import com.microsoft.applicationinsights.internal.util.SSLOptionsUtil;
 import com.microsoft.applicationinsights.internal.util.SSLUtil;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
@@ -45,16 +23,21 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created by gupele on 6/4/2015.
- */
-public final class ApacheSender43 implements ApacheSender {
+public class LazyHttpClient extends CloseableHttpClient {
 
-    private static final Logger logger = LoggerFactory.getLogger(ApacheSender43.class);
+    private static final Logger logger = LoggerFactory.getLogger(LazyHttpClient.class);
+
+    private static final LazyHttpClient INSTANCE = new LazyHttpClient();
+
+    private static final int DEFAULT_MAX_TOTAL_CONNECTIONS = 200;
+    private static final int REQUEST_TIMEOUT_IN_MILLIS = 60000;
+    private static final int DEFAULT_MAX_CONNECTIONS_PER_ROUTE = 20;
 
     public static volatile CountDownLatch safeToInitLatch;
     public static volatile HttpHost proxy;
@@ -64,27 +47,33 @@ public final class ApacheSender43 implements ApacheSender {
     @GuardedBy("lock")
     private RuntimeException initException;
     @GuardedBy("lock")
-    private CloseableHttpClient httpClient;
+    private CloseableHttpClient delegate;
 
-    static ApacheSender43 create() {
-        return new ApacheSender43();
+    public static CloseableHttpClient getInstance() {
+        return INSTANCE;
     }
 
-    private ApacheSender43() {
+    private LazyHttpClient() {
     }
 
-    @Override
-    public HttpResponse sendRequest(HttpUriRequest request) throws IOException, FriendlyException {
-        try {
-            return getHttpClient().execute(request);
-        } catch (SSLHandshakeException e) {
-            String completeUrl = "https://" + request.getURI().getHost();
-            throw SSLUtil.newSSLFriendlyException(completeUrl);
+    private CloseableHttpClient getDelegate() {
+        synchronized (lock) {
+            if (delegate != null) {
+                return delegate;
+            }
+            if (initException != null) {
+                throw initException;
+            }
+            try {
+                delegate = init();
+            } catch (RuntimeException e) {
+                initException = e;
+            }
+            return delegate;
         }
     }
 
-    @Override
-    public void dispose(HttpResponse response) {
+    public static void dispose(HttpResponse response) {
         try {
             if (response != null) {
                 ((CloseableHttpResponse) response).close();
@@ -94,26 +83,7 @@ public final class ApacheSender43 implements ApacheSender {
         }
     }
 
-    @Override
-    public HttpClient getHttpClient() {
-        synchronized (lock) {
-            if (httpClient != null) {
-                return httpClient;
-            }
-            if (initException != null) {
-                throw initException;
-            }
-            try {
-                httpClient = init();
-            } catch (RuntimeException e) {
-                initException = e;
-            }
-            return httpClient;
-        }
-    }
-
-    @Override
-    public void enhanceRequest(HttpPost request) {
+    public static void enhanceRequest(HttpPost request) {
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(REQUEST_TIMEOUT_IN_MILLIS)
                 .setSocketTimeout(REQUEST_TIMEOUT_IN_MILLIS)
@@ -138,8 +108,8 @@ public final class ApacheSender43 implements ApacheSender {
                 throw new RuntimeException(e);
             }
         }
-        final String[] allowedProtocols = SSLOptionsUtil.getAllowedProtocols();
-        final PoolingHttpClientConnectionManager cm =
+        String[] allowedProtocols = SSLOptionsUtil.getAllowedProtocols();
+        PoolingHttpClientConnectionManager cm =
                 new PoolingHttpClientConnectionManager(RegistryBuilder.<ConnectionSocketFactory>create()
                         .register("https",
                                 new SSLConnectionSocketFactory(SSLContexts.createDefault(), allowedProtocols, null,
@@ -160,5 +130,30 @@ public final class ApacheSender43 implements ApacheSender {
             builder.setProxy(proxy);
         }
         return builder.build();
+    }
+
+    @Override
+    protected CloseableHttpResponse doExecute(HttpHost target, HttpRequest request, HttpContext context) throws IOException {
+        try {
+            return getDelegate().execute(target, request, context);
+        } catch (SSLHandshakeException e) {
+            String completeUrl = "https://" + target.getHostName();
+            throw SSLUtil.newSSLFriendlyException(completeUrl);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        getDelegate().close();
+    }
+
+    @Override
+    public HttpParams getParams() {
+        return getDelegate().getParams();
+    }
+
+    @Override
+    public ClientConnectionManager getConnectionManager() {
+        return getDelegate().getConnectionManager();
     }
 }
