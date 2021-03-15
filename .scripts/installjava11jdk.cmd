7a5df38bd3111946267db63170ebf49daea43ec0
Helen Yang <heya@microsoft.com>
2021-03-15 16:18:05 -0700

    Use java 11

--- .scripts/installjava11jdk.cmd
+++ .scripts/installjava11jdk.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-set JDK_VERSION=8
-set JDK_NAME=zulu-8-azure-jdk_8.48.0.53-8.0.265-win_x64
-set JDK_ZIP_URL=https://repos.azul.com/azure-only/zulu/packages/zulu-8/8u265/zulu-8-azure-jdk_8.48.0.53-8.0.265-win_x64.zip
+set JDK_VERSION=11
+set JDK_NAME=zulu-11-azure-jdk_11.45.27-11.0.10-win_x64
+set JDK_ZIP_URL=https://repos.azul.com/azure-only/zulu/packages/zulu-11/11.0.10/zulu-11-azure-jdk_11.45.27-11.0.10-win_x64.zip
 
 echo "Installing Java %JDK_VERSION% (%JDK_NAME%)..."
 powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-java.ps1" -Url "%JDK_ZIP_URL%" -Destination "C:\Program Files\Java" -EnvVar "JAVA_HOME" -UpdatePath -Checksum "0bc2ba3066a23e217f0c9a590c3d8a7e17244e849034866606571395877ee7fc" -CleanOnFinish || exit /B 1
