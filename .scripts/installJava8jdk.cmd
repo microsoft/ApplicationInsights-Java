e80d26539cf4c8883b0694f177ea15de1a6f818e
Helen Yang <heya@microsoft.com>
2021-03-16 15:04:21 -0700

    Use java 8 JDK instead of JRE

---
+++ .scripts/installJava8jdk.cmd
@@ -0,0 +1,13 @@
+
+
+@echo off
+
+echo "Installing Java 8 JDK..."
+powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-java.ps1" -Url "https://repos.azul.com/azure-only/zulu/packages/zulu-8/8u282/zulu-8-azure-jdk_8.52.0.23-8.0.282-win_x64.zip " -Destination "C:\Program Files\Java" -EnvVar "JAVA_HOME" -Checksum "833600bbc34dba79dcfed88ff6878d6c37a3e32525b75e829b7057c091cc703e" -CleanOnFinish || exit /B 1
+
+echo "C:\Program Files\Java\zulu-8-azure-jdk_8.52.0.23-8.0.282-win_x64"
+dir "C:\Program Files\Java\zulu-8-azure-jdk_8.52.0.23-8.0.282-win_x64.zip"
+
+echo "JAVA_HOME=%JAVA_HOME%"
+
+echo "Installed Java 8 JDK Successfully."
