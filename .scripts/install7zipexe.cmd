62345b7034db602cd197caf75c7f2c0732643fc1
Helen Yang <heya@microsoft.com>
2021-03-20 14:17:08 -0700

    Install 7-Zip if it doesn't exist

--- .scripts/install7zipexe.cmd
+++ .scripts/install7zipexe.cmd
@@ -1,14 +1,20 @@
 @echo off
 
 echo "Installing 7-Zip Windows..."
+set SEVEN_ZIP=C:\7-Zip\7z.exe
 
+echo "7Zip root path: %SEVEN_ZIP%"
 set ZIP_URL=https://www.7-zip.org/a/7z2101-x64.exe
 
-powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-azcopy.ps1" -Url "%ZIP_URL%" -Destination "C:\7-Zip" -SkipUnzip -CleanOnFinish || exit /B 1
+IF NOT EXIST %SEVEN_ZIP% (
+    echo "%SEVEN_ZIP% does not exist yet."
+    powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-azcopy.ps1" -Url "%ZIP_URL%" -Destination "C:\7-Zip" -SkipUnzip -CleanOnFinish || exit /B 1
 
-IF EXIST "C:\7-Zip\7z.exe" (
-  echo "Installed 7-Zip Successfully."
+    IF EXIST %SEVEN_ZIP% (
+      echo "Installed 7-Zip Successfully."
+    ) ELSE (
+      echo "Failed to install 7-Zip."
+    )
 ) ELSE (
-  echo "Failed to install 7-Zip."
+    echo "%SEVEN_ZIP% exists, no installation is needed."
 )
-
