acb62f821c3145fa3cafd5baae1bc9ef7845a987
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-03-21 23:46:00 -0700

    Feedback

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/sampling/SamplingOverrides.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/sampling/SamplingOverrides.java
@@ -14,6 +14,7 @@ import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.trace.samplers.SamplingDecision;
 import io.opentelemetry.sdk.trace.samplers.SamplingResult;
 
+// TODO find a better name for this class (and MatcherGroup too)
 class SamplingOverrides {
 
     private final List<MatcherGroup> matcherGroups;
