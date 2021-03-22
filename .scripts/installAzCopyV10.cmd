a7baa64f84516036c3aa7fdc36d21302394c7c7e
Helen Yang <heya@microsoft.com>
2021-03-22 15:08:41 -0700

    Add a check to install 64bit vs. 32bit azcopy

--- .scripts/installAzCopyV10.cmd
+++ .scripts/installAzCopyV10.cmd
@@ -2,9 +2,15 @@
 
 echo "Installing AzCopy V10 Windows..."
 
-set ZIP_URL=https://azcopyvnext.azureedge.net/release20210226/azcopy_windows_amd64_10.9.0.zip
+set ZIP_URL_64=https://azcopyvnext.azureedge.net/release20210226/azcopy_windows_amd64_10.9.0.zip
+set ZIP_URL_32=https://azcopyvnext.azureedge.net/release20210226/azcopy_windows_386_10.9.0.zip
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 
-powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-azcopy.ps1" -Url "%ZIP_URL%" -Destination "C:\Program Files\AzCopy" -UpdatePath -PathTo7Zip "C:\7-Zip\7z.exe" -CleanOnFinish || exit /B 1
+if %OS%==64BIT (
+    powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-azcopy.ps1" -Url "%ZIP_URL_64%" -Destination "C:\Program Files\AzCopy" -UpdatePath -PathTo7Zip "C:\7-Zip\7z.exe" -CleanOnFinish || exit /B 1
+) else (
+    powershell -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0install-azcopy.ps1" -Url "%ZIP_URL_32%" -Destination "C:\Program Files\AzCopy" -UpdatePath -PathTo7Zip "C:\7-Zip\7z.exe" -CleanOnFinish || exit /B 1
+)
 
 echo "C:\Program Files\AzCopy"
 dir "C:\Program Files\AzCopy"
