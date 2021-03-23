107990b1b18bbaae896ae3b401b09f59f5538a3a
Helen Yang <heya@microsoft.com>
2021-03-23 12:16:01 -0700

    Add release scripts

---
+++ .scripts/copyBitsToAzureBlob.ps1
@@ -0,0 +1,23 @@
+param (
+    [Parameter(Mandatory=$true, HelpMessage="azuresdkpartnerdrops service principal key")]
+    [ValidateNotNullOrEmpty()]
+    [string]$ServicePrincipalKey,
+
+    [Parameter(Mandatory=$true, HelpMessage="azuresdkpartnerdrops Service principal client ID")]
+    [ValidateNotNullOrEmpty()]
+    [string]$ServicePrincipleClientId,
+
+    [Parameter(Mandatory=$true, HelpMessage="Jar location")]
+    [ValidateNotNullOrEmpty()]
+    [string]$JarPath,
+
+    [Parameter(Mandatory=$true, HelpMessage="applicationinsights-java version")]
+    [ValidateNotNullOrEmpty()]
+    [string]$SDKVersionNumber
+)
+
+Write-Host "Jar path: $JarPath"
+$Env:AZCOPY_SPA_CLIENT_SECRET=$ServicePrincipalKey
+azcopy login --service-principal --application-id $ServicePrincipleClientId
+azcopy copy "$JarPath/applicationinsights-agent-$SDKVersionNumber.jar" "https://azuresdkpartnerdrops.blob.core.windows.net/drops/applicationinsights-sdk/java/$SDKVersionNumber/"
+Remove-Item Env:AZCOPY_SPA_CLIENT_SECRET
