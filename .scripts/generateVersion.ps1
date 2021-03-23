aa0290743e8ae3263187cc39f8f3274b0dc8772f
Helen Yang <heya@microsoft.com>
2021-03-23 10:37:52 -0700

    Revert Version back to 3.0.3-BETA.2

--- .scripts/generateVersion.ps1
+++ .scripts/generateVersion.ps1
@@ -13,4 +13,3 @@ $revision = "$date$env:CDP_DEFINITION_BUILD_COUNT_DAY"
 $buildNumber = "$major.$minor.$patch.$revision"
 [Environment]::SetEnvironmentVariable("CustomBuildNumber", $buildNumber, "User")  # This will allow you to use it from env var in later steps of the same phase
 Write-Host "##vso[build.updatebuildnumber]${buildNumber}"
-Write-Host "#################################### tag: $tag"
