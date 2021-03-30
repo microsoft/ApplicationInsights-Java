0688c09084a16b46efaefc5208084f10048c4183
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-03-30 16:37:12 -0700

    Move class loader optimization opt out to another branch

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/InstrumentationModule.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/InstrumentationModule.java
@@ -49,9 +49,6 @@ public abstract class InstrumentationModule {
   private static final Logger log = LoggerFactory.getLogger(InstrumentationModule.class);
   private static final Logger muzzleLog = LoggerFactory.getLogger("muzzleMatcher");
 
-  private static boolean classLoaderOptimizationOptOutFlag =
-      Boolean.getBoolean("classLoaderOptimizationOptOut");
-
   private static final String[] EMPTY = new String[0];
 
   // Added here instead of AgentInstaller's ignores because it's relatively
@@ -139,7 +136,6 @@ public abstract class InstrumentationModule {
     InstrumentationContextProvider contextProvider = getContextProvider();
 
     AgentBuilder agentBuilder = parentAgentBuilder;
-    log.info("classLoaderOptimizationOptOutFlag: {}", classLoaderOptimizationOptOutFlag);
     for (TypeInstrumentation typeInstrumentation : typeInstrumentations) {
       AgentBuilder.Identified.Extendable extendableAgentBuilder =
           agentBuilder
@@ -148,10 +144,7 @@ public abstract class InstrumentationModule {
                       typeInstrumentation.typeMatcher(),
                       "Instrumentation type matcher unexpected exception: " + getClass().getName()),
                   failSafe(
-                      classLoaderOptimizationOptOutFlag
-                          ? moduleClassLoaderMatcher
-                          : moduleClassLoaderMatcher.and(
-                              typeInstrumentation.classLoaderOptimization()),
+                      moduleClassLoaderMatcher.and(typeInstrumentation.classLoaderOptimization()),
                       "Instrumentation class loader matcher unexpected exception: "
                           + getClass().getName()))
               .and(NOT_DECORATOR_MATCHER)
