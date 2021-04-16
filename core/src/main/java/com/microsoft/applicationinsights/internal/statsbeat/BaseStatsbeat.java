27ecb00d1181489fd7236857bfd3b64b44e5c75a
Helen Yang <heya@microsoft.com>
2021-04-16 12:59:29 -0700

    Put common custom dimensions in the base statsbeat

---
+++ core/src/main/java/com/microsoft/applicationinsights/internal/statsbeat/BaseStatsbeat.java
@@ -0,0 +1,135 @@
+/*
+ * ApplicationInsights-Java
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ *
+ * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the ""Software""), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package com.microsoft.applicationinsights.internal.statsbeat;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.TelemetryConfiguration;
+import com.microsoft.applicationinsights.internal.system.SystemInformation;
+import com.microsoft.applicationinsights.internal.util.PropertyHelper;
+import com.microsoft.applicationinsights.telemetry.MetricTelemetry;
+import com.microsoft.applicationinsights.telemetry.Telemetry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.microsoft.applicationinsights.internal.statsbeat.Constants.*;
+
+public abstract class BaseStatsbeat {
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseStatsbeat.class);
+    protected static volatile String resourceProvider;
+    protected static String operatingSystem;
+    protected TelemetryClient telemetryClient;
+
+    private String customerIkey;
+    private String version;
+    private String runtimeVersion;
+
+    public BaseStatsbeat() {
+        initializeCommonProperties();
+    }
+
+    /**
+     * @return the name of the resource provider
+     */
+    public String getResourceProvider() {
+        return resourceProvider;
+    }
+
+    public static void updateResourceProvider(String rp) {
+        resourceProvider = rp;
+        logger.debug("#### ResourceProvider has been updated from {} to {}", resourceProvider, rp);
+    }
+
+    /**
+     * @return the customer's iKey
+     */
+    public String getCustomerIkey() {
+        return customerIkey;
+    }
+
+    /**
+     * @return the operating system of the service or application that is being instrumented.
+     */
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    /**
+     * @return the version of the Java Codeless Agent
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    private void initializeCommonProperties() {
+        String sdkVersion = PropertyHelper.getQualifiedSdkVersionString();
+        if (sdkVersion.startsWith("awr")) {
+            resourceProvider = RP_APPSVC;
+            operatingSystem = OS_WINDOWS;
+        } else if (sdkVersion.startsWith("alr")) {
+            resourceProvider = RP_APPSVC;
+            operatingSystem = OS_LINUX;
+        } else if (sdkVersion.startsWith("kwr")) {
+            resourceProvider = RP_AKS;
+            operatingSystem = OS_WINDOWS;
+        } else if (sdkVersion.startsWith("klr")) {
+            resourceProvider = RP_AKS;
+            operatingSystem = OS_LINUX;
+        } else if (sdkVersion.startsWith("fwr")) {
+            resourceProvider = RP_FUNCTIONS;
+            operatingSystem = OS_WINDOWS;
+        } else if (sdkVersion.startsWith("flr")) {
+            resourceProvider = RP_FUNCTIONS;
+            operatingSystem = OS_LINUX;
+        } else if (sdkVersion.startsWith(LANGUAGE)) {
+            resourceProvider = RP_UNKNOWN;
+        }
+
+        if (operatingSystem == null) {
+            if (SystemInformation.INSTANCE.isWindows()) {
+                operatingSystem = OS_WINDOWS;
+            } else if (SystemInformation.INSTANCE.isUnix()) {
+                operatingSystem = OS_LINUX;
+            } else {
+                operatingSystem = OS_UNKNOW;
+            }
+        }
+
+        customerIkey = TelemetryConfiguration.getActive().getInstrumentationKey();
+        version = sdkVersion.substring(sdkVersion.lastIndexOf(':') + 1);
+        runtimeVersion = System.getProperty("java.version");
+    }
+
+    public abstract void send(TelemetryClient telemetryClient);
+
+    protected StatsbeatTelemetry createStatsbeatTelemetry(String name) {
+        StatsbeatTelemetry statsbeatTelemetry = new StatsbeatTelemetry(name, 0);
+        statsbeatTelemetry.getProperties().put(CUSTOM_DIMENSIONS_RP, resourceProvider);
+        statsbeatTelemetry.getProperties().put(CUSTOM_DIMENSIONS_ATTACH, ATTACH_CODELESS);
+        statsbeatTelemetry.getProperties().put(CUSTOM_DIMENSIONS_CIKEY, customerIkey);
+        statsbeatTelemetry.getProperties().put(CUSTOM_DIMENSIONS_RUNTIME_VERSION, runtimeVersion);
+        statsbeatTelemetry.getProperties().put(CUSTOM_DIMENSIONS_OS, operatingSystem);
+        statsbeatTelemetry.getProperties().put(CUSTOM_DIMENSIONS_LANGUAGE, LANGUAGE);
+        statsbeatTelemetry.getProperties().put(CUSTOM_DIMENSIONS_VERSION, version);
+        return statsbeatTelemetry;
+    }
+}
---
+++ core/src/main/java/com/microsoft/applicationinsights/internal/statsbeat/Constants.java
@@ -0,0 +1,73 @@
+/*
+ * ApplicationInsights-Java
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ *
+ * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the ""Software""), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package com.microsoft.applicationinsights.internal.statsbeat;
+
+import java.util.concurrent.TimeUnit;
+
+public final class Constants {
+
+    public static final String STATSBEAT_IKEY = "28ac6b6a-3fe0-43a9-bccd-10cb41d791c1";
+    public static final String STATSBEAT_ENDPOINT = "https://westus2-0.in.applicationinsights.azure.com/";
+    public static final String STATSBEAT_TELEMETRY_NAME = "Statsbeat";
+    public final static long DEFAULT_STATSBEAT_INTERVAL = TimeUnit.MINUTES.toSeconds(15); // default to 15 minutes
+
+    // rp
+    public static final String RP_FUNCTIONS = "functions";
+    public static final String RP_APPSVC = "appsvc";
+    public static final String RP_VM = "vm";
+    public static final String RP_AKS = "aks";
+    public static final String RP_UNKNOWN = "unknown";
+
+    public static final String LANGUAGE = "java";
+    public static final String ATTACH_CODELESS = "codeless";
+
+    public static final String OS_WINDOWS = "windows";
+    public static final String OS_LINUX = "linux";
+    public static final String OS_UNKNOW = "unknown";
+
+    // attach
+    public static final String WEBSITE_SITE_NAME = "appSrv_SiteName";
+    public static final String WEBSITE_HOSTNAME = "appSrv_wsHost";
+    public static final String WEBSITE_HOME_STAMPNAME = "appSrv_wsStamp";
+
+    // custom dimensions
+    public static final String CUSTOM_DIMENSIONS_RP = "rp";
+    public static final String CUSTOM_DIMENSIONS_RP_ID = "rpId";
+    public static final String CUSTOM_DIMENSIONS_ATTACH = "attach";
+    public static final String CUSTOM_DIMENSIONS_CIKEY = "cikey";
+    public static final String CUSTOM_DIMENSIONS_RUNTIME_VERSION = "runtimeVersion";
+    public static final String CUSTOM_DIMENSIONS_OS = "os";
+    public static final String CUSTOM_DIMENSIONS_LANGUAGE = "language";
+    public static final String CUSTOM_DIMENSIONS_VERSION = "version";
+    public static final String CUSTOM_DIMENSIONS_INSTRUMENTATION = "instrumentation";
+    public static final String CUSTOM_DIMENSIONS_FEATURE = "feature";
+
+    // features
+    public static final String JAVA_VENDOR_ORACLE = "Oracle Corporation"; // https://www.oracle.com/technetwork/java/javase/downloads/index.html
+    public static final String JAVA_VENDOR_ZULU = "Azul Systems, Inc."; // https://www.azul.com/downloads/zulu/
+    public static final String JAVA_VENDOR_MICROSOFT = "Microsoft"; // https://www.microsoft.com/openjdk
+    public static final String JAVA_VENDOR_ADOPT_OPENJDK = "AdoptOpenJDK"; // https://adoptopenjdk.net/
+    public static final String JAVA_VENDOR_REDHAT = "Red Hat, Inc."; // https://developers.redhat.com/products/openjdk/download/
+    public static final String JAVA_VENDOR_OTHER = "other";
+
+    private Constants() {}
+}
