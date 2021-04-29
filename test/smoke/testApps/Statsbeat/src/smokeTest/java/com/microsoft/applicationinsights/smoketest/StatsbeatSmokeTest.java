de46cc8cb53d6c7bab5637b7610f7d01d11ae6ce
Helen Yang <heya@microsoft.com>
2021-04-29 09:54:53 -0700

    Validate more statsbeats in the smoke test

--- test/smoke/testApps/Statsbeat/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/StatsbeatSmokeTest.java
+++ test/smoke/testApps/Statsbeat/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/StatsbeatSmokeTest.java
@@ -35,6 +35,48 @@ public class StatsbeatSmokeTest extends AiSmokeTest {
         assertNotNull(data.getProperties().get("version"));
         assertNotNull(data.getProperties().get("feature"));
         assertEquals(8, data.getProperties().size());
+
+        List<Envelope> attachMetrics = mockedIngestion.waitForItems(getMetricPredicate("Attach"), 2, 70, TimeUnit.SECONDS);
+        assertEquals(2, attachMetrics.size());
+
+        MetricData attachData = (MetricData) ((Data<?>) attachMetrics.get(0).getData()).getBaseData();
+        assertNotNull(attachData.getProperties().get("rp"));
+        assertNotNull(attachData.getProperties().get("attach"));
+        assertNotNull(attachData.getProperties().get("cikey"));
+        assertNotNull(attachData.getProperties().get("runtimeVersion"));
+        assertNotNull(attachData.getProperties().get("os"));
+        assertNotNull(attachData.getProperties().get("language"));
+        assertNotNull(attachData.getProperties().get("version"));
+        assertNotNull(attachData.getProperties().get("rpId"));
+        assertEquals(8, data.getProperties().size());
+
+        List<Envelope> requestSuccessCountMetrics = mockedIngestion.waitForItems(getMetricPredicate("Request Success Count"), 2, 70, TimeUnit.SECONDS);
+        assertEquals(2, requestSuccessCountMetrics.size());
+
+        MetricData requestSuccessCountData = (MetricData) ((Data<?>) requestSuccessCountMetrics.get(0).getData()).getBaseData();
+        assertNotNull(requestSuccessCountData.getProperties().get("rp"));
+        assertNotNull(requestSuccessCountData.getProperties().get("attach"));
+        assertNotNull(requestSuccessCountData.getProperties().get("cikey"));
+        assertNotNull(requestSuccessCountData.getProperties().get("runtimeVersion"));
+        assertNotNull(requestSuccessCountData.getProperties().get("os"));
+        assertNotNull(requestSuccessCountData.getProperties().get("language"));
+        assertNotNull(requestSuccessCountData.getProperties().get("version"));
+        assertNotNull(requestSuccessCountData.getProperties().get("instrumentation"));
+        assertEquals(8, data.getProperties().size());
+
+        List<Envelope> requestDurationMetrics = mockedIngestion.waitForItems(getMetricPredicate("Request Duration"), 2, 70, TimeUnit.SECONDS);
+        assertEquals(2, requestDurationMetrics.size());
+
+        MetricData requestDurationData = (MetricData) ((Data<?>) requestDurationMetrics.get(0).getData()).getBaseData();
+        assertNotNull(requestDurationData.getProperties().get("rp"));
+        assertNotNull(requestDurationData.getProperties().get("attach"));
+        assertNotNull(requestDurationData.getProperties().get("cikey"));
+        assertNotNull(requestDurationData.getProperties().get("runtimeVersion"));
+        assertNotNull(requestDurationData.getProperties().get("os"));
+        assertNotNull(requestDurationData.getProperties().get("language"));
+        assertNotNull(requestDurationData.getProperties().get("version"));
+        assertNotNull(requestDurationData.getProperties().get("instrumentation"));
+        assertEquals(8, data.getProperties().size());
     }
 
     private static Predicate<Envelope> getMetricPredicate(String name) {
