caf8e4a17845b750b3aebc1cb5b07c47d145227c
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-04-29 19:57:28 -0700

    oops

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/wasbootstrap/configuration/ConfigurationBuilder.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/wasbootstrap/configuration/ConfigurationBuilder.java
@@ -334,7 +334,7 @@ public class ConfigurationBuilder {
     }
 
     // never returns empty string (empty string is normalized to null)
-    private static String getEnvVar(String name) {
+    protected static String getEnvVar(String name) {
         return trimAndEmptyToNull(System.getenv(name));
     }
 
