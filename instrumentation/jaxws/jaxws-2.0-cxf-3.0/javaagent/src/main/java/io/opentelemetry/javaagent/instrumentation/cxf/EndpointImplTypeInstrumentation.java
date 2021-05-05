0323dfd01ab6d2e04d659ddd5189b62c14f68527
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-05-05 16:53:04 -0700

    Support OpenEJB CXF

--- instrumentation/jaxws/jaxws-2.0-cxf-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/cxf/EndpointImplTypeInstrumentation.java
+++ instrumentation/jaxws/jaxws-2.0-cxf-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/cxf/EndpointImplTypeInstrumentation.java
@@ -6,36 +6,37 @@
 package io.opentelemetry.javaagent.instrumentation.cxf;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.returns;
+import static net.bytebuddy.matcher.ElementMatchers.takesNoArguments;
 
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
-import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.endpoint.Server;
 
 public class EndpointImplTypeInstrumentation implements TypeInstrumentation {
 
   @Override
   public ElementMatcher<TypeDescription> typeMatcher() {
-    return named("org.apache.cxf.jaxws.EndpointImpl");
+    return named("org.apache.cxf.jaxws.JaxWsServerFactoryBean");
   }
 
   @Override
   public void transform(TypeTransformer transformer) {
     transformer.applyAdviceToMethod(
-        named("getServer").and(takesArgument(0, named(String.class.getName()))),
-        EndpointImplTypeInstrumentation.class.getName() + "$GetServerAdvice");
+        named("create")
+            .and(takesNoArguments().and(returns(named("org.apache.cxf.endpoint.Server")))),
+        EndpointImplTypeInstrumentation.class.getName() + "$CreateServerAdvice");
   }
 
-  public static class GetServerAdvice {
+  public static class CreateServerAdvice {
 
-    @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(@Advice.This EndpointImpl endpoint) {
-      if (endpoint.isPublished()) {
-        return;
-      }
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void onEnter(@Advice.Return Server server) {
+      Endpoint endpoint = server.getEndpoint();
       endpoint.getInInterceptors().add(new TracingStartInInterceptor());
       endpoint.getInInterceptors().add(new TracingEndInInterceptor());
       endpoint.getOutFaultInterceptors().add(new TracingOutFaultInterceptor());
