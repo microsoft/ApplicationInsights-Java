5d6462a53846df9a119ca713c6aced915ad430e0
Helen Y <56097766+heyams@users.noreply.github.com>
2021-05-27 13:47:02 -0700

    Retry webflux tests 3 times when it's failing (#1712)
    * Retry webflux 3 times when it's failing
    
    * print stack trace when the final retry fails
    
    * Reword
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

---
+++ test/smoke/framework/testCore/src/main/java/com/microsoft/applicationinsights/smoketest/Retry.java
@@ -0,0 +1,40 @@
+package com.microsoft.applicationinsights.smoketest;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class Retry implements TestRule {
+
+    private final int retryCount;
+
+    public Retry(int retryCount) {
+        this.retryCount = retryCount;
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        return statement(base, description);
+    }
+
+    private Statement statement(Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                Throwable caughtThrowable = null;
+                for (int i = 0; i < retryCount; i++) {
+                    try {
+                        base.evaluate();  // this will invoke test method, when the test fails, it will throw an AssertionError.
+                        return;
+                    } catch (Throwable t) {
+                        caughtThrowable = t;
+                        System.err.println(description.getDisplayName() + ": run " + (i+1) + " failed");
+                    }
+                }
+                System.err.println(description.getDisplayName() + ": giving up after " + retryCount + " failures");
+                caughtThrowable.printStackTrace();
+                throw caughtThrowable;
+            }
+        };
+    }
+}
