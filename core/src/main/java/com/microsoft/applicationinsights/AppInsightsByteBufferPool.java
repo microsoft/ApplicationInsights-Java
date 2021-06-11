16c2e176887041f24e8b61221ad06c4d257f0b55
Helen Yang <heya@microsoft.com>
2021-06-11 16:36:02 -0700

    Rename ByteBufferPool to AppInsightsByteBufferPool as it conflicts with another gradle dependency

--- core/src/main/java/com/microsoft/applicationinsights/AppInsightsByteBufferPool.java
+++ core/src/main/java/com/microsoft/applicationinsights/AppInsightsByteBufferPool.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
-class ByteBufferPool {
+class AppInsightsByteBufferPool {
 
     private static final int BYTE_BUFFER_SIZE = 65536;
     private static final int MAX_RETAINED = 10;
