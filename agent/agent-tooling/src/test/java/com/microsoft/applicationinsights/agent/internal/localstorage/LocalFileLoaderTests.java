47ce8661518fb9ac2199d5ba967e88b2e3785125
Helen Yang <heya@microsoft.com>
2021-07-01 14:14:17 -0700

    Fix spotless

--- agent/agent-tooling/src/test/java/com/microsoft/applicationinsights/agent/internal/localstorage/LocalFileLoaderTests.java
+++ agent/agent-tooling/src/test/java/com/microsoft/applicationinsights/agent/internal/localstorage/LocalFileLoaderTests.java
@@ -153,12 +153,13 @@ public class LocalFileLoaderTests {
 
   @Test
   public void testWriteGzipRawByte() throws IOException {
-    String text = "1. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore \n"
-        + "2. magna aliquyam erat, sed diam voluptua. \n"
-        + "3. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum \n"
-        + "4. dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore\n"
-        + "5. magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, \n"
-        + "6. no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+    String text =
+        "1. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore \n"
+            + "2. magna aliquyam erat, sed diam voluptua. \n"
+            + "3. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum \n"
+            + "4. dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore\n"
+            + "5. magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, \n"
+            + "6. no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
     // gzip
     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
