896dec956e9660305c6674359c0376f2841c7cc2
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-07-17 17:58:09 -0700

    micrometer

--- instrumentation/micrometer-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/micrometer/ActuatorInstrumentationModule.java
+++ instrumentation/micrometer-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/micrometer/ActuatorInstrumentationModule.java
@@ -5,14 +5,14 @@
 
 package io.opentelemetry.javaagent.instrumentation.micrometer;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
+
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.Arrays;
 import java.util.List;
 import net.bytebuddy.matcher.ElementMatcher;
-
-import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 
 @AutoService(InstrumentationModule.class)
 public class ActuatorInstrumentationModule extends InstrumentationModule {
