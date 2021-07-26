772dbaf680be96381ca35183e38a4db2c1b4b9ec
Helen Yang <heya@microsoft.com>
2021-07-26 10:35:54 -0700

    Fix file not found

--- .scripts/git-remote-checkout-e2e-tests.cmd
+++ .scripts/git-remote-checkout-e2e-tests.cmd
@@ -1,5 +1,5 @@
 set FOLDER=%~dp0
-set FILE=%~dp0git-remote-checkout-e2etests.ps1
+set FILE=%~dp0git-remote-checkout-e2e-tests.ps1
 
 powershell.exe -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Hidden -File "%FILE%" -RepoUrl https://github.com/microsoft/ApplicationInsights-E2E-Tests.git -BranchName main -RemoteName AppInsights2ETest -RepoFolder $pwd\path\to\checkout
 powershell.exe -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Hidden -File "%FILE%" -RepoUrl https://cloudes.visualstudio.com/CDPX/_git/CDPX-Pipeline-Engine -BranchName main -RemoteName AppInsights2ETest -RepoFolder $pwd\path\to\checkout
