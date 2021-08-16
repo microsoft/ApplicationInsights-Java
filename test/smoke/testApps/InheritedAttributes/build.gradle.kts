15679d85cdd9f17ea80c1bc0ffed267c4b2c47e3
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-08-16 09:52:55 -0700

    Add smoke test

---
+++ test/smoke/testApps/InheritedAttributes/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+  id("ai.smoke-test-jar")
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web:2.1.7.RELEASE")
+
+  implementation("com.microsoft.azure:applicationinsights-web")
+}
