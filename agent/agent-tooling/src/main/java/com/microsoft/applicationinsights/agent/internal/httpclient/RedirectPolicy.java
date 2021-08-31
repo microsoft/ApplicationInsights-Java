d35e2a56e8efcee814413ba275372ebdc48200cc
Helen Yang <heya@microsoft.com>
2021-08-30 18:27:26 -0700

    Comment out code

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/httpclient/RedirectPolicy.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/httpclient/RedirectPolicy.java
@@ -84,8 +84,6 @@ public final class RedirectPolicy implements HttpPipelinePolicy {
                   cacheRedirectUrl(
                       responseLocation, instrumentationKey, originalHttpRequest.getUrl());
                   context.setHttpRequest(originalHttpRequest.copy().setUrl(responseLocation));
-                  statsbeatModule.sendNetworkStatsbeatOnRedirect(
-                      instrumentationKey, originalHttpRequest.getUrl().toString());
                   return attemptRetry(context, next, originalHttpRequest, retryCount + 1);
                 }
               }
--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/StatsbeatModule.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/StatsbeatModule.java
@@ -133,15 +133,15 @@ public class StatsbeatModule {
     return instrumentationStatsbeat;
   }
 
-  // send network statsbeat whenever redirect happens since url has been changed.
+  // TODO (heya) send network statsbeat whenever redirect happens since url has been changed.
   // new url is always retrieved from the redirect policy cache map and we don't update the
   // endpoint.
-  public void sendNetworkStatsbeatOnRedirect(String ikey, String originalEndpoint) {
-    if (!disabledAll) {
-      networkStatsbeat.sendOriginalEndpointCounterOnRedirect(
-          telemetryClient, ikey, originalEndpoint);
-    }
-  }
+  //  public void sendNetworkStatsbeatOnRedirect(String ikey, String originalEndpoint) {
+  //    if (!disabledAll) {
+  //      networkStatsbeat.sendOriginalEndpointCounterOnRedirect(
+  //          telemetryClient, ikey, originalEndpoint);
+  //    }
+  //  }
 
   /** Runnable which is responsible for calling the send method to transmit Statsbeat telemetry. */
   private static class StatsbeatSender implements Runnable {
