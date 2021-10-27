a401caf5fa0b68edd8b124650773a7cc74bbfdff
Helen Yang <heya@microsoft.com>
2021-10-27 11:48:22 -0700

    Remove this

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/NonessentialStatsbeat.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/NonessentialStatsbeat.java
@@ -47,13 +47,13 @@ public class NonessentialStatsbeat extends BaseStatsbeat {
 
   @Override
   protected void send(TelemetryClient telemetryClient) {
-    if (this.readFailureCount.get() != 0) {
+    if (readFailureCount.get() != 0) {
       TelemetryItem telemetryItem =
           createStatsbeatTelemetry(telemetryClient, READ_FAILURE_COUNT, readFailureCount.get());
       telemetryClient.trackStatsbeatAsync(telemetryItem);
     }
 
-    if (this.writeFailureCount.get() != 0) {
+    if (writeFailureCount.get() != 0) {
       TelemetryItem telemetryItem =
           createStatsbeatTelemetry(telemetryClient, WRITE_FAILURE_COUNT, writeFailureCount.get());
       telemetryClient.trackStatsbeatAsync(telemetryItem);
