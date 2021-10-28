c87d034a49127071a74af93abacae2de7cf54696
Helen Yang <heya@microsoft.com>
2021-10-28 15:57:42 -0700

    Cleanup instrumentations

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/Instrumentations.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/Instrumentations.java
@@ -36,33 +36,28 @@ class Instrumentations {
   private static final Map<String, Integer> INSTRUMENTATION_MAP;
 
   static {
-    // TODO (heya) in OTel 1.4.0, all of these names change, dropping the ".javaagent" component
     INSTRUMENTATION_MAP = new HashMap<>();
     INSTRUMENTATION_MAP.put("io.opentelemetry.apache-httpasyncclient-4.1", 0);
     INSTRUMENTATION_MAP.put("io.opentelemetry.apache-httpclient-2.0", 1);
     INSTRUMENTATION_MAP.put("io.opentelemetry.apache-httpclient-4.0", 2);
     INSTRUMENTATION_MAP.put("io.opentelemetry.apache-httpclient-5.0", 3);
     INSTRUMENTATION_MAP.put("io.opentelemetry.applicationinsights-web-2.3", 4);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.azure-functions", 5);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.tomcat-7.0", 5);
     INSTRUMENTATION_MAP.put("Azure-OpenTelemetry", 6); // bridged by azure-core-1.14 module
     INSTRUMENTATION_MAP.put("io.opentelemetry.cassandra-3.0", 7);
     INSTRUMENTATION_MAP.put("io.opentelemetry.cassandra-4.0", 8);
-    // TODO (heya) this is non-span producing instrumentation
-    INSTRUMENTATION_MAP.put("io.opentelemetry.classloaders", 9);
-    // TODO (heya) this is non-span producing instrumentation
-    INSTRUMENTATION_MAP.put("io.opentelemetry.eclipse-osgi-3.6", 10);
-    // TODO (heya) this is non-span producing instrumentation
-    INSTRUMENTATION_MAP.put("io.opentelemetry.executors", 11);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.java-http-client", 9);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.rabbitmq-2.7", 10);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-integration-4.1", 11);
     INSTRUMENTATION_MAP.put("io.opentelemetry.grpc-1.6", 12);
     INSTRUMENTATION_MAP.put("io.opentelemetry.http-url-connection", 13);
-    // TODO (heya) this is not instrumentation name anymore
-    INSTRUMENTATION_MAP.put("io.opentelemetry.java-util-logging", 14);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.servlet-5.0", 14);
     INSTRUMENTATION_MAP.put("io.opentelemetry.java-util-logging-spans", 15);
     INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-1.0", 16);
     INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-2.0-common", 17);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-2.0-jersey-2.0", 18);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-2.0-resteasy-3.0", 19);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-2.0-resteasy-3.1", 20);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.async-http-client-1.9", 18);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.async-http-client-2.0", 19);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.google-http-client-1.19", 20);
     INSTRUMENTATION_MAP.put("io.opentelemetry.jdbc", 21);
     INSTRUMENTATION_MAP.put("io.opentelemetry.jedis-1.4", 22);
     INSTRUMENTATION_MAP.put("io.opentelemetry.jedis-3.0", 23);
@@ -70,57 +65,33 @@ class Instrumentations {
     INSTRUMENTATION_MAP.put("io.opentelemetry.jms-1.1", 25);
     INSTRUMENTATION_MAP.put("io.opentelemetry.kafka-clients-0.11", 26);
     INSTRUMENTATION_MAP.put("io.opentelemetry.kafka-streams-0.11", 27);
-    // TODO (heya) this is non-span producing instrumentation
-    INSTRUMENTATION_MAP.put("io.opentelemetry.kotlinx-coroutines", 28);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.jetty-httpclient-9.2", 28);
     INSTRUMENTATION_MAP.put("io.opentelemetry.lettuce-4.0", 29);
     INSTRUMENTATION_MAP.put("io.opentelemetry.lettuce-5.0", 30);
     INSTRUMENTATION_MAP.put("io.opentelemetry.lettuce-5.1", 31);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.lettuce-common", 32);
-    // TODO (heya) this is not instrumentation name anymore
-    INSTRUMENTATION_MAP.put("io.opentelemetry.log4j-2.0", 33);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-rabbit-1.0", 32);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-client-2.0", 33);
     INSTRUMENTATION_MAP.put("io.opentelemetry.log4j-spans-1.2", 34);
     INSTRUMENTATION_MAP.put("io.opentelemetry.log4j-spans-2.0", 35);
-    // TODO (heya) this is not instrumentation name anymore
-    INSTRUMENTATION_MAP.put("io.opentelemetry.logback-1.0", 36);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-client-2.0-resteasy-3.0", 36);
     INSTRUMENTATION_MAP.put("io.opentelemetry.logback-spans-1.0", 37);
     INSTRUMENTATION_MAP.put("io.opentelemetry.micrometer-1.0", 38);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.mongo-3.1", 39);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.mongo-3.7", 40);
-    // TODO (heya) add mongo-4.0
-    INSTRUMENTATION_MAP.put("io.opentelemetry.mongo-async-3.3", 41);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.mongo-common", 42);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.mongo-3.1", 39); // mongo 4.0 is covered in 3.1
+    INSTRUMENTATION_MAP.put("io.opentelemetry.grizzly-2.0", 40);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.quartz-2.0", 41);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.apache-camel-2.20", 42);
     INSTRUMENTATION_MAP.put("io.opentelemetry.netty-4.0", 43);
     INSTRUMENTATION_MAP.put("io.opentelemetry.netty-4.1", 44);
     INSTRUMENTATION_MAP.put("io.opentelemetry.okhttp-3.0", 45);
     INSTRUMENTATION_MAP.put("io.opentelemetry.opentelemetry-annotations-1.0", 46);
-    // TODO (heya) this is non-span producing instrumentation
-    INSTRUMENTATION_MAP.put("io.opentelemetry.opentelemetry-api-1.0", 47);
-    // TODO (heya) this is non-span producing instrumentation
-    INSTRUMENTATION_MAP.put("io.opentelemetry.reactor-3.1", 48);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.reactor-netty-0.9", 49);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.akka-http-10.0", 47);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-webmvc-3.1", 48);
+    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-webflux-5.0", 49);
     INSTRUMENTATION_MAP.put("io.opentelemetry.reactor-netty-1.0", 50);
     INSTRUMENTATION_MAP.put("io.opentelemetry.servlet-2.2", 51);
     INSTRUMENTATION_MAP.put("io.opentelemetry.servlet-3.0", 52);
     INSTRUMENTATION_MAP.put("io.opentelemetry.servlet-common", 53);
     INSTRUMENTATION_MAP.put("io.opentelemetry.spring-scheduling-3.1", 54);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-webflux-5.0", 55);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-webmvc-3.1", 56);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.tomcat-7.0", 57);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.java-http-client", 58);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.rabbitmq-2.7", 59);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-integration-4.1", 60);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.servlet-5.0", 61);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.async-http-client-1.9", 62);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.async-http-client-2.0", 63);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.google-http-client-1.19", 64);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.jetty-httpclient-9.2", 65);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.spring-rabbit-1.0", 66);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-client-2.0", 67);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.jaxrs-client-2.0-resteasy-3.0", 68);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.grizzly-2.0", 69);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.quartz-2.0", 70);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.apache-camel-2.20", 71);
-    INSTRUMENTATION_MAP.put("io.opentelemetry.akka-http-10.0", 72);
   }
 
   // encode BitSet to a long
