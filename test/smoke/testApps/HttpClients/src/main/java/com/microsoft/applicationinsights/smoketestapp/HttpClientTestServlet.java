46e60d54deb1223aec89a3c568116e3e4c45aed0
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-11-04 13:14:46 -0700

    Add http client 4xx and 5xx smoke tests (#1951)
    * Add http client 4xx and 5xx smoke tests
    
    * bump submodule

--- test/smoke/testApps/HttpClients/src/main/java/com/microsoft/applicationinsights/smoketestapp/HttpClientTestServlet.java
+++ test/smoke/testApps/HttpClients/src/main/java/com/microsoft/applicationinsights/smoketestapp/HttpClientTestServlet.java
@@ -37,8 +37,6 @@ import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.cookie.CookieSpecBase;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -60,8 +58,8 @@ public class HttpClientTestServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
     try {
-      int statusCode = doGetInternal(req);
-      resp.getWriter().println(statusCode);
+      doGetInternal(req);
+      resp.getWriter().println("hi!");
     } catch (ServletException e) {
       throw e;
     } catch (Exception e) {
@@ -69,102 +67,103 @@ public class HttpClientTestServlet extends HttpServlet {
     }
   }
 
-  private int doGetInternal(HttpServletRequest req) throws Exception {
+  private void doGetInternal(HttpServletRequest req) throws Exception {
     String pathInfo = req.getPathInfo();
-    if (pathInfo.equals("/")) {
-      return 200;
-    } else if (pathInfo.equals("/apacheHttpClient4")) {
-      return apacheHttpClient4();
-    } else if (pathInfo.equals("/apacheHttpClient4WithResponseHandler")) {
-      return apacheHttpClient4WithResponseHandler();
-    } else if (pathInfo.equals("/apacheHttpClient3")) {
-      return apacheHttpClient3();
-    } else if (pathInfo.equals("/apacheHttpAsyncClient")) {
-      return apacheHttpAsyncClient();
-    } else if (pathInfo.equals("/okHttp3")) {
-      return okHttp3();
-    } else if (pathInfo.equals("/okHttp2")) {
-      return okHttp2();
-    } else if (pathInfo.equals("/springWebClient")) {
-      return springWebClient();
-    } else if (pathInfo.equals("/httpUrlConnection")) {
-      return httpUrlConnection();
-    } else {
-      throw new ServletException("Unexpected url: " + pathInfo);
+    final ExecuteGetUrl executeGetUrl;
+    switch (pathInfo) {
+      case "/":
+        executeGetUrl = null;
+        break;
+      case "/apacheHttpClient4":
+        executeGetUrl = this::apacheHttpClient4;
+        break;
+      case "/apacheHttpClient4WithResponseHandler":
+        executeGetUrl = this::apacheHttpClient4WithResponseHandler;
+        break;
+      case "/apacheHttpClient3":
+        executeGetUrl = this::apacheHttpClient3;
+        break;
+      case "/apacheHttpAsyncClient":
+        executeGetUrl = this::apacheHttpAsyncClient;
+        break;
+      case "/okHttp3":
+        executeGetUrl = this::okHttp3;
+        break;
+      case "/okHttp2":
+        executeGetUrl = this::okHttp2;
+        break;
+      case "/springWebClient":
+        executeGetUrl = this::springWebClient;
+        break;
+      case "/httpUrlConnection":
+        executeGetUrl = this::httpUrlConnection;
+        break;
+      default:
+        throw new ServletException("Unexpected url: " + pathInfo);
+    }
+
+    if (executeGetUrl != null) {
+      executeGetUrl.execute("https://mock.codes/200?q=spaces%20test");
+      try {
+        executeGetUrl.execute("https://mock.codes/404");
+      } catch (Exception e) {
+        // HttpURLConnection throws exception on 404 and 500
+      }
+      try {
+        executeGetUrl.execute("https://mock.codes/500");
+      } catch (Exception e) {
+        // HttpURLConnection throws exception on 404 and 500
+      }
     }
   }
 
-  private int apacheHttpClient4() throws IOException {
-    String url = "https://www.bing.com/search?q=spaces%20test";
+  private void apacheHttpClient4(String url) throws IOException {
     HttpGet get = new HttpGet(url);
-    try (CloseableHttpResponse response = httpClient.execute(get)) {
-      return response.getStatusLine().getStatusCode();
-    }
+    httpClient.execute(get).close();
   }
 
-  private int apacheHttpClient4WithResponseHandler() throws IOException {
-    String url = "https://www.bing.com/search?q=spaces%20test";
+  private void apacheHttpClient4WithResponseHandler(String url) throws IOException {
     HttpGet get = new HttpGet(url);
-    return httpClient.execute(
-        get,
-        new ResponseHandler<Integer>() {
-          @Override
-          public Integer handleResponse(HttpResponse response) {
-            return response.getStatusLine().getStatusCode();
-          }
-        });
+    httpClient.execute(get, response -> response.getStatusLine().getStatusCode());
   }
 
-  private int apacheHttpClient3() throws IOException {
+  private void apacheHttpClient3(String url) throws IOException {
     HttpClient httpClient3 = new org.apache.commons.httpclient.HttpClient();
     CookiePolicy.registerCookieSpec("PermitAllCookiesSpec", PermitAllCookiesSpec.class);
     httpClient3.getParams().setCookiePolicy("PermitAllCookiesSpec");
-    String url = "https://www.bing.com/search?q=spaces%20test";
     GetMethod httpGet = new GetMethod(url);
     httpClient3.executeMethod(httpGet);
     httpGet.releaseConnection();
-    return httpGet.getStatusCode();
   }
 
-  private int apacheHttpAsyncClient() throws ExecutionException, InterruptedException, IOException {
-    HttpGet get = new HttpGet("https://www.bing.com/search?q=spaces%20test");
+  private void apacheHttpAsyncClient(String url)
+      throws ExecutionException, InterruptedException, IOException {
+    HttpGet get = new HttpGet(url);
     HttpResponse httpResponse = httpAsyncClient.execute(get, null).get();
     httpResponse.getEntity().getContent().close();
-    return httpResponse.getStatusLine().getStatusCode();
   }
 
-  private int okHttp3() throws IOException {
+  private void okHttp3(String url) throws IOException {
     okhttp3.OkHttpClient client = new okhttp3.OkHttpClient();
-    okhttp3.Request request =
-        new okhttp3.Request.Builder().url("https://www.bing.com/search?q=spaces%20test").build();
+    okhttp3.Request request = new okhttp3.Request.Builder().url(url).build();
     okhttp3.Response response = client.newCall(request).execute();
     response.body().close();
-    return response.code();
+    response.code();
   }
 
-  private int okHttp2() throws IOException {
+  private void okHttp2(String url) throws IOException {
     com.squareup.okhttp.OkHttpClient client = new com.squareup.okhttp.OkHttpClient();
     com.squareup.okhttp.Request request =
-        new com.squareup.okhttp.Request.Builder()
-            .url("https://www.bing.com/search?q=spaces%20test")
-            .build();
-    com.squareup.okhttp.Response response = client.newCall(request).execute();
-    response.body().close();
-    return response.code();
+        new com.squareup.okhttp.Request.Builder().url(url).build();
+    client.newCall(request).execute().body().close();
   }
 
-  private int springWebClient() {
-    return WebClient.create()
-        .get()
-        .uri("https://www.bing.com/search?q=spaces%20test")
-        .exchange()
-        .map(response -> response.statusCode())
-        .block()
-        .value();
+  private void springWebClient(String url) {
+    WebClient.create().get().uri(url).exchange().map(response -> response.statusCode()).block();
   }
 
-  private int httpUrlConnection() throws IOException {
-    URL obj = new URL("https://www.bing.com/search?q=spaces%20test");
+  private void httpUrlConnection(String url) throws IOException {
+    URL obj = new URL(url);
     HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
     // calling getContentType() first, since this triggered a bug previously in the instrumentation
     // previously
@@ -174,7 +173,11 @@ public class HttpClientTestServlet extends HttpServlet {
     byte[] buffer = new byte[1024];
     while (content.read(buffer) != -1) {}
     content.close();
-    return connection.getResponseCode();
+  }
+
+  @FunctionalInterface
+  interface ExecuteGetUrl {
+    void execute(String url) throws Exception;
   }
 
   public static class PermitAllCookiesSpec extends CookieSpecBase {
