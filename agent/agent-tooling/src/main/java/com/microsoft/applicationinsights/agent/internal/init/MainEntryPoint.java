0007b9c9a3407fed3a8cf502cf5c0717ad5ffff0
Helen Yang <heya@microsoft.com>
2021-11-18 15:06:21 -0800

    Fix lgtm

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/MainEntryPoint.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/MainEntryPoint.java
@@ -121,7 +121,7 @@ public class MainEntryPoint {
           StatusFile.putValueAndWrite(
               "AgentInitializedSuccessfully", success, startupLogger != null);
         } else {
-          if (DiagnosticsHelper.useAppSvcRpIntegrationLogging()) {
+          if (DiagnosticsHelper.useAppSvcRpIntegrationLogging() && startupLogger != null) {
             startupLogger.info(
                 "Detected running on a read-only file system. Status json file won't be created. If this is unexpected, please check that process has write access to the directory: {}",
                 StatusFile.directory);
