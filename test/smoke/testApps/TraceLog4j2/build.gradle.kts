38958ec137cc9ca60db4bbe38ca62ce46b4d64a6
Neel Gopaul <neelgopaul24@gmail.com>
2021-12-30 12:55:31 +0400

    Update Log4j version to 2.17.1

--- test/smoke/testApps/TraceLog4j2/build.gradle.kts
+++ test/smoke/testApps/TraceLog4j2/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 dependencies {
-  implementation("org.apache.logging.log4j:log4j-api:2.17.0")
-  implementation("org.apache.logging.log4j:log4j-core:2.17.0")
+  implementation("org.apache.logging.log4j:log4j-api:2.17.1")
+  implementation("org.apache.logging.log4j:log4j-core:2.17.1")
   implementation("com.microsoft.azure:applicationinsights-web") {
     // applicationinsights-core is embedded in applicationinsights-web
     // and duplicate class files produces lots of warning messages on jetty
