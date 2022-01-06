ab0b0473302e85a5690eab453fa9575f8e1d4679
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-06 15:18:28 -0800

    Bump submodule

--- agent/instrumentation/build.gradle.kts
+++ agent/instrumentation/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
   implementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-internal-class-loader:$otelInstrumentationAlphaVersion")
   implementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-internal-eclipse-osgi-3.6:$otelInstrumentationAlphaVersion")
   implementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-internal-lambda:$otelInstrumentationAlphaVersion")
+  implementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-internal-lambda-java9:$otelInstrumentationAlphaVersion")
   implementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-internal-reflection:$otelInstrumentationAlphaVersion")
   implementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-internal-url-class-loader:$otelInstrumentationAlphaVersion")
   implementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-java-http-client:$otelInstrumentationAlphaVersion")
