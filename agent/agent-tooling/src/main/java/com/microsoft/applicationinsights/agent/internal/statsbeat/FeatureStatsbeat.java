cff3174331c62d4574aad9b3de754478b8dbf7ac
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-10 15:00:06 -0800

    Add HTTP Headers feature statsbeat

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/FeatureStatsbeat.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/FeatureStatsbeat.java
@@ -159,5 +159,13 @@ public class FeatureStatsbeat extends BaseStatsbeat {
     if (!config.preview.captureHttpServer4xxAsError) {
       featureList.add(Feature.CAPTURE_HTTP_SERVER_4XX_AS_SUCCESS);
     }
+    if (!config.preview.captureHttpServerHeaders.requestHeaders.isEmpty()
+        || !config.preview.captureHttpServerHeaders.responseHeaders.isEmpty()) {
+      featureList.add(Feature.CAPTURE_HTTP_SERVER_HEADERS);
+    }
+    if (!config.preview.captureHttpClientHeaders.requestHeaders.isEmpty()
+        || !config.preview.captureHttpClientHeaders.responseHeaders.isEmpty()) {
+      featureList.add(Feature.CAPTURE_HTTP_CLIENT_HEADERS);
+    }
   }
 }
