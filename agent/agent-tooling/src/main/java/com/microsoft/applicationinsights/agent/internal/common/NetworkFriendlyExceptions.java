e68ffde81c8d51f115705f5634020916a10ae1ad
kryalama <66494519+kryalama@users.noreply.github.com>
2022-01-25 01:00:01 -0800

    adding unit tests

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/common/NetworkFriendlyExceptions.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/common/NetworkFriendlyExceptions.java
@@ -27,6 +27,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -35,63 +36,44 @@ import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLSocketFactory;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NetworkFriendlyExceptions {
-  public static final List<String> EXPECTED_CIPHERS =
-      Arrays.asList(
-          "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-          "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-          "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
-          "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256");
+
+  private static final List<FriendlyExceptionDetector> DETECTORS;
+  private static final Logger logger = LoggerFactory.getLogger(NetworkFriendlyExceptions.class);
+
+  static {
+    DETECTORS = new ArrayList<>();
+    try {
+      // Note this order is important to determine the right exception!
+      // For example SSLHandshakeException extends IOException
+      DETECTORS.add(SSLExceptionDetector.create());
+      DETECTORS.add(UnknownHostExceptionDetector.create());
+      DETECTORS.add(CipherExceptionDetector.create());
+    } catch (NoSuchAlgorithmException e) {
+      logger.debug(e.getMessage(), e);
+    }
+  }
 
   // returns true if the exception was "handled" and the caller should not log it
   public static boolean logSpecialOneTimeFriendlyException(
       Throwable error, String url, AtomicBoolean alreadySeen, Logger logger) {
-    // Handle SSL cert exceptions
-    SSLHandshakeException sslException = getCausedByOfType(error, SSLHandshakeException.class);
-    if (error instanceof SslHandshakeTimeoutException) {
-      return false;
-    }
-    if (sslException != null) {
-      if (!alreadySeen.getAndSet(true)) {
-        logger.error(getSslFriendlyMessage(url));
-      }
-      return true;
-    }
-    UnknownHostException unknownHostException =
-        getCausedByOfType(error, UnknownHostException.class);
-    if (unknownHostException != null && !alreadySeen.getAndSet(true)) {
-      // TODO log friendly message with instructions how to troubleshoot
-      //  e.g. wrong host address or cannot reach address due to network issues...
-      return false;
-    }
-
-    IOException ioException = getCausedByOfType(error, IOException.class);
-    if (ioException != null) {
-      boolean foundCiphers = true;
-      try {
-        foundCiphers = hasExpectedCiphers();
-      } catch (NoSuchAlgorithmException e) {
-        logger.debug(e.getMessage(), e);
-        return false;
-      }
-      if (!foundCiphers) {
-        if (!alreadySeen.getAndSet(true)) {
-          logger.error(getCipherFriendlyMessage(url));
-        }
-      } else {
-        return false;
-      }
-      return true;
-    }
-    return false;
+    return logSpecialOneTimeFriendlyException(error, url, alreadySeen, logger, DETECTORS);
   }
 
-  private static boolean hasExpectedCiphers() throws NoSuchAlgorithmException {
-    SSLSocketFactory socketFactory = SSLContext.getDefault().getSocketFactory();
-    List<String> cipherSuitesFromJvm = Arrays.asList(socketFactory.getSupportedCipherSuites());
-    for (String cipher : EXPECTED_CIPHERS) {
-      if (cipherSuitesFromJvm.contains(cipher)) {
+  public static boolean logSpecialOneTimeFriendlyException(
+      Throwable error,
+      String url,
+      AtomicBoolean alreadySeen,
+      Logger logger,
+      List<FriendlyExceptionDetector> detectors) {
+
+    for (FriendlyExceptionDetector detector : detectors) {
+      if (detector.detect(error)) {
+        if (!alreadySeen.getAndSet(true)) {
+          logger.error(detector.message(url));
+        }
         return true;
       }
     }
@@ -111,22 +93,6 @@ public class NetworkFriendlyExceptions {
     return getCausedByOfType(cause, type);
   }
 
-  private static String getSslFriendlyMessage(String url) {
-    return FriendlyException.populateFriendlyMessage(
-        "Unable to find valid certification path to requested target.",
-        getSslFriendlyExceptionAction(url),
-        getFriendlyExceptionBanner(url),
-        "This message is only logged the first time it occurs after startup.");
-  }
-
-  private static String getCipherFriendlyMessage(String url) {
-    return FriendlyException.populateFriendlyMessage(
-        "Probable root cause may be : missing cipher suites which are expected by the requested target.",
-        getCipherFriendlyExceptionAction(url),
-        getFriendlyExceptionBanner(url),
-        "This message is only logged the first time it occurs after startup.");
-  }
-
   private static String getFriendlyExceptionBanner(String url) {
     if (url.contains(DefaultEndpoints.LIVE_ENDPOINT)) {
       return "ApplicationInsights Java Agent failed to connect to Live metric end point.";
@@ -134,51 +100,160 @@ public class NetworkFriendlyExceptions {
     return "ApplicationInsights Java Agent failed to send telemetry data.";
   }
 
-  private static String getCipherFriendlyExceptionAction(String url) {
-    StringBuilder actionBuilder = new StringBuilder();
-    actionBuilder
-        .append(
-            "The Application Insights Java agent detects that you do not have any of the following cipher suites that are supported by the endpoint it connects to: "
-                + url)
-        .append("\n");
-    for (String missingCipher : EXPECTED_CIPHERS) {
-      actionBuilder.append(missingCipher).append("\n");
-    }
-    actionBuilder.append(
-        "Learn more about troubleshooting this network issue related to cipher suites here: https://go.microsoft.com/fwlink/?linkid=2185426");
-    return actionBuilder.toString();
+  interface FriendlyExceptionDetector {
+    boolean detect(Throwable error);
+
+    String message(String url);
   }
 
-  private static String getSslFriendlyExceptionAction(String url) {
-    String customJavaKeyStorePath = getCustomJavaKeystorePath();
-    if (customJavaKeyStorePath != null) {
+  static class SSLExceptionDetector implements FriendlyExceptionDetector {
+
+    static SSLExceptionDetector create() {
+      return new SSLExceptionDetector();
+    }
+
+    @Override
+    public boolean detect(Throwable error) {
+      if (error instanceof SslHandshakeTimeoutException) {
+        return false;
+      }
+      SSLHandshakeException sslException = getCausedByOfType(error, SSLHandshakeException.class);
+      if (sslException == null) {
+        return false;
+      }
+      return true;
+    }
+
+    @Override
+    public String message(String url) {
+      return FriendlyException.populateFriendlyMessage(
+          "Unable to find valid certification path to requested target.",
+          getSslFriendlyExceptionAction(url),
+          getFriendlyExceptionBanner(url),
+          "This message is only logged the first time it occurs after startup.");
+    }
+
+    private static String getJavaCacertsPath() {
+      String javaHome = System.getProperty("java.home");
+      return new File(javaHome, "lib/security/cacerts").getPath();
+    }
+
+    @Nullable
+    private static String getCustomJavaKeystorePath() {
+      String cacertsPath = System.getProperty("javax.net.ssl.trustStore");
+      if (cacertsPath != null) {
+        return new File(cacertsPath).getPath();
+      }
+      return null;
+    }
+
+    private static String getSslFriendlyExceptionAction(String url) {
+      String customJavaKeyStorePath = getCustomJavaKeystorePath();
+      if (customJavaKeyStorePath != null) {
+        return "Please import the SSL certificate from "
+            + url
+            + ", into your custom java key store located at:\n"
+            + customJavaKeyStorePath
+            + "\n"
+            + "Learn more about importing the certificate here: https://go.microsoft.com/fwlink/?linkid=2151450";
+      }
       return "Please import the SSL certificate from "
           + url
-          + ", into your custom java key store located at:\n"
-          + customJavaKeyStorePath
+          + ", into the default java key store located at:\n"
+          + getJavaCacertsPath()
           + "\n"
           + "Learn more about importing the certificate here: https://go.microsoft.com/fwlink/?linkid=2151450";
     }
-    return "Please import the SSL certificate from "
-        + url
-        + ", into the default java key store located at:\n"
-        + getJavaCacertsPath()
-        + "\n"
-        + "Learn more about importing the certificate here: https://go.microsoft.com/fwlink/?linkid=2151450";
   }
 
-  private static String getJavaCacertsPath() {
-    String javaHome = System.getProperty("java.home");
-    return new File(javaHome, "lib/security/cacerts").getPath();
-  }
+  static class UnknownHostExceptionDetector implements FriendlyExceptionDetector {
 
-  @Nullable
-  private static String getCustomJavaKeystorePath() {
-    String cacertsPath = System.getProperty("javax.net.ssl.trustStore");
-    if (cacertsPath != null) {
-      return new File(cacertsPath).getPath();
+    static UnknownHostExceptionDetector create() {
+      return new UnknownHostExceptionDetector();
     }
-    return null;
+
+    @Override
+    public boolean detect(Throwable error) {
+      UnknownHostException unknownHostException =
+          getCausedByOfType(error, UnknownHostException.class);
+      if (unknownHostException == null) {
+        return false;
+      }
+      return true;
+    }
+
+    @Override
+    public String message(String url) {
+      return FriendlyException.populateFriendlyMessage(
+          "Unable to resolve host in end point url",
+          getUnknownHostFriendlyExceptionAction(url),
+          getFriendlyExceptionBanner(url),
+          "This message is only logged the first time it occurs after startup.");
+    }
+
+    private static String getUnknownHostFriendlyExceptionAction(String url) {
+      return "Please upgrade your network to resolve the host in url :"
+          + url
+          + "\nLearn more about troubleshooting unknown host exception here: https://go.microsoft.com/fwlink/?linkid=2185830";
+    }
+  }
+
+  static class CipherExceptionDetector implements FriendlyExceptionDetector {
+
+    private final List<String> cipherSuitesFromJvm;
+    private static final List<String> EXPECTED_CIPHERS =
+        Arrays.asList(
+            "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256");
+
+    static CipherExceptionDetector create() throws NoSuchAlgorithmException {
+      SSLSocketFactory socketFactory = SSLContext.getDefault().getSocketFactory();
+      return new CipherExceptionDetector(Arrays.asList(socketFactory.getSupportedCipherSuites()));
+    }
+
+    CipherExceptionDetector(List<String> cipherSuitesFromJvm) {
+      this.cipherSuitesFromJvm = cipherSuitesFromJvm;
+    }
+
+    @Override
+    public boolean detect(Throwable error) {
+      IOException exception = getCausedByOfType(error, IOException.class);
+      if (exception == null) {
+        return false;
+      }
+      for (String cipher : EXPECTED_CIPHERS) {
+        if (cipherSuitesFromJvm.contains(cipher)) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    @Override
+    public String message(String url) {
+      return FriendlyException.populateFriendlyMessage(
+          "Probable root cause may be : missing cipher suites which are expected by the requested target.",
+          getCipherFriendlyExceptionAction(url),
+          getFriendlyExceptionBanner(url),
+          "This message is only logged the first time it occurs after startup.");
+    }
+
+    private static String getCipherFriendlyExceptionAction(String url) {
+      StringBuilder actionBuilder = new StringBuilder();
+      actionBuilder
+          .append(
+              "The Application Insights Java agent detects that you do not have any of the following cipher suites that are supported by the endpoint it connects to: "
+                  + url)
+          .append("\n");
+      for (String missingCipher : EXPECTED_CIPHERS) {
+        actionBuilder.append(missingCipher).append("\n");
+      }
+      actionBuilder.append(
+          "Learn more about troubleshooting this network issue related to cipher suites here: https://go.microsoft.com/fwlink/?linkid=2185426");
+      return actionBuilder.toString();
+    }
   }
 
   private NetworkFriendlyExceptions() {}
