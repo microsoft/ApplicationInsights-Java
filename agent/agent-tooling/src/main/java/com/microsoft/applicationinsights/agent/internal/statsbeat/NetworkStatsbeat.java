9170eafda646c469ecf901078ff0cae026056a28
Helen Yang <heya@microsoft.com>
2022-02-07 11:12:39 -0800

    Remove sout debugging

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/NetworkStatsbeat.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/statsbeat/NetworkStatsbeat.java
@@ -247,7 +247,6 @@ public class NetworkStatsbeat extends BaseStatsbeat {
    * e.g. endpointUrl 'https://westus-0.in.applicationinsights.azure.com/v2.1/track' host will
    * return 'westus-0'
    */
-  @SuppressWarnings("SystemOut")
   static String getHost(String endpointUrl) {
     Matcher matcher = hostPattern.matcher(endpointUrl);
 
