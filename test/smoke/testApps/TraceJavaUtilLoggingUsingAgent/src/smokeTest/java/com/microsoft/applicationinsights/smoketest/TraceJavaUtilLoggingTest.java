ced170582e9af80bf77c96e5180eac3c4fb142af
Helen Yang <heya@microsoft.com>
2022-03-01 17:00:17 -0800

    Fix java util logging

--- test/smoke/testApps/TraceJavaUtilLoggingUsingAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/TraceJavaUtilLoggingTest.java
+++ test/smoke/testApps/TraceJavaUtilLoggingUsingAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/TraceJavaUtilLoggingTest.java
@@ -57,7 +57,6 @@ public class TraceJavaUtilLoggingTest extends AiSmokeTest {
     assertEquals("This is jul warning.", md1.getMessage());
     assertEquals(SeverityLevel.Warning, md1.getSeverityLevel());
     assertEquals("Logger", md1.getProperties().get("SourceType"));
-    assertEquals("WARNING", md1.getProperties().get("LoggingLevel"));
     assertEquals("smoketestapp", md1.getProperties().get("LoggerName"));
 //    assertNotNull(md1.getProperties().get("ThreadName"));
     assertEquals(3, md1.getProperties().size());
@@ -65,7 +64,6 @@ public class TraceJavaUtilLoggingTest extends AiSmokeTest {
     assertEquals("This is jul severe.", md2.getMessage());
     assertEquals(SeverityLevel.Error, md2.getSeverityLevel());
     assertEquals("Logger", md2.getProperties().get("SourceType"));
-    assertEquals("SEVERE", md2.getProperties().get("LoggingLevel"));
     assertEquals("smoketestapp", md1.getProperties().get("LoggerName"));
 //    assertNotNull(md2.getProperties().get("ThreadName"));
     assertEquals(3, md2.getProperties().size());
@@ -97,10 +95,9 @@ public class TraceJavaUtilLoggingTest extends AiSmokeTest {
     assertEquals(SeverityLevel.Error, ed.getSeverityLevel());
     assertEquals("This is an exception!", ed.getProperties().get("Logger Message"));
     assertEquals("Logger", ed.getProperties().get("SourceType"));
-    assertEquals("SEVERE", ed.getProperties().get("LoggingLevel"));
     assertEquals("smoketestapp", ed.getProperties().get("LoggerName"));
 //    assertNotNull(ed.getProperties().get("ThreadName"));
-    assertEquals(4, ed.getProperties().size());
+    assertEquals(3, ed.getProperties().size());
 
     assertParentChild(
         rd,
