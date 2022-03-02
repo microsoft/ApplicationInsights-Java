8e268004fef1edab51d8c8710573c80fde6bf89b
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-03-01 17:47:20 -0800

    Fix build

--- agent/agent-tooling/src/test/java/com/azure/monitor/opentelemetry/exporter/implementation/quickpulse/QuickPulseTestBase.java
+++ agent/agent-tooling/src/test/java/com/azure/monitor/opentelemetry/exporter/implementation/quickpulse/QuickPulseTestBase.java
@@ -136,8 +136,6 @@ public class QuickPulseTestBase extends TestBase {
               .map(bytes -> new String(bytes, StandardCharsets.UTF_8));
       asyncString.subscribe(
           value -> {
-            System.out.println("================");
-            System.out.println(value);
             if (Pattern.matches(expectedRequestBody, value)) {
               countDown.countDown();
             }
