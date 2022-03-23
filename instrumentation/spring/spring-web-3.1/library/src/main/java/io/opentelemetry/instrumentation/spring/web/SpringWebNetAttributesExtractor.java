735e0fb3867495074c53292f8ea919d7a3c9e933
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-03-23 13:39:46 -0700

    Fix

--- instrumentation/spring/spring-web-3.1/library/src/main/java/io/opentelemetry/instrumentation/spring/web/SpringWebNetAttributesExtractor.java
+++ instrumentation/spring/spring-web-3.1/library/src/main/java/io/opentelemetry/instrumentation/spring/web/SpringWebNetAttributesExtractor.java
@@ -5,33 +5,33 @@
 
 package io.opentelemetry.instrumentation.spring.web;
 
-import io.opentelemetry.instrumentation.api.instrumenter.net.NetServerAttributesExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.net.NetClientAttributesExtractor;
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import javax.annotation.Nullable;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpResponse;
 
 final class SpringWebNetAttributesExtractor
-    extends NetServerAttributesExtractor<HttpRequest, ClientHttpResponse> {
+    extends NetClientAttributesExtractor<HttpRequest, ClientHttpResponse> {
   @Override
-  public String transport(HttpRequest httpRequest) {
+  public String transport(HttpRequest httpRequest, ClientHttpResponse response) {
     return SemanticAttributes.NetTransportValues.IP_TCP;
   }
 
   @Override
   @Nullable
-  public String peerName(HttpRequest httpRequest) {
+  public String peerName(HttpRequest httpRequest, ClientHttpResponse response) {
     return httpRequest.getURI().getHost();
   }
 
   @Override
-  public Integer peerPort(HttpRequest httpRequest) {
+  public Integer peerPort(HttpRequest httpRequest, ClientHttpResponse response) {
     return httpRequest.getURI().getPort();
   }
 
   @Override
   @Nullable
-  public String peerIp(HttpRequest httpRequest) {
+  public String peerIp(HttpRequest httpRequest, ClientHttpResponse response) {
     return null;
   }
 }
