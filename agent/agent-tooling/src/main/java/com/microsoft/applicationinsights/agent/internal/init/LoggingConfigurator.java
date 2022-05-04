43f50e9e102410fbe67f0e206e291a376c2e0133
Helen Yang <heya@microsoft.com>
2022-05-04 10:30:52 -0700

    Remove suppressWarning for stdout

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/LoggingConfigurator.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/LoggingConfigurator.java
@@ -202,7 +202,6 @@ public class LoggingConfigurator {
     return appender;
   }
 
-  @SuppressWarnings("SystemOut")
   private Appender<ILoggingEvent> configureConsoleAppender() {
     ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<>();
     appender.setContext(loggerContext);
