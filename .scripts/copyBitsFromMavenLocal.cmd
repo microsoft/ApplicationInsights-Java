5547a9ccb8d89d109e8104de7fc2e0e07a196d02
Helen Yang <heya@microsoft.com>
2022-05-10 13:40:26 -0700

    Delete unused cmd file

--- .scripts/copyBitsFromMavenLocal.cmd
+++
@@ -1,8 +0,0 @@
-echo "copy ~/.m2/repository/com/microsoft/azure/applicationinsights-agent/ to build/output"
-
-echo %~dp0
-pushd %~dp0..
-mkdir build/output
-copy ~/.m2/repository/com/microsoft/azure/applicationinsights-agent/* build/output/
-
-popd
