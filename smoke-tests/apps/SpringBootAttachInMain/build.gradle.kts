d6d7fd31d5eac910a46d0d7343d3aefa245d876b
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-05-15 16:00:14 -0700

    fix

--- smoke-tests/apps/SpringBootAttachInMain/build.gradle.kts
+++ smoke-tests/apps/SpringBootAttachInMain/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-  implementation(project(":agent:agent"))
+  implementation(project(":agent:agent", configuration = "shadow"))
 
   implementation("org.springframework.boot:spring-boot-starter-web:2.2.0.RELEASE")
   implementation("net.bytebuddy:byte-buddy-agent:1.11.0")
