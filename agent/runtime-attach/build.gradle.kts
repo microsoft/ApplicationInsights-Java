7062f58494f87d9cfd0a506afc46564c88c67edb
Jean Bisutti <jean.bisutti@gmail.com>
2022-06-14 12:33:00 +0200

    Update agent/runtime-attach/build.gradle.kts
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- agent/runtime-attach/build.gradle.kts
+++ agent/runtime-attach/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
   implementation("io.opentelemetry.contrib:opentelemetry-runtime-attach:1.14.0-alpha")
-  implementation("com.microsoft.azure:applicationinsights-agent:" + "3.2.11")
+  implementation(project(":agent:agent", configuration = "shadow"))
   testImplementation("io.opentelemetry:opentelemetry-extension-annotations:1.14.0")
   testImplementation("org.assertj:assertj-core:3.23.1")
   testImplementation("org.junit.jupiter:junit-jupiter-api:.8.2")
