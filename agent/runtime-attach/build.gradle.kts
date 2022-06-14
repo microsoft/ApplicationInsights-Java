a833cb5769a9eb4b62feb31eab8c06503e351888
jeanbisutti <jean.bisutti@gmail.com>
2022-06-14 12:53:19 +0200

    Remove hard coded dependency

--- agent/runtime-attach/build.gradle.kts
+++ agent/runtime-attach/build.gradle.kts
@@ -2,10 +2,13 @@ plugins {
   id("ai.java-conventions")
 }
 
+val otelInstrumentationAlphaVersion: String by project
+val otelVersion: String by project
+
 dependencies {
-  implementation("io.opentelemetry.contrib:opentelemetry-runtime-attach:1.14.0-alpha")
+  implementation("io.opentelemetry.contrib:opentelemetry-runtime-attach:$otelInstrumentationAlphaVersion")
   implementation(project(":agent:agent", configuration = "shadow"))
-  testImplementation("io.opentelemetry:opentelemetry-extension-annotations:1.14.0")
+  testImplementation("io.opentelemetry:opentelemetry-extension-annotations:$otelVersion")
   testImplementation("org.assertj:assertj-core:3.23.1")
   testImplementation("org.junit.jupiter:junit-jupiter-api:.8.2")
 }
