f865cd9abd26e42f059ee059d440e04a71982694
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-06-21 17:46:44 -0700

    Publish runtime-attach artifact to maven central

--- .scripts/copy-from-maven-local.cmd
+++ .scripts/copy-from-maven-local.cmd
@@ -3,5 +3,6 @@ pushd %~dp0..
 mkdir published
 
 xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-agent\* published
+xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-runtime-attach\* published
 
 popd
