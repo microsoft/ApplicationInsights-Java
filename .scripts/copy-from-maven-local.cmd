a2d1b92b355dc7ef14bb899019940d791de48c62
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-06-22 14:31:36 -0700

    Revert "Exclude maven-metadata-local.xml (#2347)"
    This reverts commit b327007dd07d2688495f84d51c069d19be52ba8f.

--- .scripts/copy-from-maven-local.cmd
+++ .scripts/copy-from-maven-local.cmd
@@ -2,7 +2,7 @@ pushd %~dp0..
 
 mkdir published
 
-xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-agent\* published /EXCLUDE:.scripts\list-of-excluded-files.txt
-xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-runtime-attach\* published /EXCLUDE:.scripts\list-of-excluded-files.txt
+xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-agent\* published
+xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-runtime-attach\* published
 
 popd
