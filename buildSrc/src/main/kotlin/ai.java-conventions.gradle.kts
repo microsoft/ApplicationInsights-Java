0fe80fe53d2bc1d15f87cb65ac9875dece106183
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-07-08 17:01:04 -0700

    Run unit tests against openj9 (and more)

--- buildSrc/src/main/kotlin/ai.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/ai.java-conventions.gradle.kts
@@ -117,6 +117,22 @@ tasks.withType<Test>().configureEach {
   }
 }
 
+afterEvaluate {
+  val testJavaVersion = gradle.startParameter.projectProperties["testJavaVersion"]?.let(JavaVersion::toVersion)
+  val useJ9 = gradle.startParameter.projectProperties["testJavaVM"]?.run { this == "openj9" }
+    ?: false
+  tasks.withType<Test>().configureEach {
+    if (testJavaVersion != null) {
+      javaLauncher.set(
+        javaToolchains.launcherFor {
+          languageVersion.set(JavaLanguageVersion.of(testJavaVersion.majorVersion))
+          implementation.set(if (useJ9) JvmImplementation.J9 else JvmImplementation.VENDOR_SPECIFIC)
+        }
+      )
+    }
+  }
+}
+
 checkstyle {
   configFile = rootProject.file("buildscripts/checkstyle.xml")
   // this version should match the version of google_checks.xml used as basis for above configuration
