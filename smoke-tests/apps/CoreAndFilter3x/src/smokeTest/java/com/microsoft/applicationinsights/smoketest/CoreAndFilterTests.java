0540e0043bd303f6238bedc6ab352bdbdc5ff0d6
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-07-28 11:19:19 -0700

    Update

--- smoke-tests/apps/CoreAndFilter3x/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/CoreAndFilterTests.java
+++ smoke-tests/apps/CoreAndFilter3x/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/CoreAndFilterTests.java
@@ -21,6 +21,10 @@
 
 package com.microsoft.applicationinsights.smoketest;
 
+import static com.microsoft.applicationinsights.smoketest.WarEnvironmentValue.TOMCAT_8_JAVA_11;
+import static com.microsoft.applicationinsights.smoketest.WarEnvironmentValue.TOMCAT_8_JAVA_11_OPENJ9;
+import static com.microsoft.applicationinsights.smoketest.WarEnvironmentValue.TOMCAT_8_JAVA_17;
+import static com.microsoft.applicationinsights.smoketest.WarEnvironmentValue.TOMCAT_8_JAVA_18;
 import static com.microsoft.applicationinsights.smoketest.WarEnvironmentValue.TOMCAT_8_JAVA_8;
 import static com.microsoft.applicationinsights.smoketest.WarEnvironmentValue.TOMCAT_8_JAVA_8_OPENJ9;
 import static com.microsoft.applicationinsights.smoketest.WarEnvironmentValue.WILDFLY_13_JAVA_8;
@@ -503,13 +507,23 @@ abstract class CoreAndFilterTests {
     assertThat(rdEnvelope.getTags()).containsEntry("ai.operation.name", operationName);
   }
 
-  // old Application Insights version that do not support Java 11+
-
   @Environment(TOMCAT_8_JAVA_8)
   static class Tomcat8Java8Test extends CoreAndFilterTests {}
 
   @Environment(TOMCAT_8_JAVA_8_OPENJ9)
   static class Tomcat8Java8OpenJ9Test extends CoreAndFilterTests {}
+
+  @Environment(TOMCAT_8_JAVA_11)
+  static class Tomcat8Java11Test extends CoreAndFilterTests {}
+
+  @Environment(TOMCAT_8_JAVA_11_OPENJ9)
+  static class Tomcat8Java11OpenJ9Test extends CoreAndFilterTests {}
+
+  @Environment(TOMCAT_8_JAVA_17)
+  static class Tomcat8Java17Test extends CoreAndFilterTests {}
+
+  @Environment(TOMCAT_8_JAVA_18)
+  static class Tomcat8Java18Test extends CoreAndFilterTests {}
 
   @Environment(WILDFLY_13_JAVA_8)
   static class Wildfly13Java8Test extends CoreAndFilterTests {}
