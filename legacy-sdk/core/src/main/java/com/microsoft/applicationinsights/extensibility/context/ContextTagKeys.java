50b7b6dcb408be88b51810f7798b239fe3cf0513
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-07-27 17:52:00 -0700

    Spotless

--- legacy-sdk/core/src/main/java/com/microsoft/applicationinsights/extensibility/context/ContextTagKeys.java
+++ legacy-sdk/core/src/main/java/com/microsoft/applicationinsights/extensibility/context/ContextTagKeys.java
@@ -164,128 +164,92 @@ public class ContextTagKeys {
 
   private String CloudRoleInstance;
 
-  /**
-   * @return current value of ApplicationId property
-   */
+  /** @return current value of ApplicationId property */
   public final String getApplicationId() {
     return this.ApplicationId;
   }
 
-  /**
-   * @param value new value of ApplicationId property
-   */
+  /** @param value new value of ApplicationId property */
   public final void setApplicationId(String value) {
     this.ApplicationId = value;
   }
 
-  /**
-   * @return current value of ApplicationVersion property
-   */
+  /** @return current value of ApplicationVersion property */
   public final String getApplicationVersion() {
     return this.ApplicationVersion;
   }
 
-  /**
-   * @param value new value of ApplicationVersion property
-   */
+  /** @param value new value of ApplicationVersion property */
   public final void setApplicationVersion(String value) {
     this.ApplicationVersion = value;
   }
 
-  /**
-   * @return current value of ApplicationTypeId property
-   */
+  /** @return current value of ApplicationTypeId property */
   public final String getApplicationTypeId() {
     return this.ApplicationTypeId;
   }
 
-  /**
-   * @param value new value of ApplicationTypeId property
-   */
+  /** @param value new value of ApplicationTypeId property */
   public final void setApplicationTypeId(String value) {
     this.ApplicationTypeId = value;
   }
 
-  /**
-   * @return current value of DeviceId property
-   */
+  /** @return current value of DeviceId property */
   public final String getDeviceId() {
     return this.DeviceId;
   }
 
-  /**
-   * @param value new value of DeviceId property
-   */
+  /** @param value new value of DeviceId property */
   public final void setDeviceId(String value) {
     this.DeviceId = value;
   }
 
-  /**
-   * @return current value of DeviceOS property
-   */
+  /** @return current value of DeviceOS property */
   public final String getDeviceOS() {
     return this.DeviceOS;
   }
 
-  /**
-   * @param value new value of DeviceOS property
-   */
+  /** @param value new value of DeviceOS property */
   public final void setDeviceOS(String value) {
     this.DeviceOS = value;
   }
 
-  /**
-   * @return current value of DeviceOSVersion property
-   */
+  /** @return current value of DeviceOSVersion property */
   public final String getDeviceOSVersion() {
     return this.DeviceOSVersion;
   }
 
-  /**
-   * @param value new value of DeviceOSVersion property
-   */
+  /** @param value new value of DeviceOSVersion property */
   public final void setDeviceOSVersion(String value) {
     this.DeviceOSVersion = value;
   }
 
-  /**
-   * @return current value of DeviceLocale property
-   */
+  /** @return current value of DeviceLocale property */
   public final String getDeviceLocale() {
     return this.DeviceLocale;
   }
 
-  /**
-   * @param value new value of DeviceLocale property
-   */
+  /** @param value new value of DeviceLocale property */
   public final void setDeviceLocale(String value) {
     this.DeviceLocale = value;
   }
 
-  /**
-   * @return current value of DeviceType property
-   */
+  /** @return current value of DeviceType property */
   public final String getDeviceType() {
     return this.DeviceType;
   }
 
-  /**
-   * @param value new value of DeviceType property
-   */
+  /** @param value new value of DeviceType property */
   public final void setDeviceType(String value) {
     this.DeviceType = value;
   }
 
-  /**
-   * @return current value of DeviceVMName property
-   */
+  /** @return current value of DeviceVMName property */
   public final String getDeviceVMName() {
     return this.DeviceVMName;
   }
 
-  /**
-   * @param value new value of DeviceVMName property
-   */
+  /** @param value new value of DeviceVMName property */
   public final void setDeviceVMName(String value) {
     this.DeviceVMName = value;
   }
@@ -299,9 +263,7 @@ public class ContextTagKeys {
     return this.DeviceRoleName;
   }
 
-  /**
-   * @param value new value of DeviceRoleName property
-   */
+  /** @param value new value of DeviceRoleName property */
   @Deprecated
   public final void setDeviceRoleName(String value) {
     this.DeviceRoleName = value;
@@ -316,458 +278,328 @@ public class ContextTagKeys {
     return this.DeviceRoleInstance;
   }
 
-  /**
-   * @param value new value of DeviceRoleInstance property
-   */
+  /** @param value new value of DeviceRoleInstance property */
   @Deprecated
   public final void setDeviceRoleInstance(String value) {
     this.DeviceRoleInstance = value;
   }
 
-  /**
-   * @return current value of DeviceOEMName property
-   */
+  /** @return current value of DeviceOEMName property */
   public final String getDeviceOEMName() {
     return this.DeviceOEMName;
   }
 
-  /**
-   * @param value new value of DeviceOEMName property
-   */
+  /** @param value new value of DeviceOEMName property */
   public final void setDeviceOEMName(String value) {
     this.DeviceOEMName = value;
   }
 
-  /**
-   * @return current value of DeviceModel property
-   */
+  /** @return current value of DeviceModel property */
   public final String getDeviceModel() {
     return this.DeviceModel;
   }
 
-  /**
-   * @param value new value of DeviceModel property
-   */
+  /** @param value new value of DeviceModel property */
   public final void setDeviceModel(String value) {
     this.DeviceModel = value;
   }
 
-  /**
-   * @return current value of DeviceNetwork property
-   */
+  /** @return current value of DeviceNetwork property */
   public final String getDeviceNetwork() {
     return this.DeviceNetwork;
   }
 
-  /**
-   * @param value new value of DeviceNetwork property
-   */
+  /** @param value new value of DeviceNetwork property */
   public final void setDeviceNetwork(String value) {
     this.DeviceNetwork = value;
   }
 
-  /**
-   * @return current value of DeviceScreenResolution property
-   */
+  /** @return current value of DeviceScreenResolution property */
   public final String getDeviceScreenResolution() {
     return this.DeviceScreenResolution;
   }
 
-  /**
-   * @param value new value of DeviceScreenResolution property
-   */
+  /** @param value new value of DeviceScreenResolution property */
   public final void setDeviceScreenResolution(String value) {
     this.DeviceScreenResolution = value;
   }
 
-  /**
-   * @return current value of DeviceLanguage property
-   */
+  /** @return current value of DeviceLanguage property */
   public final String getDeviceLanguage() {
     return this.DeviceLanguage;
   }
 
-  /**
-   * @param value new value of DeviceLanguage property
-   */
+  /** @param value new value of DeviceLanguage property */
   public final void setDeviceLanguage(String value) {
     this.DeviceLanguage = value;
   }
 
-  /**
-   * @return current value of DeviceIp property
-   */
+  /** @return current value of DeviceIp property */
   public final String getDeviceIp() {
     return this.DeviceIp;
   }
 
-  /**
-   * @param value new value of DeviceIp property
-   */
+  /** @param value new value of DeviceIp property */
   public final void setDeviceIp(String value) {
     this.DeviceIp = value;
   }
 
-  /**
-   * @return current value of LocationLatitude property
-   */
+  /** @return current value of LocationLatitude property */
   public final String getLocationLatitude() {
     return this.LocationLatitude;
   }
 
-  /**
-   * @param value new value of LocationLatitude property
-   */
+  /** @param value new value of LocationLatitude property */
   public final void setLocationLatitude(String value) {
     this.LocationLatitude = value;
   }
 
-  /**
-   * @return current value of LocationLongitude property
-   */
+  /** @return current value of LocationLongitude property */
   public final String getLocationLongitude() {
     return this.LocationLongitude;
   }
 
-  /**
-   * @param value new value of LocationLongitude property
-   */
+  /** @param value new value of LocationLongitude property */
   public final void setLocationLongitude(String value) {
     this.LocationLongitude = value;
   }
 
-  /**
-   * @return current value of LocationIP property
-   */
+  /** @return current value of LocationIP property */
   public final String getLocationIP() {
     return this.LocationIP;
   }
 
-  /**
-   * @param value new value of LocationIP property
-   */
+  /** @param value new value of LocationIP property */
   public final void setLocationIP(String value) {
     this.LocationIP = value;
   }
 
-  /**
-   * @return current value of LocationContinent property
-   */
+  /** @return current value of LocationContinent property */
   public final String getLocationContinent() {
     return this.LocationContinent;
   }
 
-  /**
-   * @param value new value of LocationContinent property
-   */
+  /** @param value new value of LocationContinent property */
   public final void setLocationContinent(String value) {
     this.LocationContinent = value;
   }
 
-  /**
-   * @return current value of LocationCountry property
-   */
+  /** @return current value of LocationCountry property */
   public final String getLocationCountry() {
     return this.LocationCountry;
   }
 
-  /**
-   * @param value new value of LocationCountry property
-   */
+  /** @param value new value of LocationCountry property */
   public final void setLocationCountry(String value) {
     this.LocationCountry = value;
   }
 
-  /**
-   * @return current value of LocationProvince property
-   */
+  /** @return current value of LocationProvince property */
   public final String getLocationProvince() {
     return this.LocationProvince;
   }
 
-  /**
-   * @param value new value of LocationProvince property
-   */
+  /** @param value new value of LocationProvince property */
   public final void setLocationProvince(String value) {
     this.LocationProvince = value;
   }
 
-  /**
-   * @return current value of LocationCity property
-   */
+  /** @return current value of LocationCity property */
   public final String getLocationCity() {
     return this.LocationCity;
   }
 
-  /**
-   * @param value new value of LocationCity property
-   */
+  /** @param value new value of LocationCity property */
   public final void setLocationCity(String value) {
     this.LocationCity = value;
   }
 
-  /**
-   * @return current value of OperationName property
-   */
+  /** @return current value of OperationName property */
   public final String getOperationName() {
     return this.OperationName;
   }
 
-  /**
-   * @param value new value of OperationName property
-   */
+  /** @param value new value of OperationName property */
   public final void setOperationName(String value) {
     this.OperationName = value;
   }
 
-  /**
-   * @return current value of OperationId property
-   */
+  /** @return current value of OperationId property */
   public final String getOperationId() {
     return this.OperationId;
   }
 
-  /**
-   * @param value new value of OperationId property
-   */
+  /** @param value new value of OperationId property */
   public final void setOperationId(String value) {
     this.OperationId = value;
   }
 
-  /**
-   * @return current value of SyntheticSource property
-   */
+  /** @return current value of SyntheticSource property */
   public final String getSyntheticSource() {
     return this.SyntheticSource;
   }
 
-  /**
-   * @param value new value of SyntheticSource property
-   */
+  /** @param value new value of SyntheticSource property */
   public final void setSyntheticSource(String value) {
     this.SyntheticSource = value;
   }
 
-  /**
-   * @return current value of OperationParentId property
-   */
+  /** @return current value of OperationParentId property */
   public final String getOperationParentId() {
     return this.OperationParentId;
   }
 
-  /**
-   * @param value new value of OperationParentId property
-   */
+  /** @param value new value of OperationParentId property */
   public final void setOperationParentId(String value) {
     this.OperationParentId = value;
   }
 
-  /**
-   * @return current value of OperationRootId property
-   */
+  /** @return current value of OperationRootId property */
   public final String getOperationRootId() {
     return this.OperationRootId;
   }
 
-  /**
-   * @param value new value of OperationRootId property
-   */
+  /** @param value new value of OperationRootId property */
   public final void setOperationRootId(String value) {
     this.OperationRootId = value;
   }
 
-  /**
-   * @return current value of SessionId property
-   */
+  /** @return current value of SessionId property */
   public final String getSessionId() {
     return this.SessionId;
   }
 
-  /**
-   * @param value new value of SessionId property
-   */
+  /** @param value new value of SessionId property */
   public final void setSessionId(String value) {
     this.SessionId = value;
   }
 
-  /**
-   * @return current value of SessionIsFirst property
-   */
+  /** @return current value of SessionIsFirst property */
   public final String getSessionIsFirst() {
     return this.SessionIsFirst;
   }
 
-  /**
-   * @param value new value of SessionIsFirst property
-   */
+  /** @param value new value of SessionIsFirst property */
   public final void setSessionIsFirst(String value) {
     this.SessionIsFirst = value;
   }
 
-  /**
-   * @return current value of SessionIsNew property
-   */
+  /** @return current value of SessionIsNew property */
   public final String getSessionIsNew() {
     return this.SessionIsNew;
   }
 
-  /**
-   * @param value new value of SessionIsNew property
-   */
+  /** @param value new value of SessionIsNew property */
   public final void setSessionIsNew(String value) {
     this.SessionIsNew = value;
   }
 
-  /**
-   * @return current value of UserType property
-   */
+  /** @return current value of UserType property */
   public final String getUserType() {
     return this.UserType;
   }
 
-  /**
-   * @param value new value of UserType property
-   */
+  /** @param value new value of UserType property */
   public final void setUserType(String value) {
     this.UserType = value;
   }
 
-  /**
-   * @return current value of UserId property
-   */
+  /** @return current value of UserId property */
   public final String getUserId() {
     return this.UserId;
   }
 
-  /**
-   * @param value new value of UserId property
-   */
+  /** @param value new value of UserId property */
   public final void setUserId(String value) {
     this.UserId = value;
   }
 
-  /**
-   * @return current value of UserAuthUserId property
-   */
+  /** @return current value of UserAuthUserId property */
   public final String getUserAuthUserId() {
     return this.UserAuthUserId;
   }
 
-  /**
-   * @param value new value of UserAuthUserId property
-   */
+  /** @param value new value of UserAuthUserId property */
   public final void setUserAuthUserId(String value) {
     this.UserAuthUserId = value;
   }
 
-  /**
-   * @return current value of UserAccountId property
-   */
+  /** @return current value of UserAccountId property */
   public final String getUserAccountId() {
     return this.UserAccountId;
   }
 
-  /**
-   * @param value new value of UserAccountId property
-   */
+  /** @param value new value of UserAccountId property */
   public final void setUserAccountId(String value) {
     this.UserAccountId = value;
   }
 
-  /**
-   * @return current value of UserAnonymousUserAcquisitionDate property
-   */
+  /** @return current value of UserAnonymousUserAcquisitionDate property */
   public final String getUserAnonymousUserAcquisitionDate() {
     return this.UserAnonymousUserAcquisitionDate;
   }
 
-  /**
-   * @param value new value of UserAnonymousUserAcquisitionDate property
-   */
+  /** @param value new value of UserAnonymousUserAcquisitionDate property */
   public final void setUserAnonymousUserAcquisitionDate(String value) {
     this.UserAnonymousUserAcquisitionDate = value;
   }
 
-  /**
-   * @return current value of UserAuthenticatedUserAcquisitionDate property
-   */
+  /** @return current value of UserAuthenticatedUserAcquisitionDate property */
   public final String getUserAuthenticatedUserAcquisitionDate() {
     return this.UserAuthenticatedUserAcquisitionDate;
   }
 
-  /**
-   * @param value new value of UserAuthenticatedUserAcquisitionDate property
-   */
+  /** @param value new value of UserAuthenticatedUserAcquisitionDate property */
   public final void setUserAuthenticatedUserAcquisitionDate(String value) {
     this.UserAuthenticatedUserAcquisitionDate = value;
   }
 
-  /**
-   * @return current value of UserAccountAcquisitionDate property
-   */
+  /** @return current value of UserAccountAcquisitionDate property */
   public final String getUserAccountAcquisitionDate() {
     return this.UserAccountAcquisitionDate;
   }
 
-  /**
-   * @param value new value of UserAccountAcquisitionDate property
-   */
+  /** @param value new value of UserAccountAcquisitionDate property */
   public final void setUserAccountAcquisitionDate(String value) {
     this.UserAccountAcquisitionDate = value;
   }
 
-  /**
-   * @return current value of UserAgent property
-   */
+  /** @return current value of UserAgent property */
   public final String getUserAgent() {
     return this.UserAgent;
   }
 
-  /**
-   * @param value new value of UserAgent property
-   */
+  /** @param value new value of UserAgent property */
   public final void setUserAgent(String value) {
     this.UserAgent = value;
   }
 
-  /**
-   * @return current value of SampleRate property
-   */
+  /** @return current value of SampleRate property */
   public final String getSampleRate() {
     return this.SampleRate;
   }
 
-  /**
-   * @param value new value of SampleRate property
-   */
+  /** @param value new value of SampleRate property */
   public final void setSampleRate(String value) {
     this.SampleRate = value;
   }
 
-  /**
-   * @return current value of InternalSdkVersion property
-   */
+  /** @return current value of InternalSdkVersion property */
   public final String getInternalSdkVersion() {
     return this.InternalSdkVersion;
   }
 
-  /**
-   * @param value new value of InternalSdkVersion property
-   */
+  /** @param value new value of InternalSdkVersion property */
   public final void setInternalSdkVersion(String value) {
     this.InternalSdkVersion = value;
   }
 
-  /**
-   * @return current value of InternalAgentVersion property
-   */
+  /** @return current value of InternalAgentVersion property */
   public final String getInternalAgentVersion() {
     return this.InternalAgentVersion;
   }
 
-  /**
-   * @param value new value of InternalAgentVersion property
-   */
+  /** @param value new value of InternalAgentVersion property */
   public final void setInternalAgentVersion(String value) {
     this.InternalAgentVersion = value;
   }
