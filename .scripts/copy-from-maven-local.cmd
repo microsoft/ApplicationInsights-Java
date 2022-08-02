ecb1a946f12b11bed005412c50c45c4ce58a35c4
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-08-02 13:40:55 -0700

    Add new artifacts to release pipeline

--- .scripts/copy-from-maven-local.cmd
+++ .scripts/copy-from-maven-local.cmd
@@ -2,8 +2,12 @@ pushd %~dp0..
 
 mkdir published\agent
 mkdir published\runtime-attach
+mkdir published\core
+mkdir published\web
 
 xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-agent\* published\agent
 xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-runtime-attach\* published\runtime-attach
+xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-core\* published\core
+xcopy /E %USERPROFILE%\.m2\repository\com\microsoft\azure\applicationinsights-web\* published\web
 
 popd
