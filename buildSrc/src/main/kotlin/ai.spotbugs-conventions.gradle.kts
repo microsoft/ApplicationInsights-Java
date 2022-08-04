5fa2d969cade9eda0e20f1ff972bb632a0c959a8
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-08-03 18:14:26 -0700

    Add comment (#2430)

--- buildSrc/src/main/kotlin/ai.spotbugs-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/ai.spotbugs-conventions.gradle.kts
@@ -6,6 +6,9 @@ spotbugs {
   omitVisitors.addAll(
     // we only use spotbugs for the findsecbugs plugin, and suppress anything else that gets flagged
     // since we use errorprone instead for this kind of static analysis
+    //
+    // the visitor names can be found in https://github.com/spotbugs/spotbugs/blob/master/spotbugs/etc/findbugs.xml
+    // and https://github.com/find-sec-bugs/find-sec-bugs/blob/master/findsecbugs-plugin/src/main/resources/metadata/findbugs.xml
     "CheckRelaxingNullnessAnnotation",
     "FindDeadLocalStores",
     "FindNullDeref",
