111c773ea5acd510708f34aef6c28d6d8b65a2f2
Helen Yang <heya@microsoft.com>
2022-08-23 17:10:53 -0700

    Fix wrong import

--- agent/azure-monitor-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/MetricDataMapper.java
+++ agent/azure-monitor-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/MetricDataMapper.java
@@ -49,7 +49,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import jdk.internal.jline.internal.Nullable;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
