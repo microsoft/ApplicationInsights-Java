95c15d8c6eece634b6a41a48c771d93c7b26806e
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-09-09 15:22:37 -0700

    Add tool to unsign (#2485)
    * Add tool to unsign
    
    * review feedback
    
    * errorprone
    
    * spotless
    
    * errorprone
    
    * extract class
    
    * errorprone
    
    * errorprone

--- agent/agent/build.gradle.kts
+++ agent/agent/build.gradle.kts
@@ -133,6 +133,7 @@ tasks {
       attributes(
         "Agent-Class" to "com.microsoft.applicationinsights.agent.Agent",
         "Premain-Class" to "com.microsoft.applicationinsights.agent.Agent",
+        "Main-Class" to "com.microsoft.applicationinsights.agent.Main",
         "Can-Redefine-Classes" to true,
         "Can-Retransform-Classes" to true
       )
