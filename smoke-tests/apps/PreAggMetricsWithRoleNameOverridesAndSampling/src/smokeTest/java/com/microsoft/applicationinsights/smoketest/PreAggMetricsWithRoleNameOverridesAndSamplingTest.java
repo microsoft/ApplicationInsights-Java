aa3e16b404c217cf7487e1f3f88a034f02ed47a4
Helen Yang <heya@microsoft.com>
2022-09-15 14:44:44 -0700

    Disable PreAggMetricsWithRoleNameOverridesAndSamplingTest for now until a fix is available

--- smoke-tests/apps/PreAggMetricsWithRoleNameOverridesAndSampling/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/PreAggMetricsWithRoleNameOverridesAndSamplingTest.java
+++ smoke-tests/apps/PreAggMetricsWithRoleNameOverridesAndSampling/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/PreAggMetricsWithRoleNameOverridesAndSamplingTest.java
@@ -27,9 +27,11 @@ import com.microsoft.applicationinsights.smoketest.schemav2.SeverityLevel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+@Disabled
 @UseAgent
 abstract class PreAggMetricsWithRoleNameOverridesAndSamplingTest {
 
