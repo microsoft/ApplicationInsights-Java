836c59b0da5e49489761f22dd995781cac9b152e
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-09-24 13:00:49 -0700

    Fix log4j1 tests

--- smoke-tests/apps/TraceLog4j1_2UsingAgent/src/main/java/com/microsoft/applicationinsights/smoketestapp/SimpleTestTraceLog4j12Servlet.java
+++ smoke-tests/apps/TraceLog4j1_2UsingAgent/src/main/java/com/microsoft/applicationinsights/smoketestapp/SimpleTestTraceLog4j12Servlet.java
@@ -3,6 +3,7 @@
 
 package com.microsoft.applicationinsights.smoketestapp;
 
+import java.lang.reflect.Field;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +11,24 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.MDC;
+import org.apache.log4j.helpers.Loader;
 
 @WebServlet("/traceLog4j12")
 public class SimpleTestTraceLog4j12Servlet extends HttpServlet {
+
+  static {
+    try {
+      // this is needed because log4j1 incorrectly thinks the initial releases of Java 10-19
+      // (which have no '.' in their versions since there is no minor version) are Java 1.1,
+      // which is before ThreadLocal was introduced and so log4j1 disables MDC functionality
+      // (and the MDC tests below fail)
+      Field field = Loader.class.getDeclaredField("java1");
+      field.setAccessible(true);
+      field.set(null, false);
+    } catch (IllegalAccessException | NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   private static final Logger logger = LogManager.getLogger("smoketestapp");
 
