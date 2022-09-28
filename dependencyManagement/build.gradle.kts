c84ad94e481f34e6d5e1c8f126708b3d3d5098c9
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-09-28 14:37:30 -0700

    Update errorprone

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -41,8 +41,8 @@ val CORE_DEPENDENCIES = listOf(
   "com.google.auto.service:auto-service-annotations:1.0.1",
   "com.google.auto.value:auto-value:1.9",
   "com.google.auto.value:auto-value-annotations:1.9",
-  "com.google.errorprone:error_prone_annotations:2.14.0",
-  "com.google.errorprone:error_prone_core:2.14.0",
+  "com.google.errorprone:error_prone_annotations:2.15.0",
+  "com.google.errorprone:error_prone_core:2.15.0",
   // When updating, also update buildSrc/build.gradle.kts
   "net.bytebuddy:byte-buddy:1.12.10",
   "net.bytebuddy:byte-buddy-dep:1.12.10",
