31d844c7350b84d2860407494e3ca7427bf12624
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-10-30 12:25:32 -0700

    Spotless

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/profiler/util/ServiceLoaderUtil.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/profiler/util/ServiceLoaderUtil.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.applicationinsights.agent.internal.profiler.util;
 
 import java.util.Iterator;
