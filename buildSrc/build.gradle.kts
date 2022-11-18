950f6c68b2ad3b283e5f28c1e65def350c3dee53
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-11-18 11:01:52 -0800

    Remove unused buildSrc dependencies (#2718)
    (this was copied over from otel where they are needed for building the
    muzzle plugin)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,29 +25,15 @@ tasks.withType<Test>().configureEach {
 dependencies {
   implementation(gradleApi())
 
-  implementation("org.eclipse.aether:aether-connector-basic:1.1.0")
-  implementation("org.eclipse.aether:aether-transport-http:1.1.0")
-  implementation("org.apache.maven:maven-aether-provider:3.3.9")
-
   // When updating, update above in plugins too
   implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
   implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:5.0.13")
-  implementation("com.google.guava:guava:31.1-jre")
   implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
-  implementation("org.ow2.asm:asm:9.4")
-  implementation("org.ow2.asm:asm-tree:9.4")
-  implementation("org.apache.httpcomponents:httpclient:4.5.13")
   implementation("org.gradle:test-retry-gradle-plugin:1.4.1")
 
   implementation("org.owasp:dependency-check-gradle:7.3.0")
 
   implementation("io.opentelemetry.instrumentation:gradle-plugins:1.20.0-alpha")
-
-  // we need to use byte buddy variant that does not shade asm
-  implementation("net.bytebuddy:byte-buddy-gradle-plugin:1.12.19") {
-    exclude(group = "net.bytebuddy", module = "byte-buddy")
-  }
-  implementation("net.bytebuddy:byte-buddy-dep:1.12.19")
 
   implementation("net.ltgt.gradle:gradle-errorprone-plugin:3.0.1")
   implementation("net.ltgt.gradle:gradle-nullaway-plugin:1.5.0")
