d58877251702e1a07fa58afad524be3cfa16d5a1
John Oliver <1615532+johnoliver@users.noreply.github.com>
2023-01-17 21:58:17 +0000

    tidy gradle

--- smoke-tests/apps/DiagnosticExtension/build.gradle.kts
+++ smoke-tests/apps/DiagnosticExtension/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
 }
 
 dependencies {
-  implementation(project(mapOf("path" to ":agent:agent-profiler:agent-diagnostics-api")))
-  implementation(project(mapOf("path" to ":agent:agent-profiler:agent-alerting-api")))
-  implementation(project(mapOf("path" to ":smoke-tests:apps:DiagnosticExtension:MockExtension")))
-  testImplementation(project(mapOf("path" to ":smoke-tests:framework")))
+  implementation(project(":agent:agent-profiler:agent-diagnostics-api"))
+  implementation(project(":agent:agent-profiler:agent-alerting-api"))
+  implementation(project(":smoke-tests:apps:DiagnosticExtension:MockExtension"))
+  testImplementation(project(":smoke-tests:framework"))
   implementation("org.springframework.boot:spring-boot-starter-web:2.1.7.RELEASE")
 }
