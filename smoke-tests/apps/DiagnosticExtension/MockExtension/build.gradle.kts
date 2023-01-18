6ddb7a6f5d637c676cc02e68d23122cc66bf47fe
John Oliver <1615532+johnoliver@users.noreply.github.com>
2023-01-18 13:00:04 +0000

    Set extension version to java 8

--- smoke-tests/apps/DiagnosticExtension/MockExtension/build.gradle.kts
+++ smoke-tests/apps/DiagnosticExtension/MockExtension/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
   `java`
 }
 
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(8))
+  }
+}
+
 dependencies {
   compileOnly(project(":agent:agent-profiler:agent-diagnostics-api"))
   compileOnly(project(":agent:agent-profiler:agent-alerting-api"))
