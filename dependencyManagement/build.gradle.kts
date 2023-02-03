a88c0f6c147dd95c354ba417ccbc5a06b8dea428
siyuniu-ms <siyuniu@microsoft.com>
2023-02-03 11:35:21 -0800

    Revert "remove lockerfile"
    This reverts commit 1e326539c8bf268f83dcee89ae9525d5e42d3e8c.

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -12,8 +12,8 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 val otelVersion = "1.22.0"
-val otelInstrumentationVersion = "1.22.1"
-val otelInstrumentationAlphaVersion = "1.22.1-alpha"
+val otelInstrumentationVersion = "1.23.0-SNAPSHOT"
+val otelInstrumentationAlphaVersion = "1.23.0-alpha-SNAPSHOT"
 val otelContribAlphaVersion = "1.18.0-alpha"
 
 rootProject.extra["otelVersion"] = otelVersion
@@ -70,6 +70,7 @@ val CORE_DEPENDENCIES = listOf(
   "io.opentelemetry.javaagent:opentelemetry-javaagent-extension-api:${otelInstrumentationAlphaVersion}",
   "io.opentelemetry.javaagent:opentelemetry-javaagent-bootstrap:${otelInstrumentationAlphaVersion}",
   "io.opentelemetry.javaagent:opentelemetry-javaagent-tooling:${otelInstrumentationAlphaVersion}",
+  "io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-servlet-common-bootstrap:${otelInstrumentationAlphaVersion}",
   // temporarily overriding transitive dependency from azure-core until next azure-bom release
   // which targets at least reactor-netty-http:1.1.1
   "io.projectreactor.netty:reactor-netty-http:1.1.2"
@@ -96,6 +97,7 @@ javaPlatform {
 }
 
 dependencies {
+
   for (bom in DEPENDENCY_BOMS) {
     api(enforcedPlatform(bom))
     val split = bom.split(':')
@@ -113,6 +115,7 @@ dependencies {
       dependencyVersions[split[0]] = split[2]
     }
   }
+//  implementation (files(''))
 }
 
 fun isNonStable(version: String): Boolean {
