493ad56a2d2a5ad07423f67ea497371cf78bd95c
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-02-21 14:59:27 -0800

    less

--- smoke-tests/apps/DotInJmxMetric/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DotInJmxMetricTest.java
+++ smoke-tests/apps/DotInJmxMetric/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DotInJmxMetricTest.java
@@ -4,14 +4,10 @@
 package com.microsoft.applicationinsights.smoketest;
 
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_11;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_11_OPENJ9;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_17;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_19;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_20;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_8;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_8_OPENJ9;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDFLY_13_JAVA_8;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDFLY_13_JAVA_8_OPENJ9;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.microsoft.applicationinsights.smoketest.schemav2.Data;
@@ -58,14 +54,8 @@ abstract class DotInJmxMetricTest {
   @Environment(TOMCAT_8_JAVA_8)
   static class Tomcat8Java8Test extends DotInJmxMetricTest {}
 
-  @Environment(TOMCAT_8_JAVA_8_OPENJ9)
-  static class Tomcat8Java8OpenJ9Test extends DotInJmxMetricTest {}
-
   @Environment(TOMCAT_8_JAVA_11)
   static class Tomcat8Java11Test extends DotInJmxMetricTest {}
-
-  @Environment(TOMCAT_8_JAVA_11_OPENJ9)
-  static class Tomcat8Java11OpenJ9Test extends DotInJmxMetricTest {}
 
   @Environment(TOMCAT_8_JAVA_17)
   static class Tomcat8Java17Test extends DotInJmxMetricTest {}
@@ -75,10 +65,4 @@ abstract class DotInJmxMetricTest {
 
   @Environment(TOMCAT_8_JAVA_20)
   static class Tomcat8Java20Test extends DotInJmxMetricTest {}
-
-  @Environment(WILDFLY_13_JAVA_8)
-  static class Wildfly13Java8Test extends DotInJmxMetricTest {}
-
-  @Environment(WILDFLY_13_JAVA_8_OPENJ9)
-  static class Wildfly13Java8OpenJ9Test extends DotInJmxMetricTest {}
 }
