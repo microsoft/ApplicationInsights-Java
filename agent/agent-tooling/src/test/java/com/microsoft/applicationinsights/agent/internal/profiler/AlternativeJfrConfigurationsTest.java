c7c13d8da14a875390c20f2912872f1855d79353
John Oliver <1615532+johnoliver@users.noreply.github.com>
2023-03-08 17:13:51 +0000

    When diagnostics are enabled, automatically use DIAGNOSTIC_PROFILE

---
+++ agent/agent-tooling/src/test/java/com/microsoft/applicationinsights/agent/internal/profiler/AlternativeJfrConfigurationsTest.java
@@ -0,0 +1,82 @@
+package com.microsoft.applicationinsights.agent.internal.profiler;
+
+import com.microsoft.applicationinsights.agent.internal.configuration.Configuration;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.function.Consumer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AlternativeJfrConfigurationsTest {
+
+  private static void assertForAllConfigs(Configuration.ProfilerConfiguration config,
+      Consumer<String> assertion) {
+
+    assertion.accept(
+        AlternativeJfrConfigurations
+            .getCpuProfileConfig(config)
+            .getConfiguration());
+
+    assertion.accept(
+        AlternativeJfrConfigurations
+            .getMemoryProfileConfig(config)
+            .getConfiguration());
+
+    assertion.accept(
+        AlternativeJfrConfigurations
+            .getManualProfileConfig(config)
+            .getConfiguration());
+
+    assertion.accept(
+        AlternativeJfrConfigurations
+            .getSpanProfileConfig(config)
+            .getConfiguration());
+  }
+
+  @Test
+  public void ifDiagnosticsAreEnabled_A_CustomJfcFileIsNotOverridden() throws IOException {
+    File tmpfile = File.createTempFile("config", "jfc");
+    try {
+      try (BufferedWriter fw = Files.newBufferedWriter(tmpfile.toPath(), StandardCharsets.UTF_8)) {
+        fw.write("a-jfc-file");
+      }
+
+      Configuration.ProfilerConfiguration config = new Configuration.ProfilerConfiguration();
+
+      config.cpuTriggeredSettings = tmpfile.getAbsolutePath();
+      config.manualTriggeredSettings = tmpfile.getAbsolutePath();
+      config.memoryTriggeredSettings = tmpfile.getAbsolutePath();
+
+      config.enableDiagnostics = true;
+      assertForAllConfigs(config, (fileContent) -> {
+        Assertions.assertEquals("a-jfc-file", fileContent);
+      });
+
+    } finally {
+      tmpfile.delete();
+    }
+  }
+
+  @Test
+  public void ifDiagnosticsAreEnabledDefaultToDiagnosticProfile() {
+    Configuration.ProfilerConfiguration config = new Configuration.ProfilerConfiguration();
+    config.enableDiagnostics = true;
+    assertForAllConfigs(config, fileContent -> {
+      Assertions.assertTrue(
+          fileContent.contains("com.microsoft.jeg.illuminate.events.jfr.DiagnosticEvent"));
+    });
+  }
+
+  @Test
+  public void ifDiagnosticsAreDisabledDoesNotUseDiagnosticProfile() {
+    Configuration.ProfilerConfiguration config = new Configuration.ProfilerConfiguration();
+    config.enableDiagnostics = false;
+    assertForAllConfigs(config, fileContent -> {
+      Assertions.assertFalse(
+          fileContent.contains("com.microsoft.jeg.illuminate.events.jfr.DiagnosticEvent"));
+    });
+  }
+}
