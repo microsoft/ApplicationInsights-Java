7b4a37e9401184decb7792a7dd5113bd23f2effa
Helen Yang <heya@microsoft.com>
2023-03-14 10:44:58 -0700

    Add a comment

--- smoke-tests/apps/Statsbeat/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/StatsbeatTest.java
+++ smoke-tests/apps/Statsbeat/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/StatsbeatTest.java
@@ -86,6 +86,7 @@ abstract class StatsbeatTest {
     assertThat(metricData.getProperties()).containsKey("os");
     assertThat(metricData.getProperties()).containsKey("language");
     assertThat(metricData.getProperties()).containsKey("version");
+    // customer defined dimensions do not apply to Statsbeat
     assertThat(metricData.getProperties()).doesNotContainEntry("tag1", "abc");
     assertThat(metricData.getProperties()).doesNotContainEntry("tag2", "def");
     assertThat(metricData.getProperties()).doesNotContainEntry("service.version", "123");
