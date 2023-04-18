144a6a0b3057e17779a20457f1b6508d2e487b34
Helen Yang <heya@microsoft.com>
2023-04-18 13:23:59 -0700

    Remove duplicate line

--- smoke-tests/apps/ClassicSdkWebInterop3x/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/ClassicSdkWebInterop3xTest.java
+++ smoke-tests/apps/ClassicSdkWebInterop3x/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/ClassicSdkWebInterop3xTest.java
@@ -40,7 +40,6 @@ abstract class ClassicSdkWebInterop3xTest {
     assertThat(telemetry.rd.getProperties())
         .containsEntry("_MS.ProcessedByMetricExtractors", "True");
     assertThat(telemetry.rd.getProperties()).containsKey("user_agent.original");
-    assertThat(telemetry.rd.getProperties()).containsKey("user_agent.original");
 
     assertThat(telemetry.rd.getSuccess()).isFalse();
   }
