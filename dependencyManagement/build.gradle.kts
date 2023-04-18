2ca4044a16fbcc99955e64c7a6c37f7f7fa7ffca
Helen Yang <heya@microsoft.com>
2023-04-18 13:04:14 -0700

    Replace http.user_agent with user_agent.original

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -11,7 +11,7 @@ data class DependencySet(val group: String, val version: String, val modules: Li
 val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
-val otelVersion = "1.23.1"
+val otelVersion = "1.25.0"
 val otelInstrumentationAlphaVersion = "1.25.0-alpha"
 val otelInstrumentationVersion = "1.25.0"
 val otelContribAlphaVersion = "1.18.0-alpha"
