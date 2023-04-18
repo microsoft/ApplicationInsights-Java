bfef633693e896616a57c04a5458b3a95e2f96a2
Helen Yang <heya@microsoft.com>
2023-04-18 14:21:22 -0700

    Revert

--- smoke-tests/apps/ClassicSdkWebInterop3xUsingOld3xAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/ClassicSdkWebInterop3xUsingOld3xAgentTest.java
+++ smoke-tests/apps/ClassicSdkWebInterop3xUsingOld3xAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/ClassicSdkWebInterop3xUsingOld3xAgentTest.java
@@ -39,6 +39,7 @@ abstract class ClassicSdkWebInterop3xUsingOld3xAgentTest {
     assertThat(telemetry.rd.getProperties()).hasSize(3);
     assertThat(telemetry.rd.getProperties())
         .containsEntry("_MS.ProcessedByMetricExtractors", "True");
+
     assertThat(telemetry.rd.getSuccess()).isFalse();
   }
 
