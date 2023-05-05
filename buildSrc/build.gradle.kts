6d64734d43e26e89ed4830e0d6501a7742e7e348
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-05-05 07:23:06 -0700

    Update shadow plugin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
   // When updating, update above in plugins too
   implementation("com.diffplug.spotless:spotless-plugin-gradle:6.18.0")
   implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:5.0.14")
-  implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
+  implementation("com.github.johnrengelman:shadow:8.1.1")
   implementation("com.gradle.enterprise:com.gradle.enterprise.gradle.plugin:3.13.1")
 
   implementation("org.owasp:dependency-check-gradle:8.2.1")
