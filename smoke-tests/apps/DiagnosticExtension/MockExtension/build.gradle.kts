188adc3ae45df6213fc9c48ecd2cb721fe714400
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-05-08 10:12:26 -0700

    Fix build (#3052)

--- smoke-tests/apps/DiagnosticExtension/MockExtension/build.gradle.kts
+++ smoke-tests/apps/DiagnosticExtension/MockExtension/build.gradle.kts
@@ -1,16 +1,24 @@
 plugins {
-  `java`
-}
-
-java {
-  toolchain {
-    languageVersion.set(JavaLanguageVersion.of(8))
-  }
+  java
 }
 
 dependencies {
   compileOnly(project(":agent:agent-profiler:agent-diagnostics-api"))
   compileOnly(project(":agent:agent-profiler:agent-alerting-api"))
+}
+
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+  with(options) {
+    release.set(8)
+    compilerArgs.add("-Werror")
+  }
 }
 
 tasks.jar {
