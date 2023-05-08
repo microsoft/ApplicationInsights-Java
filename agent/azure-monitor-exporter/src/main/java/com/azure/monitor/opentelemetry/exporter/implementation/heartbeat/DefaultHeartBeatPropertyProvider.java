e1eb2e1cf54a7afde97409840efd0a21762317e3
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-05-08 13:42:23 -0700

    Fix CodeQL failure (#3058)

--- agent/azure-monitor-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/heartbeat/DefaultHeartBeatPropertyProvider.java
+++ agent/azure-monitor-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/heartbeat/DefaultHeartBeatPropertyProvider.java
@@ -11,12 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
- *
- * <h1>Base Heartbeat Property Provider</h1>
- *
- * <p>This class is a concrete implementation of {@link HeartBeatPayloadProviderInterface} It
- * enables setting SDK Metadata to heartbeat payload.
+ * This class is a concrete implementation of {@link HeartBeatPayloadProviderInterface}. It enables
+ * setting SDK Metadata to heartbeat payload.
  */
 public class DefaultHeartBeatPropertyProvider implements HeartBeatPayloadProviderInterface {
 
