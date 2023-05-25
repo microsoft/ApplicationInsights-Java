37a5ee3c90ab27c8aa84d48f2017bd5db0e059fc
Helen Yang <heya@microsoft.com>
2023-05-25 15:42:26 -0700

    Simply the test to detect unexpected otel metrics

--- smoke-tests/apps/DetectUnexpectedOtelMetrics/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DetectUnexpectedOtelMetricsTest.java
+++ smoke-tests/apps/DetectUnexpectedOtelMetrics/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DetectUnexpectedOtelMetricsTest.java
@@ -12,15 +12,8 @@ import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCA
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_8_OPENJ9;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDFLY_13_JAVA_8;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDFLY_13_JAVA_8_OPENJ9;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.microsoft.applicationinsights.smoketest.schemav2.Data;
-import com.microsoft.applicationinsights.smoketest.schemav2.DataPoint;
-import com.microsoft.applicationinsights.smoketest.schemav2.Envelope;
-import com.microsoft.applicationinsights.smoketest.schemav2.MetricData;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeoutException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -31,26 +24,8 @@ abstract class DetectUnexpectedOtelMetricsTest {
   @RegisterExtension static final SmokeTestExtension testing = SmokeTestExtension.create();
 
   @Test
-  @TargetUri("/app2")
-  void testApp2() throws Exception {
-    testApp("app2", "12345678-0000-0000-0000-0FEEDDADBEEF");
-  }
-
-  @Test
-  @TargetUri("/app3")
-  void testApp3() throws Exception {
-    testApp("app3", "87654321-0000-0000-0000-0FEEDDADBEEF");
-  }
-
-  private static void testApp(String roleName, String iKey) throws Exception {
-    List<Envelope> clientMetrics =
-        testing.mockedIngestion.waitForMetricItems("http.client.duration", 1);
-    List<Envelope> serverMetrics =
-        testing.mockedIngestion.waitForMetricItems("http.server.duration", 1);
-
-    verifyHttpClientPreAggregatedMetrics(clientMetrics, roleName, iKey);
-    verifyHttpServerPreAggregatedMetrics(serverMetrics, roleName, iKey);
-
+  @TargetUri("/app")
+  void testApp() throws Exception {
     // no other otel metrics other than EXPECTED_OTEL_METRICS, expect an TimeoutException being
     // thrown
     assertThatThrownBy(
@@ -58,65 +33,6 @@ abstract class DetectUnexpectedOtelMetricsTest {
                 testing.mockedIngestion.waitForItemsUnexpectedOtelMetric(
                     "MetricData", envelope -> true))
         .isInstanceOf(TimeoutException.class);
-  }
-
-  private static void verifyHttpClientPreAggregatedMetrics(
-      List<Envelope> metrics, String roleName, String iKey) {
-    assertThat(metrics.size()).isEqualTo(1);
-
-    Envelope envelope = metrics.get(0);
-    assertThat(envelope.getIKey()).isEqualTo(iKey);
-    validateTags(envelope, roleName);
-    MetricData md1 = (MetricData) ((Data<?>) envelope.getData()).getBaseData();
-    validateMetricData("client", md1, "200", roleName);
-  }
-
-  private static void verifyHttpServerPreAggregatedMetrics(
-      List<Envelope> metrics, String roleName, String iKey) {
-    assertThat(metrics.size()).isEqualTo(1);
-
-    Envelope envelope = metrics.get(0);
-    assertThat(envelope.getIKey()).isEqualTo(iKey);
-    validateTags(envelope, roleName);
-    MetricData md1 = (MetricData) ((Data<?>) envelope.getData()).getBaseData();
-    validateMetricData("server", md1, "200", roleName);
-  }
-
-  private static void validateTags(Envelope envelope, String roleName) {
-    Map<String, String> tags = envelope.getTags();
-    assertThat(tags.get("ai.internal.sdkVersion")).isNotNull();
-    assertThat(tags).containsEntry("ai.cloud.roleInstance", "testroleinstance");
-    assertThat(tags).containsEntry("ai.cloud.role", roleName);
-  }
-
-  private static void validateMetricData(
-      String type, MetricData metricData, String resultCode, String roleName) {
-    List<DataPoint> dataPoints = metricData.getMetrics();
-    assertThat(dataPoints).hasSize(1);
-    DataPoint dataPoint = dataPoints.get(0);
-    assertThat(dataPoint.getCount()).isEqualTo(1);
-    assertThat(dataPoint.getValue()).isGreaterThan(0d).isLessThan(60 * 1000.0);
-    assertThat(dataPoint.getMin()).isGreaterThan(0d).isLessThan(60 * 1000.0);
-    assertThat(dataPoint.getMax()).isGreaterThan(0d).isLessThan(60 * 1000.0);
-    Map<String, String> properties = metricData.getProperties();
-    String expectedSuccess = "200".equals(resultCode) ? "True" : "False";
-    if ("client".equals(type)) {
-      assertThat(properties).hasSize(9);
-      assertThat(properties.get("_MS.MetricId")).isEqualTo("dependencies/duration");
-      assertThat(properties.get("dependency/resultCode")).isEqualTo(resultCode);
-      assertThat(properties.get("Dependency.Success")).isEqualTo(expectedSuccess);
-      assertThat(properties.get("dependency/target")).isEqualTo("mock.codes");
-      assertThat(properties.get("Dependency.Type")).isEqualTo("Http");
-    } else {
-      assertThat(properties).hasSize(7);
-      assertThat(properties.get("_MS.MetricId")).isEqualTo("requests/duration");
-      assertThat(properties.get("request/resultCode")).isEqualTo(resultCode);
-      assertThat(properties.get("Request.Success")).isEqualTo(expectedSuccess);
-    }
-    assertThat(properties.get("operation/synthetic")).isEqualTo("False");
-    assertThat(properties.get("cloud/roleInstance")).isEqualTo("testroleinstance");
-    assertThat(properties.get("cloud/roleName")).isEqualTo(roleName);
-    assertThat(properties.get("_MS.IsAutocollected")).isEqualTo("True");
   }
 
   @Environment(TOMCAT_8_JAVA_8)
