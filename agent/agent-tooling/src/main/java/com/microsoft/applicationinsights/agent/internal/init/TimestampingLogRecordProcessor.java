59e1c376ee8ae622b8826d071915747f7e7b6dfd
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-06-19 19:16:23 -0700

    Better toString()

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/TimestampingLogRecordProcessor.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/TimestampingLogRecordProcessor.java
@@ -135,5 +135,29 @@ class TimestampingLogRecordProcessor implements LogRecordProcessor {
     public int getTotalAttributeCount() {
       return delegate.getTotalAttributeCount();
     }
+
+    public String toString() {
+      return "SdkLogRecordData{resource="
+          + this.getResource()
+          + ", instrumentationScopeInfo="
+          + this.getInstrumentationScopeInfo()
+          + ", timestampEpochNanos="
+          + this.getTimestampEpochNanos()
+          + ", observedTimestampEpochNanos="
+          + this.getObservedTimestampEpochNanos()
+          + ", spanContext="
+          + this.getSpanContext()
+          + ", severity="
+          + this.getSeverity()
+          + ", severityText="
+          + this.getSeverityText()
+          + ", body="
+          + this.getBody()
+          + ", attributes="
+          + this.getAttributes()
+          + ", totalAttributeCount="
+          + this.getTotalAttributeCount()
+          + "}";
+    }
   }
 }
