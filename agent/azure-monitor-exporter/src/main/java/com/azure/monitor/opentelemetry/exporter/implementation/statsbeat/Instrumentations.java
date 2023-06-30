22c40eba28966b49fb71d65fa16bc0c04ccbba11
Helen <56097766+heyams@users.noreply.github.com>
2023-06-30 09:49:32 -0700

    Update azure sdk statsbeat tracking (#3163)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- agent/azure-monitor-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/statsbeat/Instrumentations.java
+++ agent/azure-monitor-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/statsbeat/Instrumentations.java
@@ -12,8 +12,9 @@ import org.slf4j.LoggerFactory;
 
 // this class is not currently implemented as an enum (similar to Feature)
 // because instrumentations may be more dynamic than features
-class Instrumentations {
+public class Instrumentations {
 
+  public static final String AZURE_OPENTELEMETRY = "Azure-OpenTelemetry";
   private static final Logger logger = LoggerFactory.getLogger(Instrumentations.class);
   private static final Map<String, Integer> INSTRUMENTATION_MAP;
 
@@ -26,7 +27,7 @@ class Instrumentations {
     // TODO (trask) start capturing this
     INSTRUMENTATION_MAP.put("io.opentelemetry.applicationinsights-web-2.3", 4);
     INSTRUMENTATION_MAP.put("io.opentelemetry.tomcat-7.0", 5);
-    INSTRUMENTATION_MAP.put("Azure-OpenTelemetry", 6); // bridged by azure-core-1.14 module
+    INSTRUMENTATION_MAP.put(AZURE_OPENTELEMETRY, 6); // bridged by azure-core-1.14 module
     INSTRUMENTATION_MAP.put("io.opentelemetry.cassandra-3.0", 7);
     INSTRUMENTATION_MAP.put("io.opentelemetry.cassandra-4.0", 8);
     INSTRUMENTATION_MAP.put("io.opentelemetry.java-http-client", 9);
