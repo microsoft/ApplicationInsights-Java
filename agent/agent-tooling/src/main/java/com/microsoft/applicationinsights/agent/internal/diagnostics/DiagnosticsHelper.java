69cfa3eca2970ece4287f9fbf6e7c57007652ae1
heyams <heya@microsoft.com>
2023-07-21 14:41:15 -0700

    Update springcloud

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/diagnostics/DiagnosticsHelper.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/diagnostics/DiagnosticsHelper.java
@@ -60,7 +60,7 @@ public class DiagnosticsHelper {
       rpIntegrationChar = 's';
       springAppRpIntegration = true;
       RpAttachHelper.setRpAttachType(
-          Files.exists(agentPath.resolveSibling("spring.codeless")) ? AUTO_ATTACH : MANUAL_ATTACH);
+          Files.exists(agentPath.resolveSibling("springcloud.codeless")) ? AUTO_ATTACH : MANUAL_ATTACH);
     }
     // TODO (heya) detect VM environment by checking the AzureMetadataService response, manual only
   }
