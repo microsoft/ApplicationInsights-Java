aeda3620509996e57e6d886863bd161ff845b8f4
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-08-04 13:56:41 -0700

    fix

--- agent/agent-tooling/src/test/java/com/microsoft/applicationinsights/agent/internal/perfcounter/JmxDataFetcherTest.java
+++ agent/agent-tooling/src/test/java/com/microsoft/applicationinsights/agent/internal/perfcounter/JmxDataFetcherTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.Test;
 
 class JmxDataFetcherTest {
 
-  @SuppressWarnings("unused")
+  @SuppressWarnings({"unused", "checkstyle:AbbreviationAsWordInName"})
   // This class name must end in MXBean (case sensitive)
   public interface StubMXBean {
     int getIntSample();
