3720c17cad3098d54e86da196e6e8543a4652b60
Harsimar Kaur (from Dev Box) <harskaur@microsoft.com>
2023-11-28 16:11:39 -0800

    fixing spotless violations

--- smoke-tests/apps/JmxMetric/build.gradle.kts
+++ smoke-tests/apps/JmxMetric/build.gradle.kts
@@ -15,5 +15,4 @@ dependencies {
   smokeTestImplementation("org.mock-server:mockserver-netty:5.15.0:shaded")
   smokeTestImplementation("org.awaitility:awaitility:4.2.0")
   smokeTestImplementation("io.opentelemetry.proto:opentelemetry-proto:0.14.0-alpha")
-
 }
--- smoke-tests/apps/JmxMetric/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/JmxMetricTest.java
+++ smoke-tests/apps/JmxMetric/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/JmxMetricTest.java
@@ -10,13 +10,14 @@ import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCA
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_8;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 import static org.mockserver.model.HttpRequest.request;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 import com.microsoft.applicationinsights.smoketest.schemav2.Data;
 import com.microsoft.applicationinsights.smoketest.schemav2.DataPoint;
 import com.microsoft.applicationinsights.smoketest.schemav2.Envelope;
 import com.microsoft.applicationinsights.smoketest.schemav2.MetricData;
+import io.opentelemetry.proto.metrics.v1.Metric;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -27,7 +28,6 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.mockserver.model.HttpRequest;
-import io.opentelemetry.proto.metrics.v1.Metric;
 
 @UseAgent
 abstract class JmxMetricTest {
@@ -36,26 +36,32 @@ abstract class JmxMetricTest {
   static final SmokeTestExtension testing =
       SmokeTestExtension.builder().useOtlpEndpoint().setSelfDiagnosticsLevel("TRACE").build();
 
-
   /* Note about jmx metrics in this test suite:
-   NameWithDot: An edge case where a dot in the mbean is not a path separator in the attribute, specifically when using org.weakref.jmx package
-   DefaultJmxMetricNameOverride: If a customer overrides the default ThreadCount metric with their own metric name, we should be collecting the metric with the name the customer specified
-   WildcardJmxMetric: This covers the case of ? and * in the objectName, with multiple matching object names. The expected metric value here is the sum of all the CollectionCount attribute values for each matching object name.
-        The matching objectNames are:
-        - Java 8: java.lang:type=GarbageCollector,name=PS Scavenge, java.lang:type=GarbageCollector,name=PS MarkSweep
-              - the corresponding metric names are PSScavenge and PSMarksweep
-        - Higher than Java 8: G1 Young Generation,type=GarbageCollector and G1 Old Generation,type=GarbageCollector.
-              - the corrresponding metric names are GCYoung and GCOld
-   Loaded_Class_Count: This covers the case of collecting a default jmx metric that the customer did not specify in applicationInsights.json. Also note that there are underscores
-        instead of spaces, as we are emitting the metric via OpenTelemetry now. When the upstream fixes the related bug (https://github.com/open-telemetry/opentelemetry-specification/issues/3422#issuecomment-1678116597),
-        then we can fix the code to not use underscores as a replacement.
-   BooleanJmxMetric: This covers the case of a jmx metric attribute with a boolean value.
-   DotInAttributeNameAsPathSeparator: This covers the case of an attribute having a dot in the name as a path separator.
-   */
-  static final Set<String> jmxMetricsAllJavaVersions = new HashSet<>(
-      Arrays.asList("NameWithDot", "DefaultJmxMetricNameOverride", "WildcardJmxMetric", "Loaded_Class_Count", "BooleanJmxMetric", "DotInAttributeNameAsPathSeparator"));
-  static final Set<String> gcOptionalJmxMetrics = new HashSet<>(
-      Arrays.asList("PSScavenge", "PSMarkSweep", "GCOld", "GCYoung"));
+  NameWithDot: An edge case where a dot in the mbean is not a path separator in the attribute, specifically when using org.weakref.jmx package
+  DefaultJmxMetricNameOverride: If a customer overrides the default ThreadCount metric with their own metric name, we should be collecting the metric with the name the customer specified
+  WildcardJmxMetric: This covers the case of ? and * in the objectName, with multiple matching object names. The expected metric value here is the sum of all the CollectionCount attribute values for each matching object name.
+       The matching objectNames are:
+       - Java 8: java.lang:type=GarbageCollector,name=PS Scavenge, java.lang:type=GarbageCollector,name=PS MarkSweep
+             - the corresponding metric names are PSScavenge and PSMarksweep
+       - Higher than Java 8: G1 Young Generation,type=GarbageCollector and G1 Old Generation,type=GarbageCollector.
+             - the corrresponding metric names are GCYoung and GCOld
+  Loaded_Class_Count: This covers the case of collecting a default jmx metric that the customer did not specify in applicationInsights.json. Also note that there are underscores
+       instead of spaces, as we are emitting the metric via OpenTelemetry now. When the upstream fixes the related bug (https://github.com/open-telemetry/opentelemetry-specification/issues/3422#issuecomment-1678116597),
+       then we can fix the code to not use underscores as a replacement.
+  BooleanJmxMetric: This covers the case of a jmx metric attribute with a boolean value.
+  DotInAttributeNameAsPathSeparator: This covers the case of an attribute having a dot in the name as a path separator.
+  */
+  static final Set<String> jmxMetricsAllJavaVersions =
+      new HashSet<>(
+          Arrays.asList(
+              "NameWithDot",
+              "DefaultJmxMetricNameOverride",
+              "WildcardJmxMetric",
+              "Loaded_Class_Count",
+              "BooleanJmxMetric",
+              "DotInAttributeNameAsPathSeparator"));
+  static final Set<String> gcOptionalJmxMetrics =
+      new HashSet<>(Arrays.asList("PSScavenge", "PSMarkSweep", "GCOld", "GCYoung"));
 
   @Test
   @TargetUri("/test")
@@ -64,6 +70,7 @@ abstract class JmxMetricTest {
     verifyJmxMetricsSenttoBreeze();
     verifyJmxMetricsSentToOtlpEndpoint();
   }
+
   @SuppressWarnings("PreferJavaTimeOverload")
   private void verifyJmxMetricsSentToOtlpEndpoint() {
     await()
@@ -82,11 +89,11 @@ abstract class JmxMetricTest {
 
               Map<String, Integer> occurrences = new HashMap<>();
 
-              // counting all occurrences of the jmx metrics that are applicable to all java versions
+              // counting all occurrences of the jmx metrics that are applicable to all java
+              // versions
               for (Metric metric : metrics) {
                 String metricName = metric.getName();
-                if (jmxMetricsAllJavaVersions.contains(metricName))
-                {
+                if (jmxMetricsAllJavaVersions.contains(metricName)) {
                   if (occurrences.containsKey(metricName)) {
                     occurrences.put(metricName, occurrences.get(metricName) + 1);
                   } else {
@@ -95,16 +102,15 @@ abstract class JmxMetricTest {
                 }
               }
 
-              for(Map.Entry<String,Integer> entry: occurrences.entrySet()) {
+              for (Map.Entry<String, Integer> entry : occurrences.entrySet()) {
                 System.out.println(entry.toString());
               }
               // confirm that those metrics received once or twice
               // (the collector seems to run for 5-10 sec)
               assertThat(occurrences.keySet()).hasSize(6);
-              for(int value : occurrences.values()) {
-                assertThat(value).isBetween(1,2);
+              for (int value : occurrences.values()) {
+                assertThat(value).isBetween(1, 2);
               }
-
             });
   }
 
@@ -113,14 +119,13 @@ abstract class JmxMetricTest {
         testing.mockedIngestion.waitForItems(
             envelope -> isJmxMetric(envelope), 1, 10, TimeUnit.SECONDS);
 
-    assertThat(metricItems).hasSizeBetween(8,16);
+    assertThat(metricItems).hasSizeBetween(8, 16);
 
     Set<String> metricNames = new HashSet<>();
     double wildcardValueSum = 0.0;
     double gcFirstMatch = 0.0;
     double gcSecondMatch = 0.0;
-    for (Envelope envelope : metricItems)
-    {
+    for (Envelope envelope : metricItems) {
       MetricData metricData = (MetricData) ((Data<?>) envelope.getData()).getBaseData();
       List<DataPoint> points = metricData.getMetrics();
       assertThat(points).hasSize(1);
@@ -131,13 +136,17 @@ abstract class JmxMetricTest {
       double value = points.get(0).getValue();
       if (metricName.equals("NameWithDot")) {
         assertThat(value).isEqualTo(5);
-      } if (metricName.equals("GCOld") || metricName.equals("PSScavenge")) {
+      }
+      if (metricName.equals("GCOld") || metricName.equals("PSScavenge")) {
         gcFirstMatch += value;
-      } if (metricName.equals("GCYoung") || metricName.equals("PSMarkSweep")) {
+      }
+      if (metricName.equals("GCYoung") || metricName.equals("PSMarkSweep")) {
         gcSecondMatch += value;
-      } if (metricName.equals("WildcardJmxMetric")) {
+      }
+      if (metricName.equals("WildcardJmxMetric")) {
         wildcardValueSum += value;
-      } if (metricName.equals("BooleanJmxMetric")) {
+      }
+      if (metricName.equals("BooleanJmxMetric")) {
         assertThat(value == 1.0 || value == 0.0);
       }
     }
@@ -155,7 +164,8 @@ abstract class JmxMetricTest {
     }
     MetricData md = SmokeTestExtension.getBaseData(envelope);
     String incomingMetricName = md.getMetrics().get(0).getName();
-    return jmxMetricsAllJavaVersions.contains(incomingMetricName) || gcOptionalJmxMetrics.contains(incomingMetricName);
+    return jmxMetricsAllJavaVersions.contains(incomingMetricName)
+        || gcOptionalJmxMetrics.contains(incomingMetricName);
   }
 
   @Environment(TOMCAT_8_JAVA_8)
