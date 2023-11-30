fd4aedd3489f7d154e10c111c5560567b6a245ea
Harsimar Kaur (from Dev Box) <harskaur@microsoft.com>
2023-11-30 13:25:13 -0800

    spotless

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/PerformanceCounterInitializer.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/PerformanceCounterInitializer.java
@@ -136,7 +136,6 @@ public class PerformanceCounterInitializer {
                   calculateAndRecordValueForAttribute(
                       observableDoubleMeasurement, objectName, jmxAttributeData);
                 });
-
       }
     }
   }
