2c91ea953263882d03b5ebf23358807496f23428
Jean Bisutti <jean.bisutti@gmail.com>
2023-12-19 15:31:29 +0100

    Ability to disable the Reactor and Reactor Netty instrumentations

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/AiConfigCustomizer.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/AiConfigCustomizer.java
@@ -182,8 +182,12 @@ public class AiConfigCustomizer implements Function<ConfigProperties, Map<String
         "otel.instrumentation.opentelemetry-instrumentation-annotations.enabled", "true");
     properties.put("otel.instrumentation.opentelemetry-api.enabled", "true");
     properties.put("otel.instrumentation.opentelemetry-instrumentation-api.enabled", "true");
-    properties.put("otel.instrumentation.reactor.enabled", "true");
-    properties.put("otel.instrumentation.reactor-netty.enabled", "true");
+    if (otelConfig.getBoolean("otel.instrumentation.reactor.enabled", true)) {
+      properties.put("otel.instrumentation.reactor.enabled", "true");
+    }
+    if (otelConfig.getBoolean("otel.instrumentation.reactor-netty.enabled", true)) {
+      properties.put("otel.instrumentation.reactor-netty.enabled", "true");
+    }
     properties.put("otel.instrumentation.rxjava.enabled", "true");
 
     properties.put("otel.instrumentation.servlet.enabled", "true");
