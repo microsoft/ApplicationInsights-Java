c7c0bfef8b4b10d1c04d4df37a1cdc975e16e78d
Jean Bisutti <jean.bisutti@gmail.com>
2024-01-18 15:57:54 +0100

    Perf result sending

--- perf-tests/src/test/java/AiStartUpTest.java
+++ perf-tests/src/test/java/AiStartUpTest.java
@@ -1,6 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Calendar;
+import java.util.Date;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.quickperf.junit5.QuickPerfTest;
 import org.quickperf.jvm.annotations.JvmOptions;
@@ -18,12 +26,25 @@ class AiStartUpTest {
     PetClinicApplication.main(new String[] {});
   }
 
-   public static class FileWriterFactory implements WriterFactory  {
-
+  public static class FileWriterFactory implements WriterFactory {
     @Override
     public Writer buildWriter() throws IOException {
       return new FileWriter("build/allocation-at-startup.txt");
     }
   }
+
+  @AfterEach
+  void sendResults() throws IOException {
+    String formattedAllocation = Files.readString(Path.of("build/allocation-at-startup.txt"));
+    String allocationValue = formattedAllocation.split(" Mega bytes")[0];
+
+    Calendar calendar = Calendar.getInstance();
+
+    Date date = calendar.getTime();
+
+    PerfResultSender.send(
+        date, "SDKPerfJavaAllocAtStartUp", "AllocationStartUpJava", allocationValue, "MB");
+  }
+
 
 }
