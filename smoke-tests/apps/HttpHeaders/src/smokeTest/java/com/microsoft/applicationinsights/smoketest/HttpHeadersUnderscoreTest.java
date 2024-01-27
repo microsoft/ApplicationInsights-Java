833a53fa73cdef5ea0853707dc447236eafc8490
heyams <heya@microsoft.com>
2024-01-26 16:39:28 -0800

    Revert debug

--- smoke-tests/apps/HttpHeaders/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/HttpHeadersUnderscoreTest.java
+++ smoke-tests/apps/HttpHeaders/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/HttpHeadersUnderscoreTest.java
@@ -21,8 +21,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 abstract class HttpHeadersUnderscoreTest {
 
   @RegisterExtension
-  static final SmokeTestExtension testing =
-      SmokeTestExtension.builder().setSelfDiagnosticsLevel("DEBUG").build();
+  static final SmokeTestExtension testing = SmokeTestExtension.create();
 
   @Test
   @TargetUri("/serverHeaders")
