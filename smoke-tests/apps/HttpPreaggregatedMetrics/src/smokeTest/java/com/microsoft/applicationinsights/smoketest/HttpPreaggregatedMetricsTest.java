3edce7d97a39652bdd617cd031bf64f1ca6274c3
heyams <heya@microsoft.com>
2024-02-23 19:42:49 -0800

    Spotless

--- smoke-tests/apps/HttpPreaggregatedMetrics/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/HttpPreaggregatedMetricsTest.java
+++ smoke-tests/apps/HttpPreaggregatedMetrics/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/HttpPreaggregatedMetricsTest.java
@@ -162,15 +162,9 @@ abstract class HttpPreaggregatedMetricsTest {
     DataPoint dataPoint = dataPoints.get(0);
     assertThat(dataPoint.getCount()).isEqualTo(1);
     double greaterThan = type.equals("server") ? 200.0f : 0.0f;
-    assertThat(dataPoint.getValue())
-        .isGreaterThan(greaterThan)
-        .isLessThan(60 * 1000.0);
-    assertThat(dataPoint.getMin())
-        .isGreaterThan(greaterThan)
-        .isLessThan(60 * 1000.0);
-    assertThat(dataPoint.getMax())
-        .isGreaterThan(greaterThan)
-        .isLessThan(60 * 1000.0);
+    assertThat(dataPoint.getValue()).isGreaterThan(greaterThan).isLessThan(60 * 1000.0);
+    assertThat(dataPoint.getMin()).isGreaterThan(greaterThan).isLessThan(60 * 1000.0);
+    assertThat(dataPoint.getMax()).isGreaterThan(greaterThan).isLessThan(60 * 1000.0);
     Map<String, String> properties = metricData.getProperties();
     String expectedSuccess = "200".equals(resultCode) ? "True" : "False";
     if ("client".equals(type)) {
