a7ed918f54865dd706ca46e139a1e85279c4d70c
heyams <heya@microsoft.com>
2024-04-24 13:26:32 -0700

    Remove openJ9

--- smoke-tests/apps/DiagnosticExtension/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DiagnosticExtensionTest.java
+++ smoke-tests/apps/DiagnosticExtension/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DiagnosticExtensionTest.java
@@ -6,7 +6,6 @@ package com.microsoft.applicationinsights.smoketest;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.JAVA_11;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.JAVA_17;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.JAVA_21;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.JAVA_21_OPENJ9;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.JAVA_8;
 import static java.util.Collections.emptyMap;
 
@@ -45,7 +44,4 @@ abstract class DiagnosticExtensionTest {
 
   @Environment(JAVA_21)
   static class Java21Test extends DiagnosticExtensionTest {}
-
-  @Environment(JAVA_21_OPENJ9)
-  static class Java21OpenJ9Test extends DiagnosticExtensionTest {}
 }
