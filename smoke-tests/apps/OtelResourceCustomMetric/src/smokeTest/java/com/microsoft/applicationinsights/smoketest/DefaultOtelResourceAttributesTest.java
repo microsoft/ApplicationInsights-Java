36a9bf9350156ad4646d2a85efe99ef423f429fb
heyams <heya@microsoft.com>
2024-05-22 12:02:48 -0700

    Update smoke test

--- smoke-tests/apps/OtelResourceCustomMetric/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DefaultOtelResourceAttributesTest.java
+++ smoke-tests/apps/OtelResourceCustomMetric/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/DefaultOtelResourceAttributesTest.java
@@ -14,26 +14,14 @@ import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDF
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.microsoft.applicationinsights.smoketest.schemav2.Data;
-import com.microsoft.applicationinsights.smoketest.schemav2.DataPoint;
 import com.microsoft.applicationinsights.smoketest.schemav2.Envelope;
 import com.microsoft.applicationinsights.smoketest.schemav2.MetricData;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 @UseAgent
 abstract class DefaultOtelResourceAttributesTest {
-
-  private static final List<String> EXPECTED_RESOURCE_ATTRIBUTES =
-      Arrays.asList(
-          "telemetry.sdk.language",
-          "service.name",
-          "service.instance.id",
-          "telemetry.sdk.version",
-          "telemetry.sdk.name");
 
   @RegisterExtension static final SmokeTestExtension testing = SmokeTestExtension.create();
 
@@ -41,37 +29,10 @@ abstract class DefaultOtelResourceAttributesTest {
   @TargetUri(value = "/app")
   void testApp() {
     List<Envelope> metricsEnvelops = testing.mockedIngestion.getItemsEnvelopeDataType("MetricData");
-    List<Envelope> otelResourceCustomMetrics = new ArrayList<>();
-    for (Envelope envelope : metricsEnvelops) {
-      MetricData metricData = (MetricData) ((Data<?>) envelope.getData()).getBaseData();
-      String name = metricData.getMetrics().get(0).getName();
-      if ("_OTELRESOURCE_".equals(name)) {
-        otelResourceCustomMetrics.add(envelope);
-      }
-    }
-
-    verify(otelResourceCustomMetrics);
-  }
-
-  private static void verify(List<Envelope> metrics) {
-    for (Envelope envelope : metrics) {
-      validateTags(envelope);
-      MetricData md1 = (MetricData) ((Data<?>) envelope.getData()).getBaseData();
-      validateMetricData(md1);
-    }
-  }
-
-  private static void validateTags(Envelope envelope) {
-    Map<String, String> tags = envelope.getTags();
-    assertThat(tags).containsOnlyKeys("ai.internal.sdkVersion", "ai.cloud.roleInstance");
-  }
-
-  private static void validateMetricData(MetricData metricData) {
-    List<DataPoint> dataPoints = metricData.getMetrics();
-    assertThat(dataPoints).hasSize(1);
-    Map<String, String> properties = metricData.getProperties();
-    assertThat(properties.keySet())
-        .containsExactlyInAnyOrderElementsOf(EXPECTED_RESOURCE_ATTRIBUTES);
+    metricsEnvelops.stream()
+        .map(envelope -> (MetricData) ((Data<?>) envelope.getData()).getBaseData())
+        .map(metricData -> metricData.getMetrics().get(0).getName())
+        .forEach(name -> assertThat(name).isNotEqualTo("_OTELRESOURCE_"));
   }
 
   @Environment(TOMCAT_8_JAVA_8)
