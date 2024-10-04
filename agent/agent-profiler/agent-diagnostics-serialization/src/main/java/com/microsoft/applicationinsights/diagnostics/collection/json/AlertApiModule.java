90dc63915351ec6e9cd9ba45c8fad619ebb58b99
heyams <heya@microsoft.com>
2024-10-04 13:27:34 -0700

    Rewrite AlertApiModule

--- agent/agent-profiler/agent-diagnostics-serialization/src/main/java/com/microsoft/applicationinsights/diagnostics/collection/json/AlertApiModule.java
+++ agent/agent-profiler/agent-diagnostics-serialization/src/main/java/com/microsoft/applicationinsights/diagnostics/collection/json/AlertApiModule.java
@@ -3,32 +3,57 @@
 
 package com.microsoft.applicationinsights.diagnostics.collection.json;
 
-import com.azure.json.JsonSerializable;
+import com.azure.json.JsonProviders;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonToken;
 import com.azure.json.JsonWriter;
 import com.microsoft.applicationinsights.alerting.aiconfig.AlertingConfig;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Locale;
 
-public class AlertApiModule implements JsonSerializable<AlertApiModule> {
+public class AlertApiModule {
 
-  @Override
-  public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-    jsonWriter.writeStartObject();
-    addEnumConfig(jsonWriter, AlertingConfig.RequestFilterType.class);
-    addEnumConfig(jsonWriter, AlertingConfig.RequestAggregationType.class);
-    addEnumConfig(jsonWriter, AlertingConfig.RequestTriggerThresholdType.class);
-    addEnumConfig(jsonWriter, AlertingConfig.RequestTriggerThrottlingType.class);
-    addEnumConfig(jsonWriter, AlertingConfig.RequestAggregationType.class);
-    jsonWriter.writeEndObject();
-    return jsonWriter;
+  public AlertApiModule() {
+    addEnumConfig(AlertingConfig.RequestFilterType.class);
+    addEnumConfig(AlertingConfig.RequestAggregationType.class);
+    addEnumConfig(AlertingConfig.RequestTriggerThresholdType.class);
+    addEnumConfig(AlertingConfig.RequestTriggerThrottlingType.class);
+    addEnumConfig(AlertingConfig.RequestAggregationType.class);
   }
 
-  private <T extends Enum<T>> void addEnumConfig(JsonWriter jsonWriter, Class<T> clazz)
+  private <T extends Enum<T>> void addEnumConfig(Class<T> clazz) {
+    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        JsonWriter jsonWriter = JsonProviders.createWriter(outputStream);
+        JsonReader jsonReader = JsonProviders.createReader(outputStream.toByteArray())) {
+      addSerializer(jsonWriter, clazz);
+      addDeserializer(jsonReader, clazz);
+    } catch (IOException ignored) {
+      // Ignored
+    }
+  }
+
+  private <T extends Enum<T>> void addSerializer(JsonWriter jsonWriter, Class<T> clazz)
       throws IOException {
     jsonWriter.writeStartObject(clazz.getSimpleName());
     for (T enumConstant : clazz.getEnumConstants()) {
       jsonWriter.writeStringField(
-          enumConstant.name().toLowerCase().replace("_", "-"), enumConstant.name());
+          enumConstant.name().toLowerCase(Locale.ROOT).replace("_", "-"), enumConstant.name());
     }
     jsonWriter.writeEndObject();
+  }
+
+  private <T extends Enum<T>> T addDeserializer(JsonReader jsonReader, Class<T> clazz)
+      throws IOException {
+    return jsonReader.readObject(
+        reader -> {
+          if (reader.nextToken() != JsonToken.END_OBJECT) {
+            return Enum.valueOf(
+                clazz, jsonReader.getString().toUpperCase(Locale.ROOT).replace("-", "_"));
+          } else {
+            reader.skipChildren();
+            return null;
+          }
+        });
   }
 }
