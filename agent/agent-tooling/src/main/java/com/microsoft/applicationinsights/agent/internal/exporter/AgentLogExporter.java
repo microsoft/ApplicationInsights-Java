Microsoft/AppInsights-Java

Improve sampling override behavior

agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/exporter/AgentLogExporter.java
@@ -4,13 +4,14 @@
 package com.microsoft.applicationinsights.agent.internal.exporter;
 
 import static com.azure.monitor.opentelemetry.exporter.implementation.utils.AzureMonitorMsgId.EXPORTER_MAPPING_ERROR;
-import static com.microsoft.applicationinsights.agent.internal.exporter.ExporterUtils.shouldSample;
 
+import com.azure.monitor.opentelemetry.exporter.implementation.AiSemanticAttributes;
 import com.azure.monitor.opentelemetry.exporter.implementation.LogDataMapper;
 import com.azure.monitor.opentelemetry.exporter.implementation.logging.OperationLogger;
 import com.azure.monitor.opentelemetry.exporter.implementation.models.TelemetryItem;
 import com.azure.monitor.opentelemetry.exporter.implementation.quickpulse.QuickPulse;
 import com.microsoft.applicationinsights.agent.internal.configuration.Configuration.SamplingOverride;
+import com.microsoft.applicationinsights.agent.internal.sampling.AiSampler;
 import com.microsoft.applicationinsights.agent.internal.sampling.SamplingOverrides;
 import com.microsoft.applicationinsights.agent.internal.telemetry.BatchItemProcessor;
 import com.microsoft.applicationinsights.agent.internal.telemetry.TelemetryClient;
@@ -21,6 +22,7 @@ import io.opentelemetry.javaagent.bootstrap.CallDepth;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.logs.data.LogRecordData;
 import io.opentelemetry.sdk.logs.export.LogRecordExporter;
+import io.opentelemetry.sdk.trace.samplers.SamplingDecision;
 import io.opentelemetry.semconv.SemanticAttributes;
 import java.util.Collection;
 import java.util.List;
@@ -108,24 +110,30 @@ public class AgentLogExporter implements LogRecordExporter {
             stack != null ? exceptionSamplingOverrides : logSamplingOverrides;
 
         SpanContext spanContext = log.getSpanContext();
+        Double parentSpanSampleRate = log.getAttributes().get(AiSemanticAttributes.SAMPLE_RATE);
 
-        Double samplingPercentage = samplingOverrides.getOverridePercentage(log.getAttributes());
+        AiSampler sampler = samplingOverrides.getOverride(log.getAttributes());
 
-        if (samplingPercentage != null && !shouldSample(spanContext, samplingPercentage)) {
-          continue;
-        }
-
-        if (samplingPercentage == null
-            && spanContext.isValid()
-            && !spanContext.getTraceFlags().isSampled()) {
+        if (sampler == null && spanContext.isValid() && !spanContext.getTraceFlags().isSampled()) {
           // if there is no sampling override, and the log is part of an unsampled trace, then don't
           // capture it
           continue;
         }
 
+        Double sampleRate = parentSpanSampleRate;
+        if (sampler != null) {
+          if (sampler.shouldSampleLog(spanContext, parentSpanSampleRate).getDecision()
+              == SamplingDecision.DROP) {
+            continue;
+          }
+          // sampling override percentage takes precedence
+          sampleRate = sampler.getParentlessDependencySamplingPercentage().get();
+        }
+
         logger.debug("exporting log: {}", log);
 
-        TelemetryItem telemetryItem = mapper.map(log, stack, samplingPercentage);
+        // TODO (trask) no longer need to check AiSemanticAttributes.SAMPLE_RATE in map() method
+        TelemetryItem telemetryItem = mapper.map(log, stack, sampleRate);
         telemetryItemConsumer.accept(telemetryItem);
 
         exportingLogLogger.recordSuccess();

agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/SecondEntryPoint.java
@@ -6,6 +6,7 @@ package com.microsoft.applicationinsights.agent.internal.init;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 import com.azure.core.util.logging.ClientLogger;
+import com.azure.monitor.opentelemetry.exporter.implementation.AiSemanticAttributes;
 import com.azure.monitor.opentelemetry.exporter.implementation.AzureMonitorExporterProviderKeys;
 import com.azure.monitor.opentelemetry.exporter.implementation.AzureMonitorLogRecordExporterProvider;
 import com.azure.monitor.opentelemetry.exporter.implementation.AzureMonitorMetricExporterProvider;
@@ -37,7 +38,6 @@ import com.microsoft.applicationinsights.agent.internal.configuration.SnippetCon
 import com.microsoft.applicationinsights.agent.internal.exporter.AgentLogExporter;
 import com.microsoft.applicationinsights.agent.internal.exporter.AgentMetricExporter;
 import com.microsoft.applicationinsights.agent.internal.exporter.AgentSpanExporter;
-import com.microsoft.applicationinsights.agent.internal.exporter.ExporterUtils;
 import com.microsoft.applicationinsights.agent.internal.httpclient.LazyHttpClient;
 import com.microsoft.applicationinsights.agent.internal.legacyheaders.AiLegacyHeaderSpanProcessor;
 import com.microsoft.applicationinsights.agent.internal.processors.ExporterWithLogProcessor;
@@ -45,6 +45,7 @@ import com.microsoft.applicationinsights.agent.internal.processors.ExporterWithS
 import com.microsoft.applicationinsights.agent.internal.processors.LogExporterWithAttributeProcessor;
 import com.microsoft.applicationinsights.agent.internal.processors.SpanExporterWithAttributeProcessor;
 import com.microsoft.applicationinsights.agent.internal.profiler.triggers.AlertTriggerSpanProcessor;
+import com.microsoft.applicationinsights.agent.internal.sampling.AiSampler;
 import com.microsoft.applicationinsights.agent.internal.sampling.SamplingOverrides;
 import com.microsoft.applicationinsights.agent.internal.telemetry.BatchItemProcessor;
 import com.microsoft.applicationinsights.agent.internal.telemetry.MetricFilter;
@@ -66,6 +67,7 @@ import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import io.opentelemetry.sdk.metrics.internal.view.AiViewRegistry;
 import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.sdk.trace.samplers.SamplingDecision;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -579,10 +581,14 @@ public class SecondEntryPoint
               return false;
             },
             (span, event) -> {
-              Double samplingPercentage =
-                  exceptionSamplingOverrides.getOverridePercentage(event.getAttributes());
-              return samplingPercentage != null
-                  && !ExporterUtils.shouldSample(span.getSpanContext(), samplingPercentage);
+              AiSampler sampler = exceptionSamplingOverrides.getOverride(event.getAttributes());
+              return sampler != null
+                  && sampler
+                          .shouldSampleLog(
+                              span.getSpanContext(),
+                              span.getAttributes().get(AiSemanticAttributes.SAMPLE_RATE))
+                          .getDecision()
+                      == SamplingDecision.DROP;
             });
 
     BatchItemProcessor batchItemProcessor = telemetryClient.getGeneralBatchItemProcessor();

microsoft/ApplicationInsights-Java

agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/exporter/AgentLogExporter.java
@@ -4,13 +4,14 @@
 package com.microsoft.applicationinsights.agent.internal.exporter;
 
 import static com.azure.monitor.opentelemetry.autoconfigure.implementation.utils.AzureMonitorMsgId.EXPORTER_MAPPING_ERROR;
-import static com.microsoft.applicationinsights.agent.internal.exporter.ExporterUtils.shouldSample;
 
+import com.azure.monitor.opentelemetry.autoconfigure.implementation.AiSemanticAttributes;
 import com.azure.monitor.opentelemetry.autoconfigure.implementation.LogDataMapper;
 import com.azure.monitor.opentelemetry.autoconfigure.implementation.logging.OperationLogger;
 import com.azure.monitor.opentelemetry.autoconfigure.implementation.models.TelemetryItem;
 import com.azure.monitor.opentelemetry.autoconfigure.implementation.quickpulse.QuickPulse;
 import com.microsoft.applicationinsights.agent.internal.configuration.Configuration.SamplingOverride;
+import com.microsoft.applicationinsights.agent.internal.sampling.AiSampler;
 import com.microsoft.applicationinsights.agent.internal.sampling.SamplingOverrides;
 import com.microsoft.applicationinsights.agent.internal.telemetry.BatchItemProcessor;
 import com.microsoft.applicationinsights.agent.internal.telemetry.TelemetryClient;
@@ -21,6 +22,7 @@ import io.opentelemetry.javaagent.bootstrap.CallDepth;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.logs.data.LogRecordData;
 import io.opentelemetry.sdk.logs.export.LogRecordExporter;
+import io.opentelemetry.sdk.trace.samplers.SamplingDecision;
 import io.opentelemetry.semconv.SemanticAttributes;
 import java.util.Collection;
 import java.util.List;
@@ -108,24 +110,30 @@ public class AgentLogExporter implements LogRecordExporter {
             stack != null ? exceptionSamplingOverrides : logSamplingOverrides;
 
         SpanContext spanContext = log.getSpanContext();
+        Double parentSpanSampleRate = log.getAttributes().get(AiSemanticAttributes.SAMPLE_RATE);
 
-        Double samplingPercentage = samplingOverrides.getOverridePercentage(log.getAttributes());
+        AiSampler sampler = samplingOverrides.getOverride(log.getAttributes());
 
-        if (samplingPercentage != null && !shouldSample(spanContext, samplingPercentage)) {
-          continue;
-        }
-
-        if (samplingPercentage == null
-            && spanContext.isValid()
-            && !spanContext.getTraceFlags().isSampled()) {
+        if (sampler == null && spanContext.isValid() && !spanContext.getTraceFlags().isSampled()) {
           // if there is no sampling override, and the log is part of an unsampled trace, then don't
           // capture it
           continue;
         }
 
+        Double sampleRate = parentSpanSampleRate;
+        if (sampler != null) {
+          if (sampler.shouldSampleLog(spanContext, parentSpanSampleRate).getDecision()
+              == SamplingDecision.DROP) {
+            continue;
+          }
+          // sampling override percentage takes precedence
+          sampleRate = sampler.getParentlessDependencySamplingPercentage().get();
+        }
+
         logger.debug("exporting log: {}", log);
 
-        TelemetryItem telemetryItem = mapper.map(log, stack, samplingPercentage);
+        // TODO (trask) no longer need to check AiSemanticAttributes.SAMPLE_RATE in map() method
+        TelemetryItem telemetryItem = mapper.map(log, stack, sampleRate);
         telemetryItemConsumer.accept(telemetryItem);
 
         exportingLogLogger.recordSuccess();

agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/init/SecondEntryPoint.java
@@ -6,6 +6,7 @@ package com.microsoft.applicationinsights.agent.internal.init;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 import com.azure.core.util.logging.ClientLogger;
+import com.azure.monitor.opentelemetry.autoconfigure.implementation.AiSemanticAttributes;
 import com.azure.monitor.opentelemetry.autoconfigure.implementation.AzureMonitorExporterProviderKeys;
 import com.azure.monitor.opentelemetry.autoconfigure.implementation.AzureMonitorLogRecordExporterProvider;
 import com.azure.monitor.opentelemetry.autoconfigure.implementation.AzureMonitorMetricExporterProvider;
@@ -37,7 +38,6 @@ import com.microsoft.applicationinsights.agent.internal.configuration.SnippetCon
 import com.microsoft.applicationinsights.agent.internal.exporter.AgentLogExporter;
 import com.microsoft.applicationinsights.agent.internal.exporter.AgentMetricExporter;
 import com.microsoft.applicationinsights.agent.internal.exporter.AgentSpanExporter;
-import com.microsoft.applicationinsights.agent.internal.exporter.ExporterUtils;
 import com.microsoft.applicationinsights.agent.internal.httpclient.LazyHttpClient;
 import com.microsoft.applicationinsights.agent.internal.legacyheaders.AiLegacyHeaderSpanProcessor;
 import com.microsoft.applicationinsights.agent.internal.processors.ExporterWithLogProcessor;
@@ -45,6 +45,7 @@ import com.microsoft.applicationinsights.agent.internal.processors.ExporterWithS
 import com.microsoft.applicationinsights.agent.internal.processors.LogExporterWithAttributeProcessor;
 import com.microsoft.applicationinsights.agent.internal.processors.SpanExporterWithAttributeProcessor;
 import com.microsoft.applicationinsights.agent.internal.profiler.triggers.AlertTriggerSpanProcessor;
+import com.microsoft.applicationinsights.agent.internal.sampling.AiSampler;
 import com.microsoft.applicationinsights.agent.internal.sampling.SamplingOverrides;
 import com.microsoft.applicationinsights.agent.internal.telemetry.BatchItemProcessor;
 import com.microsoft.applicationinsights.agent.internal.telemetry.MetricFilter;
@@ -66,6 +67,7 @@ import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import io.opentelemetry.sdk.metrics.internal.view.AiViewRegistry;
 import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.sdk.trace.samplers.SamplingDecision;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -579,10 +581,14 @@ public class SecondEntryPoint
               return false;
             },
             (span, event) -> {
-              Double samplingPercentage =
-                  exceptionSamplingOverrides.getOverridePercentage(event.getAttributes());
-              return samplingPercentage != null
-                  && !ExporterUtils.shouldSample(span.getSpanContext(), samplingPercentage);
+              AiSampler sampler = exceptionSamplingOverrides.getOverride(event.getAttributes());
+              return sampler != null
+                  && sampler
+                          .shouldSampleLog(
+                              span.getSpanContext(),
+                              span.getAttributes().get(AiSemanticAttributes.SAMPLE_RATE))
+                          .getDecision()
+                      == SamplingDecision.DROP;
             });
 
     BatchItemProcessor batchItemProcessor = telemetryClient.getGeneralBatchItemProcessor();