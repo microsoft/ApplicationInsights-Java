46d2ed50354ad4bd214edd4f77c60069f2f790b9
Jean Bisutti <jean.bisutti@gmail.com>
2025-04-03 16:51:15 +0200

    Log as warn invalid JMX metrics

--- agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/perfcounter/AbstractJmxPerformanceCounter.java
+++ agent/agent-tooling/src/main/java/com/microsoft/applicationinsights/agent/internal/perfcounter/AbstractJmxPerformanceCounter.java
@@ -7,7 +7,9 @@ import static com.microsoft.applicationinsights.agent.internal.diagnostics.MsgId
 
 import com.microsoft.applicationinsights.agent.internal.telemetry.TelemetryClient;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -24,6 +26,8 @@ public abstract class AbstractJmxPerformanceCounter implements PerformanceCounte
   private final Collection<JmxAttributeData> attributes;
   private boolean alreadyLogged = false;
 
+  private final Set<String> invalidJmxMetrics = new HashSet<>();
+
   /**
    * The main method. The method will fetch the data and send it. The method will not do anything if
    * there was a major problem accessing the needed counter.
@@ -38,6 +42,7 @@ public abstract class AbstractJmxPerformanceCounter implements PerformanceCounte
       for (Map.Entry<String, Collection<Object>> displayAndValues : result.entrySet()) {
         boolean ok = true;
         double value = 0.0;
+        String metricName = displayAndValues.getKey();
         for (Object obj : displayAndValues.getValue()) {
           try {
             if (obj instanceof Boolean) {
@@ -46,6 +51,14 @@ public abstract class AbstractJmxPerformanceCounter implements PerformanceCounte
               value += Double.parseDouble(String.valueOf(obj));
             }
           } catch (RuntimeException e) {
+            if (!invalidJmxMetrics.contains(metricName)) {
+              invalidJmxMetrics.add(metricName);
+              try (MDC.MDCCloseable ignored = CUSTOM_JMX_METRIC_ERROR.makeActive()) {
+                logger.warn(
+                    "{} JMX metric is invalid because only numeric and boolean JMX metric values are supported.",
+                    metricName);
+              }
+            }
             ok = false;
             break;
           }
@@ -53,7 +66,7 @@ public abstract class AbstractJmxPerformanceCounter implements PerformanceCounte
 
         if (ok) {
           try {
-            send(telemetryClient, displayAndValues.getKey(), value);
+            send(telemetryClient, metricName, value);
           } catch (RuntimeException e) {
             try (MDC.MDCCloseable ignored = CUSTOM_JMX_METRIC_ERROR.makeActive()) {
               logger.error("Error while sending JMX data: '{}'", e.toString());
