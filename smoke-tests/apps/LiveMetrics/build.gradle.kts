485ecaf1455597da5e46ce1feb3ac94bec31d6ba
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-07-15 13:52:06 -0700

    Remove unused dependencies

--- smoke-tests/apps/LiveMetrics/build.gradle.kts
+++ smoke-tests/apps/LiveMetrics/build.gradle.kts
@@ -6,6 +6,4 @@ dependencies {
   smokeTestImplementation("org.awaitility:awaitility:4.2.0")
   implementation("log4j:log4j:1.2.17")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.22.1")
-  implementation("com.azure:azure-json:1.0.0")
-  implementation("com.azure:azure-monitor-opentelemetry-autoconfigure:1.1.0")
 }
