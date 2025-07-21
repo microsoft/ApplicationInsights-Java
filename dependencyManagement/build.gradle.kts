f1af0e28fe5df84668eff424ba9d9072b4d774d7
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-07-21 20:32:12 +0000

    Update to OpenTelemetry Java Instrumentation 2.18.1

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -12,8 +12,8 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 val otelSdkVersion = "1.51.0"
-val otelInstrumentationAlphaVersion = "2.17.1-alpha"
-val otelInstrumentationVersion = "2.17.1"
+val otelInstrumentationAlphaVersion = "2.18.1-alpha"
+val otelInstrumentationVersion = "2.18.1"
 val otelContribVersion = "1.47.0"
 
 rootProject.extra["otelInstrumentationVersion"] = otelInstrumentationVersion
