68e38c877452f831f64025bf524ccd3315c5fc86
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-07-21 20:35:53 -0700

    fix

--- buildSrc/src/main/kotlin/ai.shadow-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/ai.shadow-conventions.gradle.kts
@@ -28,13 +28,13 @@ tasks.withType<ShadowJar>().configureEach {
   relocate("io.opentelemetry.api", "io.opentelemetry.javaagent.shaded.io.opentelemetry.api")
   relocate("io.opentelemetry.semconv", "io.opentelemetry.javaagent.shaded.io.opentelemetry.semconv")
   relocate("io.opentelemetry.context", "io.opentelemetry.javaagent.shaded.io.opentelemetry.context")
+  relocate("io.opentelemetry.common", "io.opentelemetry.javaagent.shaded.io.opentelemetry.common")
 
   // relocate(the OpenTelemetry extensions that are used by instrumentation modules)
   // these extensions live in the AgentClassLoader, and are injected into the user's class loader
   // by the instrumentation modules that use them
   relocate("io.opentelemetry.contrib.awsxray", "io.opentelemetry.javaagent.shaded.io.opentelemetry.contrib.awsxray")
   relocate("io.opentelemetry.extension.kotlin", "io.opentelemetry.javaagent.shaded.io.opentelemetry.extension.kotlin")
-  relocate("io.opentelemetry.extension.incubator", "io.opentelemetry.javaagent.shaded.io.opentelemetry.extension.incubator")
 
   val shadowPrefix = "com.microsoft.applicationinsights.agent.shadow"
 
