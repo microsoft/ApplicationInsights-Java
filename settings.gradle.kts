bfec1cb691ce16e91eccc48abf8c2817a1700e2b
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-07-21 21:54:13 -0700

    Remove unused code (#4357)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ import org.apache.tools.ant.taskdefs.condition.Os
 
 pluginManagement {
   plugins {
-    id("com.github.ben-manes.versions") version "0.52.0"
     id("com.github.jk1.dependency-license-report") version "2.9"
     id("me.champeau.jmh") version "0.7.3"
     id("com.gradle.plugin-publish") version "1.3.1"
