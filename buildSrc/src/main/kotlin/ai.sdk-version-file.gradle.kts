65aea5470475a391d58067c500f2883ca19fb9ec
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-07-28 15:23:32 -0700

    Fix a deprecation

--- buildSrc/src/main/kotlin/ai.sdk-version-file.gradle.kts
+++ buildSrc/src/main/kotlin/ai.sdk-version-file.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   `java-library`
 }
 
-val sdkVersionDir = "${buildDir}/generated/resources/sdk-version"
+val sdkVersionDir = layout.buildDirectory.dir("generated/resources/sdk-version")
 
 tasks {
   register("generateVersionResource") {
