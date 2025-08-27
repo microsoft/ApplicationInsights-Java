6055f248c27d74271149e9daa135af55ca7bc42f
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-08-27 16:47:12 -0700

    Update semconv artifact version (#4438)

--- agent/agent-bootstrap/build.gradle.kts
+++ agent/agent-bootstrap/build.gradle.kts
@@ -7,6 +7,6 @@ dependencies {
   // needed to access io.opentelemetry.instrumentation.api.aisdk.MicrometerUtil
   // TODO (heya) remove this when updating to upstream micrometer instrumentation
   compileOnly("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api")
-  compileOnly("io.opentelemetry:opentelemetry-semconv")
+  compileOnly("io.opentelemetry.semconv:opentelemetry-semconv")
   compileOnly("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-incubator")
 }
