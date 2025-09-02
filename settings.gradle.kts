8e669e68e24b5ac553ecc61b876a3471af77b960
Jean Bisutti <jean.bisutti@gmail.com>
2025-09-02 12:00:28 +0200

    Rollback #4373

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,6 @@ pluginManagement {
   }
 }
 
-plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 dependencyResolutionManagement {
   repositories {
     mavenCentral()
