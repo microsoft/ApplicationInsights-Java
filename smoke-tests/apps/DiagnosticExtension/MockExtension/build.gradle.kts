d213d4179982efc0e3e0ebc7686bb785b320fb46
Sean Li <xili9@microsoft.com>
2025-10-23 18:44:16 -0700

    Ignore warnings regarding Java 8 compilation warnings

--- smoke-tests/apps/DiagnosticExtension/MockExtension/build.gradle.kts
+++ smoke-tests/apps/DiagnosticExtension/MockExtension/build.gradle.kts
@@ -18,6 +18,12 @@ tasks.withType<JavaCompile>().configureEach {
   with(options) {
     release.set(8)
     compilerArgs.add("-Werror")
+    // We need to support compiling to Java 8 even when using JDK 21 to build.
+    // Suppress obsolete source/target warning added in JDK 21 while retaining -Werror for everything else.
+    // This only disables the 'options' lint category (e.g., the obsolete source/target messages).
+    if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_21)) {
+      compilerArgs.add("-Xlint:-options")
+    }
   }
 }
 
