314a866c1a9fe68f1ccdb719f572f29024544b43
Sean Li <xili9@microsoft.com>
2026-01-23 22:49:34 -0800

    ./gradlew spotlessApply

--- smoke-tests/apps/CoreAndFilter3xUsingOld3xAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/CoreAndFilter3xUsingOld3xAgentSplitTest.java
+++ smoke-tests/apps/CoreAndFilter3xUsingOld3xAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/CoreAndFilter3xUsingOld3xAgentSplitTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.applicationinsights.smoketest;
 
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_17;
@@ -7,17 +10,17 @@ import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDF
 
 public class CoreAndFilter3xUsingOld3xAgentSplitTest {
 
-	@Environment(TOMCAT_8_JAVA_17)
-	static class Tomcat8Java17Test extends CoreAndFilter3xUsingOld3xAgentTest {}
+  @Environment(TOMCAT_8_JAVA_17)
+  static class Tomcat8Java17Test extends CoreAndFilter3xUsingOld3xAgentTest {}
 
-	@Environment(TOMCAT_8_JAVA_17_OPENJ9)
-	static class Tomcat8Java17OpenJ9Test extends CoreAndFilter3xUsingOld3xAgentTest {}
+  @Environment(TOMCAT_8_JAVA_17_OPENJ9)
+  static class Tomcat8Java17OpenJ9Test extends CoreAndFilter3xUsingOld3xAgentTest {}
 
-	// note: old 3.x agents don't support Java 19
+  // note: old 3.x agents don't support Java 19
 
-	@Environment(WILDFLY_13_JAVA_8)
-	static class Wildfly13Java8Test extends CoreAndFilter3xUsingOld3xAgentTest {}
+  @Environment(WILDFLY_13_JAVA_8)
+  static class Wildfly13Java8Test extends CoreAndFilter3xUsingOld3xAgentTest {}
 
-	@Environment(WILDFLY_13_JAVA_8_OPENJ9)
-	static class Wildfly13Java8OpenJ9Test extends CoreAndFilter3xUsingOld3xAgentTest {}
+  @Environment(WILDFLY_13_JAVA_8_OPENJ9)
+  static class Wildfly13Java8OpenJ9Test extends CoreAndFilter3xUsingOld3xAgentTest {}
 }
