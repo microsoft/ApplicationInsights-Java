8ec2df15369c285067c6622c34851c64c570316c
xiang17 <xili9@microsoft.com>
2026-03-05 10:56:23 -0800

    Split the slowest smoke test to reduce PR check time (#4598)

--- smoke-tests/apps/CoreAndFilter3xUsingOld3xAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/CoreAndFilter3xUsingOld3xAgentTest.java
+++ smoke-tests/apps/CoreAndFilter3xUsingOld3xAgent/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/CoreAndFilter3xUsingOld3xAgentTest.java
@@ -5,12 +5,8 @@ package com.microsoft.applicationinsights.smoketest;
 
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_11;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_11_OPENJ9;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_17;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_17_OPENJ9;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_8;
 import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.TOMCAT_8_JAVA_8_OPENJ9;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDFLY_13_JAVA_8;
-import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.WILDFLY_13_JAVA_8_OPENJ9;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -537,18 +533,4 @@ abstract class CoreAndFilter3xUsingOld3xAgentTest {
 
   @Environment(TOMCAT_8_JAVA_11_OPENJ9)
   static class Tomcat8Java11OpenJ9Test extends CoreAndFilter3xUsingOld3xAgentTest {}
-
-  @Environment(TOMCAT_8_JAVA_17)
-  static class Tomcat8Java17Test extends CoreAndFilter3xUsingOld3xAgentTest {}
-
-  @Environment(TOMCAT_8_JAVA_17_OPENJ9)
-  static class Tomcat8Java17OpenJ9Test extends CoreAndFilter3xUsingOld3xAgentTest {}
-
-  // note: old 3.x agents don't support Java 19
-
-  @Environment(WILDFLY_13_JAVA_8)
-  static class Wildfly13Java8Test extends CoreAndFilter3xUsingOld3xAgentTest {}
-
-  @Environment(WILDFLY_13_JAVA_8_OPENJ9)
-  static class Wildfly13Java8OpenJ9Test extends CoreAndFilter3xUsingOld3xAgentTest {}
 }
