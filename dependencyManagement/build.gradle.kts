2f036c110da1014a7df10d516d5e3b39bf3e9864
Sean Li <xili9@microsoft.com>
2026-03-23 13:20:05 -0700

    Bump otelInstrumentationVersion to 2.26.0, otelSdkVersion to 1.60.1, otelContribVersion to 1.54.0

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -7,9 +7,9 @@ data class DependencySet(val group: String, val version: String, val modules: Li
 val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
-val otelSdkVersion = "1.59.0"
-val otelInstrumentationAlphaVersion = "2.25.0-alpha"
-val otelInstrumentationVersion = "2.25.0"
+val otelSdkVersion = "1.60.1"
+val otelInstrumentationAlphaVersion = "2.26.0-alpha"
+val otelInstrumentationVersion = "2.26.0"
 val otelContribVersion = "1.54.0"
 
 rootProject.extra["otelInstrumentationVersion"] = otelInstrumentationVersion
