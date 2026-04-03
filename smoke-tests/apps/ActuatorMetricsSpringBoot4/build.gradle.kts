ba8e9d3d6ce9b50097cfc74531bb77463e944a3b
Sean Li <xili9@microsoft.com>
2026-04-03 15:36:06 -0700

    Created a new smoke test to validate Micrometer/ActuatorMetrics instrumentation for Spring Boot 4

---
+++ smoke-tests/apps/ActuatorMetricsSpringBoot4/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+  id("ai.smoke-test-jar")
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web:4.0.0")
+  implementation("org.springframework.boot:spring-boot-starter-actuator:4.0.0")
+}
---
+++ smoke-tests/apps/ActuatorMetricsSpringBoot4/src/smokeTest/java/com/microsoft/applicationinsights/smoketest/ActuatorMetricsSpringBoot4Test.java
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.applicationinsights.smoketest;
+
+import static com.microsoft.applicationinsights.smoketest.EnvironmentValue.JAVA_21;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.microsoft.applicationinsights.smoketest.schemav2.Data;
+import com.microsoft.applicationinsights.smoketest.schemav2.DataPoint;
+import com.microsoft.applicationinsights.smoketest.schemav2.Envelope;
+import com.microsoft.applicationinsights.smoketest.schemav2.MetricData;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+@UseAgent
+abstract class ActuatorMetricsSpringBoot4Test {
+
+  @RegisterExtension static final SmokeTestExtension testing = SmokeTestExtension.create();
+
+  @Test
+  @TargetUri("/test")
+  void shouldCaptureCustomMetricRegisteredOnSpringMeterRegistry() throws Exception {
+    testing.getTelemetry(0);
+
+    List<Envelope> metricItems =
+        testing.mockedIngestion.waitForItems(
+            ActuatorMetricsSpringBoot4Test::isTargetMetric, 1, 20, TimeUnit.SECONDS);
+
+    MetricData data = (MetricData) ((Data<?>) metricItems.get(0).getData()).getBaseData();
+    List<DataPoint> points = data.getMetrics();
+
+    assertThat(points).anySatisfy(point -> assertThat(point.getName()).isEqualTo("demo_requests_total"));
+    assertThat(data.getProperties()).containsEntry("endpoint", "test");
+  }
+
+  static boolean isTargetMetric(Envelope input) {
+    if (!input.getData().getBaseType().equals("MetricData")) {
+      return false;
+    }
+    MetricData data = (MetricData) ((Data<?>) input.getData()).getBaseData();
+    if (!"test".equals(data.getProperties().get("endpoint"))) {
+      return false;
+    }
+    for (DataPoint point : data.getMetrics()) {
+      if ("demo_requests_total".equals(point.getName()) && point.getValue() >= 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Environment(JAVA_21)
+  static class Java21Test extends ActuatorMetricsSpringBoot4Test {}
+}
