767f2de4e65e2cf2b2f25577d50b39be3bec60c5
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-22 08:59:32 -0700

    simplify

--- smoke-tests/apps/ActuatorMetricsSpringBoot4/src/main/java/com/microsoft/applicationinsights/smoketestapp/SpringBootApp.java
+++ smoke-tests/apps/ActuatorMetricsSpringBoot4/src/main/java/com/microsoft/applicationinsights/smoketestapp/SpringBootApp.java
@@ -5,17 +5,9 @@ package com.microsoft.applicationinsights.smoketestapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootApp {
-
-  @Bean
-  public ServletWebServerFactory servletWebServerFactory() {
-    return new TomcatServletWebServerFactory();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootApp.class, args);
