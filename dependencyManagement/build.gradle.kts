6ad766cd768a0af724aae09445a988198794e551
Sean Li <xili9@microsoft.com>
2026-05-20 17:58:27 -0700

    Sync OTel versions with upstream

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -7,10 +7,10 @@ data class DependencySet(val group: String, val version: String, val modules: Li
 val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
-val otelSdkVersion = "1.60.1"
+val otelSdkVersion = "1.61.0"
 val otelInstrumentationAlphaVersion = "2.27.0-alpha"
-val otelInstrumentationVersion = "2.26.1"
-val otelContribVersion = "1.54.0"
+val otelInstrumentationVersion = "2.27.0"
+val otelContribVersion = "1.56.0"
 
 rootProject.extra["otelInstrumentationVersion"] = otelInstrumentationVersion
 rootProject.extra["otelInstrumentationAlphaVersion"] = otelInstrumentationAlphaVersion
